32651f934924f2dcb32f298b65b22904b4acb672
Claudia J. Kang <claudiajkang@gmail.com>
2019-11-12 21:56:24 +0900

    Update insertion sort source code

--- sort_insertion.py
+++ sort_insertion.py
@@ -1,13 +1,9 @@
 def insertion(l):
-    for i in range(0, len(l)):
-        t = i
-        print("++++")
-        print(i)
-        for j in range(i+1, len(l)):
-            if l[t] > l[j]:
-                l.insert(t, l[j])
-                del l[j+1] 
-        print(l)
+    for i in range(1, len(l)):
+        j = i
+        while j > 0 and l[j-1] > l[j]:
+            l[j-1], l[j] = l[j], l[j-1]
+            j -=1
 
 tl = [11,3,28,43,9,4]
 print(tl)
