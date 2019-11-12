5a20c841c9f4a92eb04386570f837a9b72d73ff8
Claudia J. Kang <claudiajkang@gmail.com>
2019-11-13 00:54:56 +0900

    Update gnome sort book source

--- sort_gnome.py
+++ sort_gnome.py
@@ -1,21 +1,15 @@
 def gnome(l):
-    for i in range(0, len(l)-1):
-        print("+++")
-        t=i
-        for j in range(i+1, -1, -1):
-            if t < j and l[t] > l[j] and t>-1:
-                l[t], l[j] = l[j], l[t]
-                print(l)
-
-                if t == j-1:
-                    t = j-2
-
+    i = 0
+    while i<=len(l):
+        if i==0 or l[i-1] <= l[i]:
+            i=i+1
+        else:
+            l[i-1], l[i] = l[i], l[i-1]
+            i=i-1
+        
 temp = [5,3,2,4]
 print("---")
 print(temp)
 gnome(temp)
 print("---")
 print(temp)
-
-
-
