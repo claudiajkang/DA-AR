0da73e70844c450e942c042734742b634f932579
Claudia J. Kang <claudiajkang@gmail.com>
2019-11-12 21:02:55 +0900

    Update bubble sort book source

--- sort_bubble.py
+++ sort_bubble.py
@@ -1,11 +1,9 @@
 def bubble(l):
-    for i in range(0, len(l)):
-        for j in range(0, len(l)-1):
+    for i in range(len(l)-1, 0, -1):
+        for j in range(i):
             if l[j] > l[j+1]:
-                t = l[j]
-                l[j] = l[j+1]
-                l[j+1] = t
-           
+                l[j], l[j+1] = l[j+1], l[j]
+
                 print("----")
                 print(i) 
                 print(l)
