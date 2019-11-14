7d1a7dc8eb40d90a23de3c97e1ac93f6d69db788
Claudia J. Kang <claudiajkang@gmail.com>
2019-11-14 12:40:18 +0900

    Add linear search

---
+++ search_linear.py
@@ -0,0 +1,26 @@
+def linearsearch(olist, val):
+    for i in olist:
+        if i == val:
+            return True
+
+    return False
+
+def resprint(res, val):
+    ps = str(val) + ": "
+
+    if res:
+        ps = ps + "O"
+
+    else:
+        ps = ps + "X"
+
+    print(ps)
+
+plist = [1,5,6,8,3]
+fv = 6
+res = linearsearch(plist,fv)
+resprint(res, fv)
+print("---")
+fv = 10
+res = linearsearch(plist,fv)
+resprint(res, fv)
