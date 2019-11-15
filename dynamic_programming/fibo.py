603ddc7746649e93277f6d8f3c4031dbd7964e83
Claudia J. Kang <claudiajkang@gmail.com>
2019-11-15 14:21:45 +0900

    Update fibonacci book source by using DP

--- dynamic_programming/fibo.py
+++ dynamic_programming/fibo.py
@@ -1,13 +1,9 @@
 def fibo(n, res):
-    if n == 1 or n == 2:
-        if res[n] == 0:
-            res[n] = 1
-        return 1
-    elif res[n] == 0:
+    if res[n] == 0:
         res[n] = fibo(n-1, res) + fibo(n-2, res)
-
     return res[n]
 
-n = 100
+n= 10
 res = [0] * (n+1)
-print(fibo(n,res))
+res[1], res[2] = 1,1
+print(fibo(n, res))
