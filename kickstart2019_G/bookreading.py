6a9c8a6891222116727233693d259c0a86111e98
Claudia J. Kang <claudiajkang@gmail.com>
2019-11-16 16:48:58 +0900

    Update kickstart2019 round G bookreading code #2

--- kickstart2019_G/bookreading.py
+++ kickstart2019_G/bookreading.py
@@ -1,18 +1,15 @@
 import math
 
 T = int(input())
-
-for i in range(T):
+for t in range(T):
     [N, M, Q] = [int(s) for s in input().split()]
     P = [int(s) for s in input().split()]
     R = [int(s) for s in input().split()]
-
-    ans = 0
-
+    total = 0
     for r in R:
-        ans += math.floor(N/r)
+        total += math.floor(N / r)
         for p in P:
             if p % r == 0:
-                ans -= 1
+                total -= 1
 
-        print("Case #{}: {}".format(i+1, ans))
+    print("Case #{}: {}".format(t + 1, total))
