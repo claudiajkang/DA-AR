38bde0fa1d8900a262f7d7c9c04857b10050c2d0
Claudia J. Kang <claudiajkang@gmail.com>
2019-11-16 16:44:56 +0900

    Update kickstart2019 round G bookreading code

--- kickstart2019_G/bookreading.py
+++ kickstart2019_G/bookreading.py
@@ -1,25 +1,18 @@
+import math
+
 T = int(input())
 
 for i in range(T):
-    N, M, Q = map(int, input().split())
-    BOOK = [1] * (N+1)
+    [N, M, Q] = [int(s) for s in input().split()]
+    P = [int(s) for s in input().split()]
+    R = [int(s) for s in input().split()]
 
-    P = list(map(int, input().split()))
-    R = list(map(int, input().split()))
+    ans = 0
 
-    sum = 0
+    for r in R:
+        ans += math.floor(N/r)
+        for p in P:
+            if p % r == 0:
+                ans -= 1
 
-    for i in range(M):
-        v = P[i]
-        BOOK[v] = 0
-
-    if N > len(P):
-        for j in range(Q):
-            sum = sum + int(N/R[j])
-
-            for k in P:
-                if k % R[j] == 0:
-                    sum = sum - 1
-
-    ps = "Case #{}: {}".format(i+1,sum)
-    print(ps)
+        print("Case #{}: {}".format(i+1, ans))
