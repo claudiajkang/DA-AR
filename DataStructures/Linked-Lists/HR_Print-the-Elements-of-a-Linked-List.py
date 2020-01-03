0afc5c68ca52161d08b4a9e5797120510c9d6c96
Claudia J. Kang <claudiajkang@gmail.com>
2020-01-03 15:05:52 +0900

    Add code that solved Print the Elements of a Linked List problem
    prob : https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem

---
+++ DataStructures/Linked-Lists/HR_Print-the-Elements-of-a-Linked-List.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+    def get_value(self):
+        return self.data
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    while True:
+        if head.next is None:
+            print(head.data)
+            break
+
+        else:
+            print(head.data)
+            head = head.next
+
+
+if __name__ == '__main__':
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
