2cc4d14997dc2fd1e1518ea955f5257b9a22b9e0
Claudia J. Kang <claudiajkang@gmail.com>
2020-01-04 15:36:28 +0900

    Add successful code that solved Reverse a linked list problem
    prob : https://www.hackerrank.com/challenges/reverse-a-linked-list/problem

---
+++ DataStructures/Linked-Lists/HR_Reverse-a-linked-list.py
@@ -0,0 +1,91 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+# Complete the reverse function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def reverse(head):
+    temp_list = list()
+
+    onode = head
+
+    while True:
+        if onode is None:
+            break
+
+        temp_list.append(onode.data)
+        onode = onode.next
+
+    new_head = SinglyLinkedList()
+
+    for k in range(len(temp_list)-1, -1, -1):
+        new_head.insert_node(temp_list[k])
+
+    return new_head.head
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
