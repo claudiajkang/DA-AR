79a236680af17a199cf7c52fe3d1568e0d72532a
Claudia J. Kang <claudiajkang@gmail.com>
2020-01-04 14:26:39 +0900

    Add code that solved Delete a Node problem
    prob : https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem

---
+++ DataStructures/Linked-Lists/HR_Delete-a-Node.py
@@ -0,0 +1,86 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the deleteNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def deleteNode(head, position):
+    parent_node = None
+
+    for i in range(0, position):
+        if i == 0:
+            parent_node = head
+
+        else:
+            parent_node = parent_node.next
+
+    if position == 0:
+        return head.next
+
+    del_node_next = parent_node.next
+    parent_node.next = del_node_next.next
+
+    return head
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
