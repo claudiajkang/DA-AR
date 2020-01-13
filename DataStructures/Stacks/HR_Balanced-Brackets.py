b95878d3ab84108a80e1ba1bb49f6c3eda031d8f
Claudia J. Kang <claudiajkang@gmail.com>
2020-01-13 15:38:50 +0900

    Update code that solved Balanced Brackets problem
    prob : https://www.hackerrank.com/challenges/balanced-brackets/problem

--- DataStructures/Stacks/HR_Balanced-Brackets.py
+++ DataStructures/Stacks/HR_Balanced-Brackets.py
@@ -8,17 +8,46 @@ import sys
 
 # Complete the isBalanced function below.
 def isBalanced(s):
+    tstack = list()
+    start = ['(', '{', '[']
+    end = [')', '}', ']']
+
+    for t in range(len(s)):
+        value = s[t]
+
+        if len(tstack) == 0:
+            tstack.append(value)
+            continue
+
+        if value in start:
+            tstack.append(value)
+            continue
+
+        if value in end:
+            svalue = tstack[-1]
+            if svalue in start:
+                index = start.index(svalue)
+                if (svalue == start[index]) and (value == end[index]):
+                    n = tstack.pop()
+                    continue
+
+            tstack.append(value)
+
+    if len(tstack) == 0:
+        return "YES"
+    else:
+        return "NO"
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     t = int(input())
 
     for t_itr in range(t):
         s = input()
-
         result = isBalanced(s)
+        print(result)
+        # fptr.write(result + '\n')
 
-        fptr.write(result + '\n')
-
-    fptr.close()
+    # fptr.close()
