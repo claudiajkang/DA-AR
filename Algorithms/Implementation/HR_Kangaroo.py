bf7b7f8a37dc0673fc6f22ad79dfcf0241ebd6b8
Claudia J. Kang <claudiajkang@gmail.com>
2020-01-28 16:26:57 +0900

    Add code that solved Kangaroo problem
    prob : https://www.hackerrank.com/challenges/kangaroo/problem

---
+++ Algorithms/Implementation/HR_Kangaroo.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    t1 = x1
+    t2 = x2
+    res = "NO"
+
+    for i in range(0, 100000):
+        t1 += v1
+        t2 += v2
+
+        if t1 == t2:
+            res = "YES"
+            break
+
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
