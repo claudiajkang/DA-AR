bfb8c585decc0b144f92eca4ca8cd48d0c3be4ac
Claudia J. Kang <claudiajkang@gmail.com>
2020-01-30 19:38:30 +0900

    Add code that solved Save the Prisoner problem
    prob : https://www.hackerrank.com/challenges/save-the-prisoner/problem

---
+++ Algorithms/Implementation/HR_Save-the-Prisoner.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the saveThePrisoner function below.
+def saveThePrisoner(n, m, s):
+    ts = s
+    tm = m
+
+    if s != 1:
+        t = ts - 1
+        ts -= t
+        tm += t
+
+    res = tm % n
+
+    if res == 0:
+        res = n
+
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nms = input().split()
+
+        n = int(nms[0])
+
+        m = int(nms[1])
+
+        s = int(nms[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
