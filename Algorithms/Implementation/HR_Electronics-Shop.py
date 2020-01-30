6fdd29c3ea6b14a4efc2f3447b1cb10df6621a08
Claudia J. Kang <claudiajkang@gmail.com>
2020-01-30 12:17:34 +0900

    Add code that solved Electronics Shop problem
    prob : https://www.hackerrank.com/challenges/electronics-shop/problem

---
+++ Algorithms/Implementation/HR_Electronics-Shop.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import os
+import sys
+
+
+def getMoneySpent(keyboards, drives, b):
+    tc = -1
+
+    for k in keyboards:
+        for d in drives:
+            if tc < (k + d) <= b:
+                tc = k + d
+
+    return tc
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
