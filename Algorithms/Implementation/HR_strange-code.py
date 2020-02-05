6d419a9c7b486c85d06ff58b47873be23e6ae42d
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-05 15:45:54 +0900

    Add refactoring code that solved strange-code problem
    prob : https://www.hackerrank.com/challenges/strange-code/problem

--- Algorithms/Implementation/HR_strange-code.py
+++ Algorithms/Implementation/HR_strange-code.py
@@ -8,23 +8,38 @@ import sys
 
 
 def strangeCounter(t):
-    cycle = 0
-    value = list()
+    value = [3]
+    si = 0
+    time = [1]
 
-    if len(str(t)) >= 10:
-        cycle = 10
+    if t <= 3:
+        c = 4
+        for i in range(t):
+            c -= 1
+
+        return c
 
     for i in range(t):
-        tv = 3 * (1 << cycle)
-        if tv <= t:
-            if tv not in value:
-                value.append(tv)
-                cycle += 1
-
-        else:
-            start_time = value[-1] + 1
-            value.append(tv)
+        nv = value[-1] * 2
+        value.append(nv)
+        if t < (nv * 2):
             break
+
+    for j in range(1, len(value)):
+        time.append(time[j - 1] + value[j - 1])
+        if time[j-1] <= t < time[j]:
+            si = j
+        elif t >= time[j]:
+            si = j
+
+    rvalue = value[si]
+
+    for j in range(time[si], t+1):
+        if j == t:
+            return rvalue
+        rvalue -= 1
+
+    return 1
 
 
 if __name__ == '__main__':
