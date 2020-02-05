372083e373cfcc7dfcc4e804411d4b73b1d18b77
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-05 17:17:39 +0900

    This is a combination of 2 commits.
    Update print_st code for BOJ

--- print_st.py
+++ print_st.py
@@ -4,21 +4,30 @@ problem = input("problem url: ")
 
 tp = problem.split('/')
 
-filename = tp[-2]
 
 prob_url = problem
 if '?' in problem:
     prob_url = problem.split('?')[0]
 
+if "acmicpc" in prob_url:
+    pre_val = "BOJ_"
+    filename = tp[-1]
+
+if "hacker" in prob_url:
+    pre_val = "HR_"
+    filename = tp[-2]
+
 print("\n======filename")
-print("HR_"+filename+".py")
-print("\n======PR MSG #1")
+print(pre_val+filename+".py")
+print("\n======PRE PR")
 print("git add .")
 print('git commit -m "')
+print("\n======PR MSG #1")
 print("Init source code from "+filename+" problem")
 print("\nprob : "+prob_url+'"')
 print("\n======PR MSG #2")
-print("git add .")
-print('git commit -m "')
 print("Update code that solved "+filename+" problem")
+print("\nprob : "+prob_url+'"')
+print("\n======PR MSG #3")
+print("Add code that solved "+filename+" problem")
 print("\nprob : "+prob_url+'"'+"\n")
