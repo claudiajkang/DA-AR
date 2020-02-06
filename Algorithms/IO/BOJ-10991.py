01b0f7cd645f72a618b3400cc166a7d506849b4a
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-06 20:01:24 +0900

    Add code that solved BOJ-10991 problem
    prob : https://www.acmicpc.net/problem/10991

---
+++ Algorithms/IO/BOJ-10991.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+for i in range(N):
+    if N > 1:
+        for j in range(N-i-1):
+            print(' ', end='')
+
+    for k in range(i + 1):
+        print('*', end=' ')
+
+    print('')
