9971e61fc39e14e345b00e83a2b1197d68a54229
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-06 16:06:29 +0900

    Change commit message in print_st.py

--- print_st.py
+++ print_st.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-problem = input("problem url: ")
+problem = input('problem url: ')
 
 tp = problem.split('/')
 
@@ -9,25 +9,20 @@ prob_url = problem
 if '?' in problem:
     prob_url = problem.split('?')[0]
 
-if "acmicpc" in prob_url:
-    pre_val = "BOJ_"
-    filename = tp[-1]
+if 'acmicpc' in prob_url:
+    filename = 'BOJ-' + tp[-1]
 
-if "hacker" in prob_url:
-    pre_val = "HR_"
-    filename = tp[-2]
+if 'hacker' in prob_url:
+    filename = 'HR-' + tp[-2]
 
-print("\n======filename")
-print(pre_val+filename+".py")
-print("\n======PRE PR")
-print("git add .")
-print('git commit -m "')
-print("\n======PR MSG #1")
-print("Init source code from "+filename+" problem")
-print("\nprob : "+prob_url+'"')
-print("\n======PR MSG #2")
-print("Update code that solved "+filename+" problem")
-print("\nprob : "+prob_url+'"')
-print("\n======PR MSG #3")
-print("Add code that solved "+filename+" problem")
-print("\nprob : "+prob_url+'"'+"\n")
+print('\n======filename')
+print(filename+'.py')
+print('\n======PR MSG #1')
+print('git add .;git commit -m "Init source code from '+filename+' problem')
+print('\nprob : '+prob_url+'"')
+print('\n======PR MSG #2')
+print('git add .;git commit -m "Update code that solved '+filename+' problem')
+print('\nprob : '+prob_url+'"')
+print('\n======PR MSG #3')
+print('git add .;git commit -m "Add code that solved '+filename+' problem')
+print('\nprob : '+prob_url+'"'+'\n')
