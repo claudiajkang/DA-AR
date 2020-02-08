0b8e08174da1ccf248064956253c34c8fc95307a
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-08 14:11:39 +0900

    Review BOJ-10992 problem

--- Algorithms/IO/BOJ-10992.py
+++ Algorithms/IO/BOJ-10992.py
@@ -1,25 +1,20 @@
 import sys
-
+  
 N = int(sys.stdin.readline())
 
 for i in range(N):
     for j in range(N-i-1):
-        print(' ', end ='')
+        print(' ', end = '')
 
-    print('*', end = '')
-
-    if i == (N-1):
-        for k in range(2*i):
-            print('*', end ='')
+    print('*', end ='')
+    if i != (N-1):
+        c = (i-1)*2 + 1
+        for j in range(c):
+            print(' ', end = '')
+            if j == (c-1):
+                print('*', end ='')
 
     else:
-        for j in range(i):
-            print(' ', end ='')
-
-        for j in range(i-1):
-            print(' ', end = '')
-
-        if i:
+        for i in range(i+N-1):
             print('*', end ='')
-
     print('')
