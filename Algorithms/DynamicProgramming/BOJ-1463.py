324a593ba324129597c7239457fe9f9f6314686b
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-11 18:16:27 +0900

    Review BOJ-1463

--- Algorithms/DynamicProgramming/BOJ-1463.py
+++ Algorithms/DynamicProgramming/BOJ-1463.py
@@ -1,21 +1,16 @@
 import sys
   
-X = int(sys.stdin.readline())
+N = int(sys.stdin.readline())
 m = 1000001
 dp = [0 for i in range(m)]
 
-for i in range(1, X+1):
-    if i == 1:
-        dp[i] = 0
-    eif i in [2, 3]:  
+for i in range(2, N+1):
+    if i == 2 or i == 3:  
         dp[i] = 1
     else:
-        d2, d3 = m, m
         d1 = dp[i-1] + 1
-        if i % 2 == 0:
-            d2 = dp[int(i/2)] + 1
-        if i % 3 == 0:
-            d3 = dp[int(i/3)] + 1
+        d2 = [dp[int(i/2)]+1, m][i%2]
+        d3 = [dp[int(i/3)]+1, m, m][i%3]
         dp[i] = min(d1,d2,d3)
 
-print(dp[X])
+print(dp[N])
