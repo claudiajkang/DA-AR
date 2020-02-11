a4ca6cb4bb9c7fe3316afe0601549d470b93bbaf
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-11 17:14:25 +0900

    Review BOJ-10992

--- Algorithms/IO/BOJ-10992.py
+++ Algorithms/IO/BOJ-10992.py
@@ -1,25 +1,15 @@
 import sys
-  
+
 N = int(sys.stdin.readline())
-w = 2*N - 1
-ps = ''
 
-if N == 1:
-    ps += '*'
+for i in range(N):
+    if i == 0:
+        p = ' '*(N-i-1) + '*'
 
-else:
-    for i in range(N):
-        if i == (N-1):
-            for k in range(w):
-                ps += '*'
-        else:
-            for j in range(N-i-1):
-                ps += ' '
-            ps += '*'
-            v = 2 * i -1
-            for j in range(v):
-                ps += ' '
-                if j == (v-1):
-                    ps += '*'
-            ps += '\n'
-print(ps)
+    elif i == (N-1):
+        p = '*'*(2*N-1)
+
+    else:
+        p = ' '*(N-i-1) + '*' + ' '*(2*i -1) + '*'
+
+    print(p)
