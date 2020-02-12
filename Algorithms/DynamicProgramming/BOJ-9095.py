cd2f14515220fd42702cb15b5d58160244342ca8
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-12 15:26:22 +0900

    Review BOJ-9095

--- Algorithms/DynamicProgramming/BOJ-9095.py
+++ Algorithms/DynamicProgramming/BOJ-9095.py
@@ -1,20 +1,19 @@
 import sys
 
 T = int(sys.stdin.readline())
-m = 12
-dp = [0 for i in range(m)]
-
-for i in range(1, m):
-    if i == 1:
-        dp[i] = 1
-    elif i == 2: 
-        dp[i] = 2
-    elif i == 3:
-        dp[i] = 4
-    else:
-        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+inputs = list()
 
 for i in range(T):
-    idx = int(sys.stdin.readline())
-    print(dp[idx])
+    inputs.append(int(sys.stdin.readline()))
 
+m = max(inputs)
+dp = [0 for i in range(m+1)]
+ 
+for i in range(1, m+1):
+    if 1<= i <= 3:
+        dp[i] = [1, 2, 4][i-1]
+    else:
+        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+
+for j in inputs:
+    print(dp[j])
