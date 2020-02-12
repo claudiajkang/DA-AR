fb435152fd74191a370d3d926486f461f22d3713
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-12 14:18:01 +0900

    Review BOJ-11726

--- Algorithms/DynamicProgramming/BOJ-11726.py
+++ Algorithms/DynamicProgramming/BOJ-11726.py
@@ -1,19 +1,12 @@
 import sys
   
 n = int(sys.stdin.readline())
-m = 1001
-res = [0 for i in range(m)]
+dp = [0 for i in range(n+1)]
 
-for i in range(1, n+1):
-    if i == 1:
-        res[i] = 1
-    elif i == 2:
-        res[i] = 2
-    elif i == 3:
-        res[i] = 3
-    elif i == 4:
-        res[i] = 5
+for i in range(1,n+1):
+    if 1 <= i <= 4:
+        dp[i] = [1,2,3,5][i-1]
     else:
-        res[i] = res[i-1] + res[i-2]
+        dp[i] = dp[i-1] + dp[i-2]
 
-print(res[n]%10007)
+print(dp[n]%10007)
