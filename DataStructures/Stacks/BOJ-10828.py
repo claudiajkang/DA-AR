0a49c8b5ee278041f26e4e8d8a5ab372e52ae677
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-16 05:58:13 +0900

    Review BOJ-10828

--- DataStructures/Stacks/BOJ-10828.py
+++ DataStructures/Stacks/BOJ-10828.py
@@ -1,54 +1,20 @@
-import sys
+from sys import stdin
 
+next(stdin)
+s = list()
 
-class Stack:
-    def __init__(self):
-        self.s = list()
-        self.size = 0
-
-    def push(self, x):
-        self.s.append(x)
-        self.set_size()
-
-    def set_size(self):
-        self.size = len(self.s)
-
-    def get_size(self):
-        return self.size
-
-    def empty(self):
-        if self.size == 0:
-            return 1
-        return 0
-
-    def pop(self):
-        if self.size == 0:
-            return -1
-        v = self.s.pop()
-        self.set_size()
-        return v
-
-    def top(self):
-        if self.size == 0:
-            return -1
-        return self.s[-1]
-
-
-N = int(sys.stdin.readline())
-stacks = Stack()
-
-for i in range(N):
-    c = sys.stdin.readline()
-    command = c.split()[0]
-
-    if command == 'push':
-        v = c.split()[1]
-        stacks.push(v)
-    elif command == 'pop':
-        print(stacks.pop())
-    elif command == 'top':
-        print(stacks.top())
-    elif command == 'size':
-        print(stacks.get_size())
-    elif command == 'empty':
-        print(stacks.empty())
+for line in stdin:
+        c = line.split()[0]
+        if c == 'push':
+                s.append(line.split()[1])
+        elif c == 'pop':
+                if s: print(s.pop())
+                else: print(-1)
+        elif c == 'size':
+                print(len(s))
+        elif c == 'empty':
+                if s: print(0)
+                else: print(1)
+        elif c == 'top':
+                if s: print(s[-1])
+                else: print(-1)
