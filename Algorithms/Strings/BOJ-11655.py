2d6c32ca449aecc2bb1973556a3c5cc2f47dc287
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-16 06:22:53 +0900

    Review BOJ-11655

--- Algorithms/Strings/BOJ-11655.py
+++ Algorithms/Strings/BOJ-11655.py
@@ -1,21 +1,19 @@
-import sys
+from sys import stdin
 
-lo = 'abcdefghijklmnopqrstuvwxyzabcdefghijklm'
-up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM'
+s = stdin.readline().replace('\n','')
+lo = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmn'
+up = lo.upper()
+res = ''
 
-S = sys.stdin.readline().replace('\n','')
+for i in s:
+        if i in lo:
+                idx = lo.find(i)
+                res += lo[idx+13]
+        elif i in up:
+                idx = up.find(i)
+                res += up[idx+13]
+        else:
+                res += i
 
-res = ['' for i in range(101)]
-
-for i in range(len(S)):
-    if S[i] in lo:
-        idx = lo.find(S[i])
-        res[i] = lo[idx+13]
-    elif S[i] in up:
-        idx = up.find(S[i])
-        res[i] = up[idx+13]
-    else:
-        res[i] = S[i]
-
-print(''.join(res))
+print(res)
     
