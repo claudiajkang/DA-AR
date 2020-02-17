2d3bfb65fe669e8b849ff2f5fec220b1c882a160
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-17 14:23:12 +0900

    Review BOJ-2609

--- Algorithms/Mathematics/BOJ-2609.py
+++ Algorithms/Mathematics/BOJ-2609.py
@@ -1,21 +1,13 @@
 from sys import stdin
+  
+def gcd(a, b):
+    if b:
+        return gcd(b, a%b)
+    else:
+        return a
 
 A, B = map(int, stdin.readline().strip().split())
+g = gcd(A,B)
 
-def gcd(a,b):
-    v1, v2 = A, B
-    if A < B:
-        v1, v2 = B, A
-    
-    mod = v1 % v2
-
-    while mod > 0:
-        v1 = v2
-        v2 = mod
-        mod = v1 % v2
-    
-    return v2
-
-print(gcd(A,B))
-
-print(int((A*B)/gcd(A,B)))
+print(g)
+print(int((A*B)/g))
