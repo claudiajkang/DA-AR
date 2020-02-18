b12307f9dac24e1bdec8e332b2875580de2726c9
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-18 14:25:43 +0900

    Review BOJ-2745

--- Algorithms/Mathematics/BOJ-2745.py
+++ Algorithms/Mathematics/BOJ-2745.py
@@ -1,25 +1,5 @@
 from sys import stdin
 
 N, B = stdin.readline().strip().split()
-alpa = ''
-res = 0
 B = int(B)
-SN = len(N)
-
-for i in range(B):
-    if i >= 10:
-        alpa += chr(65+(i-10))
-
-    else:
-        alpa += str(i)
-
-val = []
-
-for i in range(SN-1, -1, -1):
-    val.append(alpa.find(N[i]))
-
-for i, v in enumerate(val):
-    res += v * pow(B, i)
-
-print(res)
-
+print(int(N, base=B))
