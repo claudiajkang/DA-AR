d89154ce201a44cab3fcff5ee3be82e27b677260
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-19 14:45:43 +0900

    Review HR_Insert-a-node-at-the-head-of-a-linked-list

---
+++ DataStructures/Linked-Lists/HR_Insert-a-node-at-the-head-of-a-linked-list.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def insertNodeAtHead(llist, data):
+    nnode = SinglyLinkedListNode(data)
+    if not llist:
+        tnode = nnode
+    else:
+        tempnode = llist
+        tnode = nnode
+        nnode.next = tempnode
+
+    llist = tnode
+    return llist
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtHead(llist.head, llist_item)
+        llist.head = llist_head
+    
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+    
+    fptr.close()
+
