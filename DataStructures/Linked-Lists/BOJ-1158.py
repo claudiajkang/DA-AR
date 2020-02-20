54a7e4e665fbb07c9fe760e09b27edc5cd9a3304
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-20 14:30:08 +0900

    Review BOJ-1158

--- DataStructures/Linked-Lists/BOJ-1158.py
+++ DataStructures/Linked-Lists/BOJ-1158.py
@@ -1,44 +1,12 @@
 from sys import stdin
 
-class Node:
-    def __init__(self,data=None):
-        self.data = data
-        self.next = None
+N, K = map(int, stdin.readline().split())
+c = [i for i in range(1, N+1)]
+d = []
+e = 0
 
-class LinkedList:
-    def __init__(self ):
-        self.head = Node('head')
-        self.tail = self.head
+while c:
+    e = (e+K-1) % len(c)
+    d.append(str(c.pop(e)))
 
-    def add(self, data):
-        nnode = Node(data)
-        nnode.next = self.head.next
-        self.tail.next = nnode
-        self.tail = nnode
-
-def main():
-    N, K = map(int, stdin.readline().strip().split())
-    LL = LinkedList()
-
-    for i in range(1, N+1):
-        LL.add(i)
-
-    p = LL.head
-    res = []
-    F = 0
-
-    while len(res) < N:
-        for i in range(K-1):
-            p = p.next
-
-        dnode = p.next
-        if str(dnode.data) not in res:
-            res.append(str(dnode.data))
-            p.next = dnode.next
-        else:
-            res.append(str(dnode.next.data))
-            p.next = dnode.next.next
-
-    print('<'+', '.join(res)+'>')
-
-main()
+print('<'+', '.join(d)+'>')
