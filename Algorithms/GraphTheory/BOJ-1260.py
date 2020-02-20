1190430d042865c5224a4eadb15e614b99564a46
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-20 19:32:50 +0900

    Review BOJ-1260

--- Algorithms/GraphTheory/BOJ-1260.py
+++ Algorithms/GraphTheory/BOJ-1260.py
@@ -1,13 +1,15 @@
 from sys import stdin
+import queue
 
 N, M, V = map(int, stdin.readline().strip().split())
 g = {}
 
 for i in range(M):
     t = list(map(int, stdin.readline().strip().split()))
+
     if t[0] not in g.keys():
         g[t[0]] = []
-    
+
     if t[-1] not in g.keys():
         g[t[-1]] = []
 
@@ -19,55 +21,43 @@ if V not in g.keys():
     print(V)
 
 else:
-
-    dstack = [V]
-    dvisit = []
-    gn = len(g.keys())
-
-
     for i in g.keys():
         g[i] = sorted(g[i])
 
-    while True:
-        if dstack:
-            cur = dstack.pop()
-            for i in reversed(g[cur]):
-                if (str(i) not in dvisit) and (i in dstack):
-                    idx = dstack.index(i)
-                    v = dstack.pop(idx)
-                    dstack.append(v)
-                elif str(i) not in dvisit:
-                    dstack.append(i)
+    dstack = [V]
+    dvisit = []
 
-            if str(cur) not in dvisit:
-                dvisit.append(str(cur))
+    while dstack:
+        cur = dstack.pop()
+        if str(cur) not in dvisit:
+            dvisit.append(str(cur))
         else:
-            break
+            continue
+        for i in reversed(g[cur]):
+            if str(i) not in dvisit:
+                dstack.append(i)
 
-    if len(dvisit) != N:
+    vqueue = queue.Queue()
+    vqueue.put(V)
+    vvisit = []
+
+    while vqueue.queue:
+        cur = vqueue.get()
+        if str(cur) not in vvisit:
+            vvisit.append(str(cur))
+        else:
+            continue
+        for i in g[cur]:
+            if str(i) not in vvisit:
+                vqueue.put(i)
+
+
+    if len(dvisit) != N and len(vvisit) != N:
         for i in g.keys():
             if str(i) not in dvisit:
                 dvisit.append(str(i))
-
-    bqueue = [V]
-    bvisit = []
-
-    while True:
-        if bqueue:
-            cur = bqueue.pop(0)
-            for i in g[cur]:
-                if str(i) not in bvisit:
-                    bqueue.append(i)
-
-            if str(cur) not in bvisit:
-                bvisit.append(str(cur))
-        else:
-            break
-
-    if len(bvisit) != N:
-        for i in g.keys():
-            if str(i) not in bvisit:
-                bvisit.append(str(i))
+            if str(i) not in vvisit:
+                vvisit.append(str(i))
 
     print(' '.join(dvisit))
-    print(' '.join(bvisit))
+    print(' '.join(vvisit))
