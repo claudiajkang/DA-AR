c511d24bbed990b5b92613d21d126dd01b08ddfd
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-21 18:40:00 +0900

    Review BOJ-11724

--- Algorithms/GraphTheory/BOJ-11724.py
+++ Algorithms/GraphTheory/BOJ-11724.py
@@ -1,38 +1,28 @@
 from sys import stdin
 
-def dfs(v, g, d):
-    d[v] = True
-    dstack = [v]
-
-    while dstack:
-        cur = dstack.pop()
-        for i in sorted(g[cur]):
-            if not d[i]:
-                dstack.append(i)
-                d[i] = True
-
-    return d
-
-
 N, M = map(int, stdin.readline().split())
-G = [[] for _ in range(N+1)]
-A, B = 0, 0
+visited = [False for i in range(N+1)]
+g = [[] for i in range(N+1)]
+c = 0
 
 for i in range(M):
     A, B = map(int, stdin.readline().split())
-    G[A].append(B)
-    G[B].append(A)
-
-visited = [False for i in range(N+1)]
-c = 0
+    g[A].append(B)
+    g[B].append(A)
 
 for i in range(1, N+1):
-    if visited[i]:
-        continue
+    if not visited[i]:
+        c += 1
+        dstack = [i]
+
+        while dstack:
+            cur = dstack.pop()
+            visited[cur] = True
+            for j in sorted(g[cur]):
+                if not visited[j]:
+                    visited[j] = True
+                    dstack.append(j)
     elif c == N:
         break
-    else:
-        c += 1
-        visited = dfs(i, G, visited)
 
 print(c)
