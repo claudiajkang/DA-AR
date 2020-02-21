48fb1470ee741903252f198423b5d26832fc4c2e
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-21 19:18:51 +0900

    Review BOJ-1168

--- DataStructures/Linked-Lists/BOJ-1168.py
+++ DataStructures/Linked-Lists/BOJ-1168.py
@@ -1,12 +1,12 @@
 from sys import stdin
 
 N, K = map(int, stdin.readline().split())
-PP = [i for i in range(1,N+1)]
 res = []
+PP = [i for i in range(1, N + 1)]
 idx = 0
 
 while PP:
-    idx = (idx + K - 1) % len(PP)
+    idx = int((idx + K - 1) % len(PP))
     res.append(str(PP.pop(idx)))
 
-print('<'+', '.join(res)+'>')
+print('<' + ', '.join(res) + '>')
