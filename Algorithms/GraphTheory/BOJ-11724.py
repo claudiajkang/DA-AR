2089b3834d72eab97504b5ab92fc14cdcb5b8bf5
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-23 14:30:42 +0900

    Review BOJ-11724

--- Algorithms/GraphTheory/BOJ-11724.py
+++ Algorithms/GraphTheory/BOJ-11724.py
@@ -1,28 +1,28 @@
 from sys import stdin
 
 N, M = map(int, stdin.readline().split())
-visited = [False for i in range(N+1)]
 g = [[] for i in range(N+1)]
-c = 0
 
 for i in range(M):
     A, B = map(int, stdin.readline().split())
     g[A].append(B)
     g[B].append(A)
 
+res = 0
+visited = [False for i in range(N+1)]
+
 for i in range(1, N+1):
     if not visited[i]:
-        c += 1
+        res += 1
+        visited[i] = True
+
         dstack = [i]
 
         while dstack:
             cur = dstack.pop()
-            visited[cur] = True
-            for j in sorted(g[cur]):
+            for j in g[cur]:
                 if not visited[j]:
                     visited[j] = True
                     dstack.append(j)
-    elif c == N:
-        break
 
-print(c)
+print(res)
