025961275125811a2eb580b2b7adeefab4007b3c
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-24 14:33:49 +0900

    Review BOJ-10451

--- Algorithms/GraphTheory/BOJ-10451.py
+++ Algorithms/GraphTheory/BOJ-10451.py
@@ -2,29 +2,39 @@ import sys
 
 T = int(sys.stdin.readline())
 
-for ti in range(T):
-    L = int(sys.stdin.readline())
-    val = list(map(int, sys.stdin.readline().strip().split()))
-    g = [[] for i in range(L+1)]
+for l in range(T):
+    N = int(sys.stdin.readline())
+    PI = list(map(int, sys.stdin.readline().strip().split()))
 
-    for i in range(1, L+1):
-        v = val[i-1]
-        g[i].append(v)
-        g[v].append(i)
+    P = [[] for i in range(N + 1)]
 
-    visited = [0 for i in range(L+1)]
+    for i in range(1, N + 1):
+        tv = PI[i - 1]
+        P[i].append(tv)
+        if tv == i:
+            continue
+        P[tv].append(i)
+
+    visited = [False for i in range(N + 1)]
     res = 0
-    for i in range(1, L+1):
-        if visited[i] == 0:
-            visited[i] = 1
+
+    for i in range(1, N + 1):
+        if not visited[i]:
             dstack = [i]
-            res += 1
+            visited[i] = True
 
             while dstack:
                 cur = dstack.pop()
-                for j in g[cur]:
-                    if visited[j] == 0:
-                        visited[j] = 1
+
+                if len(P[cur]) == 1 and P[cur][0] == cur:
+                    res += 1
+                    break
+
+                for j in P[cur]:
+                    if not visited[j]:
+                        visited[j] = True
                         dstack.append(j)
+                    elif j in dstack:
+                        res += 1
 
     print(res)
