ab153988e95332c6aa649ae405b5ce32511c2a7f
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-26 08:58:29 +0900

    Review BOJ-9466

--- Algorithms/GraphTheory/BOJ-9466.py
+++ Algorithms/GraphTheory/BOJ-9466.py
@@ -1,30 +1,30 @@
-import sys
-sys.setrecursionlimit(111111)
+from sys import stdin
 
+T = int(stdin.readline())
 
-def dfs(x):
-    global result
-    visited[x] = True
-    cycle.append(x)  # 사이클을 이루는 팀을 확인하기 위함
-    number = numbers[x]
+for _ in range(T):
+    n = int(stdin.readline())
+    st = [0] + list(map(int, stdin.readline().strip().split()))
+    visited = [False] * (n+1)
+    res = []
 
-    if visited[number]:  # 방문가능한 곳이 끝났는지
-        if number in cycle:  # 사이클 가능 여부
-            result += cycle[cycle.index(number):] # 사이클 되는 구간 부터만 팀을 이룸
-        return
-    else:
-        dfs(number)
-
-
-for _ in range(int(input())):
-    N = int(input())
-    numbers = [0] + list(map(int, input().split()))
-    visited = [True] + [False] * N  # 방문 여부
-    result = []
-
-    for i in range(1, N + 1):
-        if not visited[i]:  # 방문 안한 곳이라면
+    for i in range(1, n+1):
+        if not visited[i]:
+            dstack = [i]
             cycle = []
-            dfs(i)  # DFS 함수 돌림
 
-    print(N - len(result))  # 팀에 없는 사람 수
+            while dstack:
+                cur = dstack.pop()
+                visited[cur] = True
+                nexts = st[cur]
+                cycle.append(cur)
+
+                if visited[nexts]:
+                    if nexts in cycle:
+                        cdx = cycle.index(nexts)
+                        res += cycle[cdx:]
+                        break
+                else:
+                    dstack.append(nexts)
+
+    print(n-len(res))
