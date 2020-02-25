215c319352caaca59c5754c4d4619ba3aef2287d
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-25 13:51:30 +0900

    Review BOJ-1707

--- Algorithms/GraphTheory/BOJ-1707.py
+++ Algorithms/GraphTheory/BOJ-1707.py
@@ -2,36 +2,33 @@ from sys import stdin
 
 K = int(stdin.readline())
 
-for t in range(K):
-    V, E = map(int, stdin.readline().strip().split())
-    g = [[] for l in range(V + 1)]
+for l in range(K):
+    V, E = map(int, stdin.readline().split())
+    g = [[] for i in range(V + 1)]
 
-    for j in range(E):
-        A, B = map(int, stdin.readline().strip().split())
+    for i in range(E):
+        A, B = map(int, stdin.readline().split())
         g[A].append(B)
         g[B].append(A)
 
-    res = 0
-    visited = [None for l in range(V + 1)]
+    visited = [False for i in range(V + 1)]
     color = ['R', 'G']
-
+    res = 0
     for i in range(1, V + 1):
         if not visited[i]:
+            visited[i] = color[int(i % 2)]
             dstack = [i]
-            visited[i] = color[0]
 
             while dstack:
                 cur = dstack.pop()
-                ci = color.index(visited[cur])
-                tc = color[int((ci+1) % 2)]
+                cdx = color.index(visited[cur])
                 for j in g[cur]:
                     if not visited[j]:
+                        visited[j] = color[int((cdx + 1) % 2)]
                         dstack.append(j)
-                        visited[j] = tc
-                    elif visited[j] != tc:
+                    elif visited[j] != color[int((cdx + 1) % 2)]:
                         res = 1
                         break
                 if res:
                     break
-
     print(["YES", "NO"][res])
