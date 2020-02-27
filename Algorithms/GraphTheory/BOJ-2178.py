36f14cca824115843af7009394498ac16aeb35cc
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-27 16:17:14 +0900

    Review BOJ-2178

--- Algorithms/GraphTheory/BOJ-2178.py
+++ Algorithms/GraphTheory/BOJ-2178.py
@@ -2,25 +2,28 @@ from sys import stdin
 from collections import deque
 
 N, M = map(int, stdin.readline().split())
-G = [[] for _ in range(N+2)]
-G[0] = [-1] * (M+2)
-G[-1] = [-1] * (M+2)
+MAP = [[] for _ in range(N+2)]
+MAP[0] = [-1] * (M+2)
+MAP[-1] = [-1] * (M+2)
+q = deque()
 P = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
 for i in range(1, N+1):
-    G[i] = [-1] + list(map(int, list(stdin.readline().strip()))) + [-1]
+    MAP[i] = [-1] + list(map(int, list(stdin.readline().strip()))) + [-1]
 
-route = deque()
-route.append([1, 1, 1])
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        if MAP[i][j] == 1:
+            q.append([i, j, 1])
 
-while len(route):
-    ci, cj, d = route.popleft()
+            while len(q):
+                ci, cj, d = q.popleft()
 
-    for ii, jj in P:
-        ti = ii + ci
-        tj = jj + cj
-        if G[ti][tj] == 1:
-            G[ti][tj] = d + 1
-            route.append([ti, tj, d+1])
+                for ii, jj in P:
+                    ti = ii + ci
+                    tj = jj + cj
+                    if MAP[ti][tj] == 1:
+                        MAP[ti][tj] = d + 1
+                        q.append([ti, tj, d+1])
 
-print(G[N][M])
+print(MAP[N][M])
