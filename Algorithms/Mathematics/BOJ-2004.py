b0cc80d2f53d37e37f2276378dad095e9271e90c
Claudia J. Kang <claudiajkang@gmail.com>
2020-02-27 14:29:17 +0900

    Review BOJ-2004

--- Algorithms/Mathematics/BOJ-2004.py
+++ Algorithms/Mathematics/BOJ-2004.py
@@ -4,20 +4,17 @@ def count(n, s):
     if not n:
         return 0
     r = n // s
-    i = s*s
+    i = s * s
+
     while i <= n:
         r += (n // i)
         i *= s
+
     return r
 
 n, m = map(int, stdin.readline().split())
 
-five = count(n, 5)
-five -= count(n-m, 5)
-five -= count(m, 5)
-
-two = count(n, 2)
-two -= count(n-m, 2)
-two -= count(m, 2)
+five = count(n, 5) - (count(m, 5) + count(n-m, 5))
+two = count(n, 2) - (count(m, 2) + count(n-m, 2))
 
 print(min(five, two))
