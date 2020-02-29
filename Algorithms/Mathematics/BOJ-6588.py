3a53e38c71c3a29dbbb8546852b18ed858d4c9bb
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-01 08:36:44 +0900

    Review BOJ-6588

--- Algorithms/Mathematics/BOJ-6588.py
+++ Algorithms/Mathematics/BOJ-6588.py
@@ -1,31 +1,24 @@
 from sys import stdin
+  
+MN = 1000000
+PN = [True] * (MN+1)
+PN[0], PN[1] = False, False
 
-MAX = 1000000
-PN = [False, False] + [True for i in range(2, MAX+1)]
-
-for i in range(2, MAX+1):
-    if pow(i, 2) > MAX:
+for i in range(2, MN+1):
+    if i * i > MN:
         break
-    if PN[i]:
-        v = MAX // i
-        for j in range(2, v+1):
-            PN[i*j] = False
+    else:
+        for j in range(2, MN // i + 1):
+            PN[i * j] = False
+        if i % 2 == 0: PN[i] = False
 
-        if i % 2 == 0:
-            PN[i] = False
 
 for l in stdin:
     l = int(l)
     if l == 0:
         break
 
-    A = -1
     for i in range(l, 2, -1):
         if PN[i] and PN[l-i]:
-            A = i
+            print("%d = %d + %d" % (l, l-i, i))
             break
-
-    if A == -1:
-        print("Goldbach's conjecture is wrong.")
-    else:
-        print("%d = %d + %d" % (l, l-A, A))
