57cd7e5717a36a5f85452a0d09c272b302c81208
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-01 10:01:13 +0900

    Review BOJ-7576

--- Algorithms/GraphTheory/BOJ-7576.py
+++ Algorithms/GraphTheory/BOJ-7576.py
@@ -2,45 +2,41 @@ from sys import stdin
 from collections import deque
 
 M, N = map(int, stdin.readline().split())
-BOX = [[] for i in range(N+2)]
-BOX[0] = [-1] * (M+2)
-BOX[-1] = [-1] * (M+2)
-day = 0
+B = [[] for _ in range(N+2)]
+res = 0
+P = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 tomato = deque()
-Point = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-tsum = 0
+at = 0
 
 for i in range(1, N+1):
-    val = list(map(int, stdin.readline().split()))
-    tsum += sum(val)
-    BOX[i] = [-1] + val + [-1]
-    if val.count(1) > 0:
-        for j in range(1, M+1):
-            if BOX[i][j] == 1:
-                tomato.append([i, j, 1])
+    v = list(map(int, stdin.readline().split()))
+    at += v.count(1)
+    B[i] = [-1] + v + [-1]
+    if v.count(1) > 0:
+        for j in range(M):
+            if v[j] == 1:
+                tomato.append([i, j+1, 0])
 
-if tsum == (N*M):
+if at == (M*N):
     print(0)
 
-else:
-    while tomato:
-        ci, cj, d = tomato.popleft()
+B[0] = [-1] * (M+2)
+B[-1] = [-1] * (M+2)
 
-        for ii, jj in Point:
-            ti = ci + ii
-            tj = cj + jj
-            if BOX[ti][tj] == 0:
-                BOX[ti][tj] = d + 1
-                day = max(day, d + 1)
-                tomato.append([ti, tj, d + 1])
 
-    for i in range(1, N+1):
-        if BOX[i].count(0) >= 1:
-            day = -1
-            break
+while len(tomato):
+    ci, cj, cd = tomato.popleft()
+    for ii, jj in P:
+        ti = ci + ii
+        tj = cj + jj
+        if B[ti][tj] == 0:
+            B[ti][tj] = cd + 1
+            tomato.append([ti, tj, cd + 1])
+            res = max(res, cd + 1)
 
-    if day > 0:
-        day -= 1
+for i in range(1, N+1):
+    if B[i][1:M+1].count(0):
+        res = -1
 
-    print(day)
+print(res)
 
