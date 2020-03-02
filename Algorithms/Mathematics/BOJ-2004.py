7ac091f06fd2d041c3cc9733ffbc898b12b79d28
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-02 14:14:39 +0900

    Review BOJ-2004

--- Algorithms/Mathematics/BOJ-2004.py
+++ Algorithms/Mathematics/BOJ-2004.py
@@ -1,5 +1,5 @@
 from sys import stdin
-  
+
 def count(n, s):
     c = 0
 
@@ -9,9 +9,10 @@ def count(n, s):
 
     return c
 
+
 N, M = map(int, stdin.readline().split())
 
-f = count(N, 5) - (count(M, 5) + count(N-M, 5))
-t = count(N, 2) - (count(M, 2) + count(N-M, 2))
+five = count(N, 5) - count(N-M, 5) - count(M, 5)
+two = count(N, 2) - count(N-M, 2) - count(M, 2)
 
-print(min(f,t))
+print(min(five, two))
