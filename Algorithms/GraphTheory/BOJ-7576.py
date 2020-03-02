7bf94b69f5e4a82619e1a8d505772da9695af2a5
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-02 16:27:26 +0900

    Review BOJ-7576

--- Algorithms/GraphTheory/BOJ-7576.py
+++ Algorithms/GraphTheory/BOJ-7576.py
@@ -1,42 +1,44 @@
 from sys import stdin
 from collections import deque
 
-M, N = map(int, stdin.readline().split())
-B = [[] for _ in range(N+2)]
-res = 0
-P = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-tomato = deque()
-at = 0
+read = lambda: stdin.readline().rstrip()
 
-for i in range(1, N+1):
-    v = list(map(int, stdin.readline().split()))
-    at += v.count(1)
-    B[i] = [-1] + v + [-1]
-    if v.count(1) > 0:
-        for j in range(M):
-            if v[j] == 1:
-                tomato.append([i, j+1, 0])
+M, N = map(int, read().split())
+TOMA = [[-1] * (M + 2) for _ in range(N + 2)]
+q = deque()
+ALLTOMA = 0
+POINT = [[0, -1], [0, 1], [1, 0], [-1, 0]]
 
-if at == (M*N):
+for i in range(1, N + 1):
+    T = list(map(int, read().split()))
+    TOMA[i] = [-1] + T + [-1]
+    if T.count(1) > 0:
+        ALLTOMA += T.count(1)
+        for j in range(1, M + 1):
+            if TOMA[i][j] == 1:
+                q.append([i, j, 0])
+
+if ALLTOMA == (M * N):
     print(0)
 
-B[0] = [-1] * (M+2)
-B[-1] = [-1] * (M+2)
+else:
+    days = 0
 
+    while len(q):
+        ci, cj, cd = q.popleft()
 
-while len(tomato):
-    ci, cj, cd = tomato.popleft()
-    for ii, jj in P:
-        ti = ci + ii
-        tj = cj + jj
-        if B[ti][tj] == 0:
-            B[ti][tj] = cd + 1
-            tomato.append([ti, tj, cd + 1])
-            res = max(res, cd + 1)
+        for ii, jj in POINT:
+            ti = ii + ci
+            tj = jj + cj
+            if TOMA[ti][tj] == 0:
+                TOMA[ti][tj] = cd + 1
+                q.append([ti, tj, cd + 1])
+                days = max(days, cd + 1)
 
-for i in range(1, N+1):
-    if B[i][1:M+1].count(0):
-        res = -1
+    for i in range(1, N + 1):
+        if TOMA[i].count(0) > 0:
+            days = -1
+            break
 
-print(res)
+    print(days)
 
