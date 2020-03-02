c9a36ebbb8d7b8821dd09e0787e9e4813b1676ee
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-03 06:24:56 +0900

    Review BOJ-1167

--- DataStructures/Trees/BOJ-1167.py
+++ DataStructures/Trees/BOJ-1167.py
@@ -4,37 +4,34 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 
-def dfs(g, start, visited):
+def dfs(g, start):
+    visited = [0] * (len(g))
     q = deque()
     q.append([start, 0])
-    visited[start] = 0
     md = 0
+    visited[start] = 1
 
     while len(q):
-        ci, cd = q.popleft()
+        cur, cd = q.popleft()
 
-        for i, d in g[ci]:
-            if visited[i] == -1:
-                visited[i] = cd + d
-                md = max(md, cd + d)
-                q.append([i, cd + d])
+        for ti, td in g[cur]:
+            if not visited[ti]:
+                visited[ti] = cd + td
+                md = max(md, cd + td)
+                q.append([ti, cd + td])
 
     return visited.index(md), md
 
 
 V = int(read())
 G = [[] for _ in range(V + 1)]
-visited = [-1] * (V + 1)
 
 for i in range(1, V + 1):
     T = list(map(int, read().split()[:-1]))
     for j in range(1, len(T), 2):
         G[T[0]].append([T[j], T[j + 1]])
 
-fmidx, fmval = dfs(G, 1, visited)
+first_idx, first_val = dfs(G, 1)
+second_idx, second_val = dfs(G, first_idx)
 
-visited = [-1] * (V + 1)
-
-tmidx, tmval = dfs(G, fmidx, visited)
-
-print(tmval)
+print(second_val)
