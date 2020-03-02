c584ae6a7f5c8be36182ab0931f49874bc866953
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-02 14:49:56 +0900

    Review BOJ-11725

--- DataStructures/Trees/BOJ-11725.py
+++ DataStructures/Trees/BOJ-11725.py
@@ -1,24 +1,28 @@
 from sys import stdin
 from collections import deque
 
-N = int(stdin.readline())
+read = lambda : stdin.readline().strip()
+
+N = int(read())
 G = [[] for i in range(N+1)]
-res = ['0' for _ in range(N+1)]
-q = deque()
-bfs = []
+P = ['-1'] * (N+1)
 
 for i in range(1, N):
-    A, B = list(map(int, stdin.readline().split()))
+    A, B = map(int, read().split())
     G[A].append(B)
     G[B].append(A)
 
-q.append(1)
+for i in range(1, N+1):
+    if P[i] == '-1':
+        q = deque()
+        q.append(i)
 
-while len(q):
-    cur = q.popleft()
-    for i in G[cur]:
-        if res[i] == '0':
-            res[i] = str(cur)
-            q.append(i)
+        while len(q):
+            cur = q.popleft()
 
-print('\n'.join(res[2:]))
+            for j in G[cur]:
+                if P[j] == '-1':
+                    P[j] = str(cur)
+                    q.append(j)
+
+print('\n'.join(P[2:]))
