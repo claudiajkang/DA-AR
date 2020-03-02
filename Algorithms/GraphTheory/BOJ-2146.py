f1f312d41f3df76b37e76dcccbad6087d7fd2ddb
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-03 08:14:36 +0900

    Review BOJ-2146

--- Algorithms/GraphTheory/BOJ-2146.py
+++ Algorithms/GraphTheory/BOJ-2146.py
@@ -1,31 +1,31 @@
 from sys import stdin
 from collections import deque
 read = lambda : stdin.readline().rstrip()
+qu = lambda : deque()
 
 N = int(read())
 MAP = [[-1] * (N+2) for _ in range(N+2)]
-LAND = [[-1] + [0] * (N+1) + [-1] for _ in range(N+2)]
-LAND[0] = [-1] * (N+2)
-LAND[-1] = [-1] * (N+2)
-POINT = [[0, -1], [0, 1], [1, 0], [-1, 0]]
-bq = []
+LAND = [[-1] + [0] * N + [-1] for _ in range(N+2)]
+BOUND = qu()
+POINT = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
 for i in range(1, N+1):
-    MAP[i] = [-1] + list(map(int, read().split())) + [-1]
-    for j in range(2, N+1):
-        if [MAP[i][j-1], MAP[i][j]] == [1, 0]:
-            bq.append([i, j-1])
-        elif [MAP[i][j-1], MAP[i][j]] == [0, 1]:
-            bq.append([i, j])
+    T = list(map(int, read().split()))
+    MAP[i] = [-1] + T + [-1]
+    for j in range(1, N+1):
+        if [MAP[i][j-1], MAP[i][j]] == [0, 1]:
+            BOUND.append([i, j])
+        elif [MAP[i][j], MAP[i][j+1]] == [1, 0]:
+            BOUND.append([i, j])
 
-ln = 0
+LAND_NUM = 0
 for i in range(1, N+1):
     for j in range(1, N+1):
         if MAP[i][j] == 1 and LAND[i][j] == 0:
-            q = deque()
+            LAND_NUM += 1
+            LAND[i][j] = LAND_NUM
+            q = qu()
             q.append([i, j])
-            ln += 1
-            LAND[i][j] = ln
 
             while len(q):
                 ci, cj = q.popleft()
@@ -34,42 +34,38 @@ for i in range(1, N+1):
                     ti = ci + ii
                     tj = cj + jj
                     if MAP[ti][tj] == 1 and LAND[ti][tj] == 0:
-                        LAND[ti][tj] = ln
+                        LAND[ti][tj] = LAND_NUM
                         q.append([ti, tj])
 
-lq = [[] for _ in range(ln+1)]
 
-for i, j in bq:
+lq = [[] for _ in range(LAND_NUM+1)]
+
+for i, j in BOUND:
     idx = LAND[i][j]
-    lq[idx].append([i, j, 0])
+    lq[idx].append([i, j])
 
-res = [200 for _ in range(ln+1)]
+res = [10000 for _ in range(LAND_NUM+1)]
 
-for i in range(1, ln+1):
-    q = deque()
-    DIS = [[0] * (N+2) for _ in range(N+2)]
-    end = 0
+for i in range(1, LAND_NUM+1):
+    q = qu()
+    DIST = [[0] * (N + 2) for _ in range(N + 2)]
 
-    for ii, jj, dd in lq[i]:
-        q.append([ii, jj, dd])
+    for ii, jj in lq[i]:
+        q.append([ii, jj, 0])
 
-    while len(q):
+    while len(q) and res[i] == 10000:
         ci, cj, cd = q.popleft()
-        ln = LAND[ci][cj]
 
         for ii, jj in POINT:
             ti = ci + ii
             tj = cj + jj
-            if MAP[ti][tj] == 0:
-                if DIS[ti][tj] == 0:
-                    DIS[ti][tj] = cd + 1
-                    q.append([ti, tj, cd+1])
-            elif MAP[ti][tj] == 1:
-                if LAND[ti][tj] != i:
-                    res[i] = min(res[i], cd)
-                    break
 
-        if res[i] != 200:
-            break
+            if MAP[ti][tj] == 0 and DIST[ti][tj] == 0:
+                DIST[ti][tj] = cd + 1
+                q.append([ti, tj, cd + 1])
+
+            elif MAP[ti][tj] == 1 and LAND[ti][tj] != i and DIST[ti][tj] == 0:
+                res[i] = cd
+                break
 
 print(min(res))
