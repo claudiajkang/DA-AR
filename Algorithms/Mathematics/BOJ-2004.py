93865ff644b122c3179aa6cd3fa8250e82147da8
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-03 06:13:18 +0900

    Review BOJ-2004

--- Algorithms/Mathematics/BOJ-2004.py
+++ Algorithms/Mathematics/BOJ-2004.py
@@ -1,4 +1,5 @@
 from sys import stdin
+read = lambda : stdin.readline().rstrip()
 
 def count(n, s):
     c = 0
@@ -9,10 +10,9 @@ def count(n, s):
 
     return c
 
+N, M = map(int, read().split())
 
-N, M = map(int, stdin.readline().split())
-
-five = count(N, 5) - count(N-M, 5) - count(M, 5)
-two = count(N, 2) - count(N-M, 2) - count(M, 2)
+five = count(N, 5) - count(M, 5) - count(N-M, 5)
+two = count(N, 2) - count(M, 2) - count(N-M, 2)
 
 print(min(five, two))
