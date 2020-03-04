1f3d167fbcd54c8b5bed5853fec29fa67ac2a06a
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-05 08:54:26 +0900

    Review BOJ-2156

--- Algorithms/DynamicProgramming/BOJ-2156.py
+++ Algorithms/DynamicProgramming/BOJ-2156.py
@@ -1,25 +1,22 @@
 from sys import stdin
 read = lambda : int(stdin.readline())
 
-N = read()
-DP = [0 for _ in range(10001)]
-G = [0 for _ in range(10001)]
+n = read()
+g = [0 for _ in range(n+1)]
 
-for i in range(1, N+1):
-    G[i] = read()
+for i in range(1, n+1):
+    g[i] = read()
 
-if 1 <= N <= 10000:
-    DP[1] = G[1]
-    DP[2] = max(G[2], DP[1]+G[2])
+if 1 <= n <= 10000:
+    dp = [0 for _ in range(10001)]
+    dp[1] = g[1]
+    for i in range(2, n+1):
+        if i == 2:
+            dp[2] = max(dp[1] + g[2], g[2])
+        else:
+            dp[i] = max(dp[i-1], g[i] + dp[i-2], g[i] + g[i-1] + dp[i-3])
 
-    for i in range(3, N+1):
-        V1 = DP[i-1]
-        V2 = G[i] + DP[i-2]
-        V3 = G[i] + G[i-1] + DP[i-3]
-
-        DP[i] = max(V1, V2, V3)
-
-    print(DP[N])
+    print(dp[n])
 
 else:
     print(0)
