0c155c3dfa41468dc733183574fc4006c2fcc9dc
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-04 09:53:26 +0900

    Review BOJ-2146

--- Algorithms/GraphTheory/BOJ-2146.py
+++ Algorithms/GraphTheory/BOJ-2146.py
@@ -1,71 +1,70 @@
 from sys import stdin
 from collections import deque
 read = lambda : stdin.readline().rstrip()
-qu = lambda : deque()
 
 N = int(read())
-MAP = [[-1] * (N+2) for _ in range(N+2)]
+MAP = [[-1]*(N+2) for _ in range(N+2)]
 LAND = [[-1] + [0] * N + [-1] for _ in range(N+2)]
-BOUND = qu()
+BOUND = deque()
 POINT = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
 for i in range(1, N+1):
-    T = list(map(int, read().split()))
-    MAP[i] = [-1] + T + [-1]
+    MAP[i] = [-1] + list(map(int, read().split())) + [-1]
     for j in range(1, N+1):
-        if [MAP[i][j-1], MAP[i][j]] == [0, 1]:
+        if [MAP[i][j], MAP[i][j+1]] == [1, 0]:
             BOUND.append([i, j])
-        elif [MAP[i][j], MAP[i][j+1]] == [1, 0]:
+        elif [MAP[i][j-1], MAP[i][j]] == [0, 1]:
             BOUND.append([i, j])
 
-LAND_NUM = 0
+ln = 0
 for i in range(1, N+1):
     for j in range(1, N+1):
         if MAP[i][j] == 1 and LAND[i][j] == 0:
-            LAND_NUM += 1
-            LAND[i][j] = LAND_NUM
-            q = qu()
+            ln += 1
+            LAND[i][j] = ln
+            q = deque()
             q.append([i, j])
 
             while len(q):
                 ci, cj = q.popleft()
 
                 for ii, jj in POINT:
-                    ti = ci + ii
-                    tj = cj + jj
+                    ti = ii + ci
+                    tj = jj + cj
                     if MAP[ti][tj] == 1 and LAND[ti][tj] == 0:
-                        LAND[ti][tj] = LAND_NUM
+                        LAND[ti][tj] = ln
                         q.append([ti, tj])
 
+BLAND = [[] for _ in range(ln+1)]
+RES = [10000 for _ in range(ln+1)]
 
-lq = [[] for _ in range(LAND_NUM+1)]
+for ii, jj in BOUND:
+    BLAND[LAND[ii][jj]].append([ii, jj, 0])
 
-for i, j in BOUND:
-    idx = LAND[i][j]
-    lq[idx].append([i, j])
 
-res = [10000 for _ in range(LAND_NUM+1)]
+for i in range(1, ln+1):
+    q = deque()
 
-for i in range(1, LAND_NUM+1):
-    q = qu()
-    DIST = [[0] * (N + 2) for _ in range(N + 2)]
+    for ii, jj, dd in BLAND[i]:
+        q.append([ii, jj, dd])
 
-    for ii, jj in lq[i]:
-        q.append([ii, jj, 0])
+    DIS = [[-1] + [0] * N + [-1] for _ in range(N+2)]
 
-    while len(q) and res[i] == 10000:
+    while len(q):
         ci, cj, cd = q.popleft()
 
         for ii, jj in POINT:
             ti = ci + ii
             tj = cj + jj
-
-            if MAP[ti][tj] == 0 and DIST[ti][tj] == 0:
-                DIST[ti][tj] = cd + 1
+            if MAP[ti][tj] == 0 and DIS[ti][tj] == 0:
+                DIS[ti][tj] = cd + 1
                 q.append([ti, tj, cd + 1])
 
-            elif MAP[ti][tj] == 1 and LAND[ti][tj] != i and DIST[ti][tj] == 0:
-                res[i] = cd
+            elif MAP[ti][tj] == 1 and LAND[ti][tj] != i:
+                RES[i] = cd
                 break
 
-print(min(res))
+        if RES[i] != 10000:
+            break
+
+print(min(RES))
