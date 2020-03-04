717a840388dc1d540de999a6f0e0dfdb84a1b002
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-04 11:28:08 +0900

    Review BOJ-2193

--- Algorithms/DynamicProgramming/BOJ-2193.py
+++ Algorithms/DynamicProgramming/BOJ-2193.py
@@ -3,8 +3,12 @@ from sys import stdin
 N = int(stdin.readline())
 DP = [0, 1, 1, 2] + [0 for _ in range(4, 91)]
 
-if 3 < N <= 90:
-    for i in range(4, N+1):
-        DP[i] = DP[i-2] + DP[i-1]
+if N > 90 or N < 1:
+    print(0)
 
-print(DP[N])
+else:
+    if N > 3:
+        for i in range(4, N+1):
+            DP[i] = DP[i-1] + DP[i-2]
+
+    print(DP[N])
