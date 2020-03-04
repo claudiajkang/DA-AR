a9d75c1613c25ea715f410d7248cfe1a7ef27be7
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-04 11:06:35 +0900

    Review BOJ-11057

--- Algorithms/DynamicProgramming/BOJ-11057.py
+++ Algorithms/DynamicProgramming/BOJ-11057.py
@@ -4,11 +4,11 @@ N = int(stdin.readline())
 DP = [[0] * 10 for _ in range(N+1)]
 DP[1] = [1] * 10
 
-for i in range(2, N+1):
-    DP[i][0] = DP[i-1][0]
-    DP[i][1] = DP[i][0] + DP[i-1][1]
+if N > 1:
+    for i in range(2, N+1):
+        DP[i][0] = DP[i-1][0]
 
-    for j in range(2, 10):
-        DP[i][j] = DP[i][j-1] + DP[i-1][j]
+        for j in range(1, 10):
+            DP[i][j] = DP[i-1][j] + DP[i][j-1]
 
 print(sum(DP[N]) % 10007)
