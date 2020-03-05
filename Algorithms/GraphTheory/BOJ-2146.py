838983e764ca0adc2772be657c7aa0638a8848b4
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-05 09:36:53 +0900

    Review BOJ-2146

--- Algorithms/GraphTheory/BOJ-2146.py
+++ Algorithms/GraphTheory/BOJ-2146.py
@@ -1,24 +1,26 @@
-from sys import stdin
 from collections import deque
-read = lambda : stdin.readline().rstrip()
+from sys import stdin
+read = lambda: stdin.readline().rstrip()
 
 N = int(read())
-MAP = [[-1]*(N+2) for _ in range(N+2)]
+MAP = [[-1] * (N+2) for _ in range(N+2)]
 LAND = [[-1] + [0] * N + [-1] for _ in range(N+2)]
-BOUND = deque()
 POINT = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+BOUND = deque()
 
-for i in range(1, N+1):
+for i in range(1, N + 1):
     MAP[i] = [-1] + list(map(int, read().split())) + [-1]
-    for j in range(1, N+1):
-        if [MAP[i][j], MAP[i][j+1]] == [1, 0]:
+
+    for j in range(1, N + 1):
+        if [MAP[i][j], MAP[i][j + 1]] == [1, 0]:
             BOUND.append([i, j])
-        elif [MAP[i][j-1], MAP[i][j]] == [0, 1]:
+
+        elif [MAP[i][j - 1], MAP[i][j]] == [0, 1]:
             BOUND.append([i, j])
 
 ln = 0
-for i in range(1, N+1):
-    for j in range(1, N+1):
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
         if MAP[i][j] == 1 and LAND[i][j] == 0:
             ln += 1
             LAND[i][j] = ln
@@ -29,26 +31,24 @@ for i in range(1, N+1):
                 ci, cj = q.popleft()
 
                 for ii, jj in POINT:
-                    ti = ii + ci
-                    tj = jj + cj
+                    ti = ci + ii
+                    tj = cj + jj
                     if MAP[ti][tj] == 1 and LAND[ti][tj] == 0:
                         LAND[ti][tj] = ln
                         q.append([ti, tj])
 
-BLAND = [[] for _ in range(ln+1)]
-RES = [10000 for _ in range(ln+1)]
+BLAND = [[] for _ in range(ln + 1)]
+RES = [10000 for _ in range(ln + 1)]
 
 for ii, jj in BOUND:
-    BLAND[LAND[ii][jj]].append([ii, jj, 0])
+    BLAND[LAND[ii][jj]].append([ii, jj])
 
-
-for i in range(1, ln+1):
+for i in range(1, ln + 1):
     q = deque()
+    DIS = [[-1] + [0] * N + [-1] for _ in range(N + 2)]
 
-    for ii, jj, dd in BLAND[i]:
-        q.append([ii, jj, dd])
-
-    DIS = [[-1] + [0] * N + [-1] for _ in range(N+2)]
+    for ti, tj in BLAND[i]:
+        q.append([ti, tj, 0])
 
     while len(q):
         ci, cj, cd = q.popleft()
@@ -56,6 +56,7 @@ for i in range(1, ln+1):
         for ii, jj in POINT:
             ti = ci + ii
             tj = cj + jj
+
             if MAP[ti][tj] == 0 and DIS[ti][tj] == 0:
                 DIS[ti][tj] = cd + 1
                 q.append([ti, tj, cd + 1])
