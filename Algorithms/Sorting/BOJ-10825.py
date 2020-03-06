2cf058472087ee849a38441460b69e8c0ffdffda
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-06 10:21:54 +0900

    Review BOJ-10825

--- Algorithms/Sorting/BOJ-10825.py
+++ Algorithms/Sorting/BOJ-10825.py
@@ -1,29 +1,17 @@
 from sys import stdin
 read = lambda : stdin.readline().rstrip()
 
-N = int(read())
+n = int(read())
+st = []
 
-KOR = {}
-ENG = {}
-MATH = {}
+for i in range(n):
+    name, k, e, m = read().split()
+    st.append((name, int(k), int(e), int(m)))
 
-for i in range(1, N+1):
-    na, ko, en, ma = read().split()
-    ko, en, ma = map(int, [ko, en, ma])
+st.sort(key=lambda item: item[0])
+st.sort(key=lambda item: item[3], reverse=True)
+st.sort(key=lambda item: item[2])
+st.sort(key=lambda item: item[1], reverse=True)
 
-    if ko not in KOR.keys():
-        KOR[ko] = {}
-
-    if en not in KOR[ko].keys():
-        KOR[ko][en] = {}
-
-    if ma not in KOR[ko][en].keys():
-        KOR[ko][en][ma] = []
-
-    KOR[ko][en][ma].append(na)
-
-for k in sorted(KOR.keys(), reverse=True):
-    for e in sorted(KOR[k]):
-        for m in sorted(KOR[k][e], reverse=True):
-            for n in sorted(KOR[k][e][m]):
-                print(n)
+for item in st:
+    print(item[0])
