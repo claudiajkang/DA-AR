86a3cd25c787e17f626ae6a6da8620aa3d74b276
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-07 05:59:33 +0900

    Review BOJ-1654

--- Algorithms/BinarySearch/BOJ-1654.py
+++ Algorithms/BinarySearch/BOJ-1654.py
@@ -1,5 +1,5 @@
 from sys import stdin
-read = lambda : stdin.readline().rstrip()
+read = lambda: stdin.readline().rstrip()
 
 K, N = map(int, read().split())
 L = [0] * K
@@ -7,7 +7,7 @@ L = [0] * K
 for i in range(K):
     L[i] = int(read())
 
-L = sorted(L, reverse=True)
+L = sorted(L)
 lo = 0
 hi = max(L)
 mid = 0
@@ -17,15 +17,15 @@ while lo <= hi:
     mid = (lo + hi) // 2
     if mid == 0:
         break
-    s = 0
 
+    s = 0
     for i in L:
         s += (i // mid)
 
-    if s < N:
-        hi = mid - 1
-    else:
+    if s >= N:
         lo = mid + 1
 
-print(hi)
+    else:
+        hi = mid - 1
 
+print(hi)
