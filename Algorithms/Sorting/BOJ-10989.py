1640f8638db1d139ab85d73d0f9df9f28f387f2d
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-06 11:09:34 +0900

    Review BOJ-10989

--- Algorithms/Sorting/BOJ-10989.py
+++ Algorithms/Sorting/BOJ-10989.py
@@ -2,13 +2,13 @@ from sys import stdin
 read = lambda : int(stdin.readline())
 
 N = read()
-PN = [0 for _ in range(10001)]
+PN = [0] * 10001
 
 for i in range(N):
-    V = read()
-    PN[V] += 1
+    v = read()
+    PN[v] += 1
 
 for i in range(1, 10001):
-    if PN[i] > 0:
+    if PN[i]:
         for j in range(PN[i]):
             print(i)
