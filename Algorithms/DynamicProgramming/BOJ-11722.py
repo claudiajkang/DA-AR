a29cf1e95e5703f252b8a53bbc40d210a9cae24b
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-07 21:04:26 +0900

    Add code that solved BOJ-11722 problem
    prob : https://www.acmicpc.net/problem/11722

---
+++ Algorithms/DynamicProgramming/BOJ-11722.py
@@ -0,0 +1,15 @@
+from sys import stdin
+read = lambda: stdin.readline().rstrip()
+
+N = int(read())
+A = list(map(int, read().split()))
+DP = [0] * N
+
+for i in range(N):
+    for j in range(i-1, -1, -1):
+        if A[j] > A[i]:
+            DP[i] = max(DP[i], DP[j])
+
+    DP[i] += 1
+
+print(max(DP))
