6da008e57d6476eb9fc76aad48f2590b408a2874
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-09 06:38:07 +0900

    Review BOJ-2805

--- Algorithms/BinarySearch/BOJ-2805.py
+++ Algorithms/BinarySearch/BOJ-2805.py
@@ -1,26 +1,30 @@
 from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
-N, M = map(int, read().split())
-T = list(map(int, read().split()))
-T.sort(reverse=True)
+n, m = map(int, read().split())
+t = list(map(int, read().split()))
+t.sort(reverse=True)
 
 lo = 0
-hi = max(T)
+hi = max(t)
 mid = 0
 
 while lo < hi:
     mid = (lo + hi) // 2
+
     s = 0
-    for i in T:
+    for i in t:
         if i > mid:
-            s += (i-mid)
+            s += (i - mid)
         else:
             break
-    if s < M:
+
+    if s < m:
         hi = mid
-    elif s == M or lo == mid:
+
+    elif s == m or lo == mid:
         break
+
     else:
         lo = mid
 
