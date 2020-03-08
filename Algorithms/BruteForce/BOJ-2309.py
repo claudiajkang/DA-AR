475c845c8d678c7fbc9be025ce5f658048b49464
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-08 09:06:29 +0900

    Review BOJ-2309

--- Algorithms/BruteForce/BOJ-2309.py
+++ Algorithms/BruteForce/BOJ-2309.py
@@ -1,26 +1,21 @@
 from sys import stdin
+read = lambda: int(stdin.readline().rstrip())
 
-H = [0] * 9
+H = [read() for _ in range(9)]
+H.sort()
 
-for i in range(9):
-    H[i] = int(stdin.readline())
-
-H = sorted(H)
-S = 100
-res = []
-diff = sum(H) - 100
+p = []
 
 for i in range(9):
     for j in range(9):
-        if i != j:
-            if H[i] + H[j] == diff:
-                res.append([i, j])
-                break
+        p.append([i, j])
 
-    if len(res) > 0:
-        for i, j in res:
-            for ii in range(9):
-                if ii not in [i, j]:
-                    print(H[ii])
-            break
+allsum = sum(H)
+for i, j in p:
+    r = allsum - H[i] - H[j]
+    if r == 100:
+        for ii in range(9):
+            if ii not in [i, j]:
+                print(H[ii])
+
         break
