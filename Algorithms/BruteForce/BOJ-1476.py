9eb7f6035c5035b6c7e00d36f0e49829071cd185
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-09 06:57:06 +0900

    Review BOJ-1476

--- Algorithms/BruteForce/BOJ-1476.py
+++ Algorithms/BruteForce/BOJ-1476.py
@@ -2,23 +2,23 @@ from sys import stdin
 
 e, s, m = map(int, stdin.readline().rstrip().split())
 
-te, ts, tm = 0, 0, 0
-c = 0
+if e == s and s == m:
+    print(s)
+    exit()
 
-while True:
-    if e == te and s == ts and m == tm:
-        break
+else:
+    te = ts = tm = 0
+    y = 0
 
-    te += 1
-    ts += 1
-    tm += 1
-    c += 1
+    while not (te == e and ts == s and tm == m):
+        te += 1
+        ts += 1
+        tm += 1
+        y += 1
 
-    if te > 15:
-        te = 1
-    if ts > 28:
-        ts = 1
-    if tm > 19:
-        tm = 1
+        if te > 15: te = 1
+        if ts > 28 : ts = 1
+        if tm > 19 : tm = 1
 
-print(c)
+    print(y)
+
