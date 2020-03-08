1d94f3d5068833f7c081b805f35eb0b09f808342
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-08 09:37:15 +0900

    Review BOJ-1629

--- Algorithms/DivideAndConquer/BOJ-1629.py
+++ Algorithms/DivideAndConquer/BOJ-1629.py
@@ -4,13 +4,14 @@ from sys import stdin
 def cal(a, b, c):
     if 0 <= b <= 1:
         return a % c
-    r = cal(a, b // 2, c)
-    if b % 2:
-        return (((r * r) % c) * a) % c
-    else:
+    if b % 2 == 0:
+        r = cal(a, b // 2, c)
         return (r * r) % c
+
+    else:
+        r = cal(a, b // 2, c)
+        return (((r * r) % c) * a) % c
 
 
 A, B, C = map(int, stdin.readline().rstrip().split())
-
 print(cal(A, B, C))
