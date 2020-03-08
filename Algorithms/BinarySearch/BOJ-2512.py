07405f00912ff31745f80f9d66d1af718a0076ac
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-08 09:04:55 +0900

    Review BOJ-2512

--- Algorithms/BinarySearch/BOJ-2512.py
+++ Algorithms/BinarySearch/BOJ-2512.py
@@ -1,32 +1,31 @@
 from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
-N = int(read())
-NMONEY = sorted(list(map(int, read().split())))
-M = int(read())
+n = int(read())
+nm = sorted(list(map(int, read().split())), reverse=True)
+m = int(read())
+
+if sum(nm) <= m:
+    print(max(nm))
+    exit()
 
 lo = 0
 hi = 10 ** 9
 mid = 0
 s = 0
 
-if sum(NMONEY) <= M:
-    print(max(NMONEY))
-
-else:
-    while lo <= hi:
-        mid = (lo + hi) // 2
-
-        s = 0
-        for i in NMONEY:
-            if i <= mid:
-                s += i
-            else:
-                s += mid
-
-        if s <= M:
-            lo = mid + 1
+while lo <= hi:
+    mid = (lo + hi) // 2
+    s = 0
+    for i in nm:
+        if i >= mid:
+            s += mid
         else:
-            hi = mid - 1
+            s += i
 
-    print(hi)
+    if s > m:
+        hi = mid - 1
+    else:
+        lo = mid + 1
+
+print(hi)
