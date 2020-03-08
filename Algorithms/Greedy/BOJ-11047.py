9a6797a356dfc13ba00daee6184d8cf9127b7fe6
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-09 06:47:04 +0900

    Review BOJ-11047

--- Algorithms/Greedy/BOJ-11047.py
+++ Algorithms/Greedy/BOJ-11047.py
@@ -5,15 +5,14 @@ n, k = map(int, read().split())
 a = [int(read()) for _ in range(n)]
 a.sort(reverse=True)
 
-c = 0
 r = k
+c = 0
 
 for i in a:
     if i <= r:
         c += (r // i)
-        r -= i * (r // i)
-
-    if r == 0:
-        break
+        r = (r % i)
+        if r == 0:
+            break
 
 print(c)
