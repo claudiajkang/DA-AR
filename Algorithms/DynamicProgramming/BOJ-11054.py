d2e02f18d0cc5a1bca186409bb38d424b8341c56
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-09 10:41:43 +0900

    Review BOJ-11054

--- Algorithms/DynamicProgramming/BOJ-11054.py
+++ Algorithms/DynamicProgramming/BOJ-11054.py
@@ -3,22 +3,24 @@ read = lambda: stdin.readline().rstrip()
 
 n = int(read())
 a = list(map(int, read().split()))
-b4 = [0] * n
-af = [0] * n
+
+up = [0] * n
+do = [0] * n
 dp = [0] * n
 
 for i in range(n):
     for j in range(i-1, -1, -1):
         if a[i] > a[j]:
-            b4[i] = max(b4[i], b4[j])
-    b4[i] += 1
+            up[i] = max(up[j], up[i])
+
+    up[i] += 1
 
 for i in range(n-1, -1, -1):
     for j in range(n-1, i, -1):
         if a[i] > a[j]:
-            af[i] = max(af[i], af[j])
+            do[i] = max(do[j], do[i])
 
-    af[i] += 1
-    dp[i] = b4[i] + af[i] - 1
+    do[i] += 1
+    dp[i] = up[i] + do[i] - 1
 
 print(max(dp))
