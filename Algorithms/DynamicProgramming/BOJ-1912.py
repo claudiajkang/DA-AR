d42de09669484425f8ed4b2d3a69d010e6278c00
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-09 12:09:41 +0900

    Review BOJ-1912

--- Algorithms/DynamicProgramming/BOJ-1912.py
+++ Algorithms/DynamicProgramming/BOJ-1912.py
@@ -2,13 +2,10 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
-a = list(map(int, read().split())) + [0]
-dp = [0] * n
+a = list(map(int, read().split()))
+dp = [a[0]] + [0] * (n-1)
 
-for i in range(n):
-    if i == 0:
-        dp[i] = a[i]
-    else:
-        dp[i] = max(dp[i-1] + a[i], a[i])
+for i in range(1, n):
+    dp[i] = max(dp[i-1] + a[i], a[i])
 
-print(max(dp))
+print(dp)
