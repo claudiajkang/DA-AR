258282428bb3b05e384dce347f44d09418b7a08c
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-09 12:33:37 +0900

    Review BOJ-11728

--- Algorithms/DivideAndConquer/BOJ-11728.py
+++ Algorithms/DivideAndConquer/BOJ-11728.py
@@ -2,7 +2,7 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n, m = map(int, read().split())
-nmlist = list(map(int, read().split())) + list(map(int, read().split()))
-print(' '.join(map(str, sorted(nmlist))))
-
-
+l = list(map(int, read().split())) + list(map(int, read().split()))
+l.sort()
+r = map(str, l)
+print(' '.join(r))
