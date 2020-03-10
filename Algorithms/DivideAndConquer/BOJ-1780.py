326a84c13aab78bc7af01125cfeb2d2bfa8fcba1
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-10 19:54:02 +0900

    Review BOJ-1780

--- Algorithms/DivideAndConquer/BOJ-1780.py
+++ Algorithms/DivideAndConquer/BOJ-1780.py
@@ -1,29 +1,28 @@
 import sys
 sys.setrecursionlimit(10**6)
 read = lambda : sys.stdin.readline().strip()
-cnt = [0]*3
+
+res = [0]*3
 n = int(read())
-matrix = [list(map(int, read().split())) for _ in range(n)]
+p = [list(map(int, read().split())) for _ in range(n)]
 
 
-def same(x, y, n):
-    for i in range(x, x+n):
-        for j in range(y, y+n):
-            if matrix[x][y] != matrix[i][j]:
+def same(x, y, diff):
+    for i in range(x, x+diff):
+        for j in range(y, y+diff):
+            if p[x][y] != p[i][j]:
                 return False
     return True
 
-
-def solve(x, y, n):
-    if same(x, y, n):
-        cnt[matrix[x][y]+1] += 1
+def solve(x, y, diff):
+    if same(x, y, diff):
+        res[p[x][y]+1] += 1
         return
-    m = n//3
+    d = diff//3
     for i in range(0, 3):
         for j in range(0, 3):
-            solve(x+i*m, y+j*m, m)
+            solve(x+i*d, y+j*d, d)
 
 
 solve(0, 0, n)
-for i in cnt:
-    print(i)
+print('\n'.join(map(str, res)))
