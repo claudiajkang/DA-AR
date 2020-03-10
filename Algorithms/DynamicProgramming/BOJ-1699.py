d7253c7501a75d11410a2948e6c1fa50481915e5
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-10 16:34:48 +0900

    Review BOJ-1699

--- Algorithms/DynamicProgramming/BOJ-1699.py
+++ Algorithms/DynamicProgramming/BOJ-1699.py
@@ -1,13 +1,25 @@
-N = int(input())
-dp = [0] * 100001
+from sys import stdin
+read = lambda: stdin.readline().rstrip()
 
-for i in range(1, N+1):
-    dp[i] = i
-    for j in range(1, i):
-        if (j * j) > i:
-            break
+n = int(read())
+un = []
+dp = [0] * (n+1)
 
-        if dp[i] > (dp[i-j*j] + 1):
-            dp[i] = dp[i-j*j] + 1
+for i in range(1, n+1):
+    if i*i > n:
+        break
+    un.append(i*i)
+    dp[i*i] = 1
 
-print(dp[N])
+un.sort()
+
+for i in range(1, n+1):
+    if not dp[i]:
+        dp[i] = dp[i-1] + 1
+        for j in un:
+            if i < j:
+                break
+            if dp[j] + dp[i-j] < dp[i]:
+                dp[i] = dp[j] + dp[i-j]
+
+print(dp[n])
