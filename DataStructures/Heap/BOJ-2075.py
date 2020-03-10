4bf2e0fd5d8b1749293fdfe280d54aea6b3d911f
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-11 06:27:15 +0900

    Review BOJ-2075

--- DataStructures/Heap/BOJ-2075.py
+++ DataStructures/Heap/BOJ-2075.py
@@ -3,18 +3,15 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
-h = []
+a = []
 
-mn = 0
 for i in range(n):
     v = list(map(int, read().split()))
-    if i == 0:
-        mn = max(v)
 
     for j in v:
-        heapq.heappush(h, int(j))
+        heapq.heappush(a, j)
 
-        if len(h) > n:
-            v = heapq.heappop(h)
+        if len(a) > n:
+            heapq.heappop(a)
 
-print(heapq.heappop(h))
+print(heapq.heappop(a))
