d5c8eef42d0794eede71009f1fb68270e915c049
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-11 13:39:42 +0900

    Review BOJ-2110

--- Algorithms/BinarySearch/BOJ-2110.py
+++ Algorithms/BinarySearch/BOJ-2110.py
@@ -1,17 +1,20 @@
 from sys import stdin
+
 read = lambda: stdin.readline().rstrip()
 
-def rc(dis):
+
+def count(d):
     global h, n
-    count = 1
+
     cur = h[0]
-
+    c = 1
     for i in range(1, n):
-        if cur + dis <= h[i]:
-            count += 1
+        if cur + d <= h[i]:
             cur = h[i]
+            c += 1
 
-    return count
+    return c
+
 
 n, c = map(int, read().split())
 h = [int(read()) for _ in range(n)]
@@ -22,15 +25,13 @@ end = h[-1]
 res = 0
 
 while start <= end:
-    distance = (start + end) // 2
-    r = rc(distance)
+    dis = (start + end) // 2
+    r = count(dis)
 
     if r >= c:
-        res = distance
-        start = distance + 1
-
+        res = dis
+        start = dis + 1
     else:
-        end = distance - 1
+        end = dis - 1
 
 print(res)
-
