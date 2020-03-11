08707e846322dff198c9eb52bfeebc72ebb9b79a
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-11 11:55:56 +0900

    Review BOJ-2133

--- Algorithms/DynamicProgramming/BOJ-2133.py
+++ Algorithms/DynamicProgramming/BOJ-2133.py
@@ -1,18 +1,19 @@
 from sys import stdin
 
-n = int(stdin.readline())
+n = int(stdin.readline().rstrip())
+
 dp = [1] + [0] * n
 
-if n < 2:
-    print(0)
-    exit()
-
-dp[2] = 3
-for i in range(3, n+1):
+for i in range(1, n+1):
     if i % 2 == 1:
         continue
+
+    if i == 2:
+        dp[i] = 3
+        continue
+
     dp[i] += dp[i-2] * dp[2]
-    for j in range(0, i-3):
+    for j in range(i-3):
         dp[i] += dp[j] * 2
 
 print(dp[n])
