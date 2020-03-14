8b05812b0ffee155e061f47078caf58d562d1510
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-14 12:08:06 +0900

    Review BOJ-11052

--- Algorithms/DynamicProgramming/BOJ-11052.py
+++ Algorithms/DynamicProgramming/BOJ-11052.py
@@ -6,15 +6,11 @@ p = [0] + list(map(int, read().split()))
 dp = [0] * (n+1)
 
 for i in range(1, n+1):
-    if i == 1:
-        dp[i] = p[i]
-
-    else:
-        dp[i] = p[i]
-        for j in range(i-1, 0, -1):
-            if i % j == 0:
-                dp[i] = max(dp[i], (i//j)*dp[j])
-
+    dp[i] = p[i]
+    for j in range(i-1, 0, -1):
+        if i % j == 0:
+            dp[i] = max(dp[i], (i//j)*dp[j], dp[j] + dp[i-j])
+        else:
             dp[i] = max(dp[i], dp[j] + dp[i-j])
 
 print(dp[n])
