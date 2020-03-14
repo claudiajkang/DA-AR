739f6182a6f643bb5421cf901ed7e2be9d95d32b
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-14 10:37:52 +0900

    Reivew BOJ-1725

--- DataStructures/Stacks/BOJ-1725.py
+++ DataStructures/Stacks/BOJ-1725.py
@@ -2,15 +2,16 @@ from sys import stdin
 read = lambda: int(stdin.readline().rstrip())
 
 n = read()
-h = [read() for _ in range(n)] + [0]
-
+h = [read() for i in range(n)] + [0]
+stack = [0]
 res = 0
-stack = []
-for i in range(n + 1):
-    while stack and h[stack[-1]] >= h[i]:
-        j = stack.pop()
+
+for i in range(1, n+1):
+    while stack and h[stack[-1]] > h[i]:
+        c = stack.pop()
         wd = i - stack[-1] - 1 if stack else i
-        res = max(res, h[j] * wd)
+        res = max(res, h[c] * wd)
+
     stack.append(i)
 
 print(res)
