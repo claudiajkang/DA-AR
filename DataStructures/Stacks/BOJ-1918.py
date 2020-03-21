7a30ace7a4510d6e5890bbe847eb3caf4bd52c95
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-22 05:50:24 +0900

    Review BOJ-1918

--- DataStructures/Stacks/BOJ-1918.py
+++ DataStructures/Stacks/BOJ-1918.py
@@ -1,24 +1,24 @@
 from sys import stdin
 
-v = '('+stdin.readline().rstrip()+')'
-cal = {'(': 0, '+': 1, '-': 1, '*': 2, '/': 2}
+exp = '(' + stdin.readline().rstrip() + ')'
 stack = []
+cal = {'(': 0, '+': 1, '-': 1, '*': 2, '/': 2}
 
-for i in v:
+for i in exp:
     if i.isupper():
-        print(i, end='')
+        print(i, end = '')
 
     elif i == '(':
         stack.append(i)
 
     elif i == ')':
         while stack[-1] != '(':
-            print(stack.pop(), end='')
+            print(stack.pop(), end = '')
+
         stack.pop()
 
     elif i in cal.keys():
         while stack[-1] != '(' and cal[i] <= cal[stack[-1]]:
-            print(stack.pop(), end='')
+            print(stack.pop(), end = '')
+
         stack.append(i)
-
-
