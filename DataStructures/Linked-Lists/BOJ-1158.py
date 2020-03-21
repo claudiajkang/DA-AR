d13a236b28fb204acbfc3166582eddcbb55c1116
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-22 06:56:04 +0900

    Review BOJ-1158

--- DataStructures/Linked-Lists/BOJ-1158.py
+++ DataStructures/Linked-Lists/BOJ-1158.py
@@ -10,53 +10,46 @@ class LinkedList:
     def __init__(self, k):
         self.head = None
         self.tail = None
-        self.k = k
-        self.s = 0
+        self.k = k - 1
 
     def add(self, data):
         nnode = Node(data)
 
         if not self.head:
             self.head = nnode
+            self.tail = nnode
 
         else:
             self.tail.next = nnode
 
         self.tail = nnode
         self.tail.next = self.head
-        self.s += 1
 
     def find(self):
-        p = self.tail
-        c = self.tail
+        p = self.head
+        c = self.head
 
         for i in range(self.k):
             p = c
-            c = p.next
+            c = c.next
 
-        tv = c.data
-
-        if self.head.data == c.data:
-            self.head = p.next
-
-        self.tail = p.next
         p.next = c.next
-        self.s -= 1
+        self.tail = p
+        self.head = c.next
 
-        return str(tv)
+        return c.data
 
-    def size(self):
-        return self.s
 
 n, k = map(int, stdin.readline().rstrip().split())
 ll = LinkedList(k)
-res = []
-
-for i in range(1, n+1):
-    ll.add(i)
-
-while ll.size() > 0:
-    res.append(ll.find())
 
 if n > 0:
-    print("<"+", ".join(res)+">")
+    for i in range(1, n+1):
+        ll.add(i)
+
+    print("<", end="")
+    for i in range(n):
+        print(ll.find(), end = "")
+        if i != n-1:
+            print(", ", end ="")
+    print(">")
