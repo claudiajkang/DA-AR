3449f992a8d283efafdb2c63635cbb95fe7e470a
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-24 05:51:12 +0900

    Review BOJ-1168

--- DataStructures/Linked-Lists/BOJ-1168.py
+++ DataStructures/Linked-Lists/BOJ-1168.py
@@ -2,13 +2,13 @@ from sys import stdin
 
 n, k = map(int, stdin.readline().rstrip().split())
 arr = [i for i in range(1, n+1)]
+k -= 1
 
 if n > 0:
-    print("<", end = "")
-    
-    ci = (k-1)
+    print("<", end="")
+    ci = k
     for i in range(n-1):
-        print(arr.pop(ci), end = ", ")
-        ci = (ci + k-1) % len(arr)
+        print(arr.pop(ci), end=", ")
+        ci = (ci + k) % len(arr)
 
-    print(str(arr.pop()) + ">")
+    print(arr.pop(), end=">")
