6718323ae780dab3cd5ae95e64373a50374e1a7b
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-23 16:26:25 +0900

    Review BOJ-1406

--- DataStructures/Linked-Lists/BOJ-1406.py
+++ DataStructures/Linked-Lists/BOJ-1406.py
@@ -2,23 +2,25 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 left = list(read())
+n = int(read())
 right = []
 
-m = int(read())
-
-for i in range(m):
+for i in range(n):
     cmd = read().split()
 
     if cmd[0] == "L":
         if left:
             right.append(left.pop())
+
     elif cmd[0] == "D":
         if right:
             left.append(right.pop())
+
     elif cmd[0] == "B":
         if left:
             left.pop()
+
     elif cmd[0] == "P":
         left.append(cmd[1])
 
-print(''.join(left)+''.join(reversed(right)))
+print(''.join(left) + ''.join(reversed(right)))
