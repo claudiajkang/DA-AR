bfaacfbb23aff29f1deaec3f910e742866979e80
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-24 05:36:09 +0900

    Review BOJ-1021

--- DataStructures/Linked-Lists/BOJ-1021.py
+++ DataStructures/Linked-Lists/BOJ-1021.py
@@ -3,10 +3,11 @@ read = lambda: stdin.readline().rstrip()
 
 n, m = map(int, read().split())
 q = [i for i in range(1, n+1)]
+position = list(map(int, read().split()))
 res = 0
 
-for find in map(int, read().split()):
-    idx = q.index(find)
+for p in position:
+    idx = q.index(p)
     res += min(len(q[idx:]), len(q[:idx]))
     q = q[idx+1:] + q[:idx]
 
