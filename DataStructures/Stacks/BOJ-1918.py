31fabd05614f6b3853a211d4c886a32d67cbbed5
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-23 19:22:07 +0900

    Review BOJ-1918

--- DataStructures/Stacks/BOJ-1918.py
+++ DataStructures/Stacks/BOJ-1918.py
@@ -6,19 +6,18 @@ cal = {'(': 0, '+': 1, '-': 1, '*': 2, '/': 2}
 
 for i in exp:
     if i.isupper():
-        print(i, end = '')
+        print(i, end = "")
 
-    elif i == '(':
+    elif i == "(":
         stack.append(i)
 
-    elif i == ')':
-        while stack[-1] != '(':
-            print(stack.pop(), end = '')
-
+    elif i == ")":
+        while stack[-1] != "(":
+            print(stack.pop(), end="")
         stack.pop()
 
     elif i in cal.keys():
-        while stack[-1] != '(' and cal[i] <= cal[stack[-1]]:
-            print(stack.pop(), end = '')
+        while stack[-1] != "(" and cal[i] <= cal[stack[-1]]:
+            print(stack.pop(), end = "")
 
         stack.append(i)
