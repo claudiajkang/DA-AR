73778c6e4078edbf6c3ecf8402b28b31c108264f
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-25 20:17:24 +0900

    Review BOJ-1107

--- Algorithms/BruteForce/BOJ-1107.py
+++ Algorithms/BruteForce/BOJ-1107.py
@@ -9,20 +9,16 @@ if n == 100:
     print(0)
     exit()
 
-else:
-    channel = []
-    res = abs(n-100)
+res = abs(n - 100)
 
-    for i in range(1000001):
-        e = True
-        for j in str(i):
-            if j in broke:
-                e = False
-                break
+for i in range(1000001):
+    e = True
+    for j in str(i):
+        if j in broke:
+            e = False
+            break
 
-        if e:
-            if abs(i-n) + len(str(i)) < res:
-                res = abs(i-n) + len(str(i))
+    if e:
+        res = min(res, len(str(i))+abs(i-n))
 
-    print(res)
-
+print(res)
