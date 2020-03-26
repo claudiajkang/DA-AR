78bc269617cba0b4877dd1bd472958a008e72751
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-26 15:14:30 +0900

    Review BOJ-1715

--- DataStructures/Heap/BOJ-1715.py
+++ DataStructures/Heap/BOJ-1715.py
@@ -1,18 +1,19 @@
 from sys import stdin
 from heapq import heappush, heappop
+read = lambda: int(stdin.readline().rstrip())
 
-n = int(stdin.readline().rstrip())
+n = read()
 hq = []
-res = 0
 
 for i in range(n):
-    heappush(hq, int(stdin.readline().rstrip()))
+    heappush(hq, read())
 
+res = 0
 while len(hq) > 1:
     h1 = heappop(hq)
-    h2 = 0
-    if hq: h2 = heappop(hq)
+    h2 = heappop(hq)
     res += (h1+h2)
-    if hq: heappush(hq, h1+h2)
+    if hq:
+        heappush(hq, (h1+h2))
 
 print(res)
