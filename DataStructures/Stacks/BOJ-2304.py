bd1c23ac6d6d8770583817937cd9f983010da9f2
Claudia J. Kang <claudiajkang@gmail.com>
2020-03-26 15:06:50 +0900

    Review BOJ-2304

--- DataStructures/Stacks/BOJ-2304.py
+++ DataStructures/Stacks/BOJ-2304.py
@@ -3,31 +3,28 @@ read = lambda: stdin.readline().rstrip()
 
 n = int(read())
 h = [0] * 1001
-ei = 0
-highest = 0
+end = 0
 
 for i in range(n):
-    x, l = map(int, read().split())
-    h[x] = l
-    highest = max(highest, h[x])
-    ei = max(ei, x)
+    x, y = map(int, read().split())
+    h[x] = y
+    end = max(end, x)
 
-hi = h.index(highest)
+highest = h.index(max(h))
+
 stack = [0]
 res = 0
-
-for i in range(hi+1):
-    if stack and h[stack[-1]] <= h[i]:
-        x = stack.pop()
-        res += (i-x) * h[x]
+for i in range(highest+1):
+    if stack and h[stack[-1]] < h[i]:
+        tx = stack.pop()
+        res += (i - tx) * h[tx]
         stack.append(i)
 
 stack = [0]
-
-for i in range(ei, hi-1, -1):
+for i in range(end, highest-1, -1):
     if stack and h[stack[-1]] <= h[i]:
-        x = stack.pop()
-        res += abs(x-i) * h[x]
+        tx = stack.pop()
+        res += (tx - i) * h[tx]
         stack.append(i)
 
 while stack:
