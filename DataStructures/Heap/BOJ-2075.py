4ff4fc583a71806ba07dc09fbe23db2a8be3093b
Claudia J.Kang <claudiajkang@gmail.com>
2020-03-28 21:04:37 +0900

    Review BOJ-2075

--- DataStructures/Heap/BOJ-2075.py
+++ DataStructures/Heap/BOJ-2075.py
@@ -9,12 +9,7 @@ for i in range(n):
     for j in map(int, read().split()):
         heappush(hq, j)
 
-    while len(hq) > (n + n):
+    while len(hq) > n:
         heappop(hq)
 
-r = 0
-for j in range(n+1):
-    if hq:
-        r = heappop(hq)
-
-print(r)
+print(heappop(hq))
