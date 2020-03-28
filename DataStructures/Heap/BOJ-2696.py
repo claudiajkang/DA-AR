94b6d086ac3579f5194b3a7b8c6e5506ca16039b
Claudia J.Kang <claudiajkang@gmail.com>
2020-03-28 21:26:22 +0900

    Review BOJ-2696

--- DataStructures/Heap/BOJ-2696.py
+++ DataStructures/Heap/BOJ-2696.py
@@ -5,39 +5,38 @@ read = lambda: stdin.readline().rstrip()
 
 t = int(read())
 
-for tt in range(t):
+for _ in range(t):
     m = int(read())
     arr = [0]
 
-    for k in range(m // 10 + 1):
+    for j in range(m//10 + 1):
         arr += list(map(int, read().split()))
 
     hq = []
-    c = m // 2 + 1 if m % 2 else m // 2
-    print(c)
-    res = ""
-
+    res = []
+    print(len(arr)//2)
     for i in range(1, m+1):
         heappush(hq, arr[i])
 
         if i == 1:
-            res += str(arr[i]) + " "
+            res.append(arr[i])
             continue
 
-        if i % 2 == 1:
-            tq = deque()
+        elif i % 2:
+            idx = len(hq) // 2
+            t = deque()
+            for j in range(idx):
+                t.append(heappop(hq))
 
-            for k in range(i // 2):
-                tq.append(heappop(hq))
+            res.append(heappop(hq))
+            heappush(hq, res[-1])
 
-            res += str(hq[0]) + " "
+            while t:
+                heappush(hq, t.popleft())
 
-            while hq and tq:
-                heappush(hq, tq.pop())
+        if len(res) == 10:
+            print(' '.join(map(str, res)))
+            res = []
 
-        if i == 20:
-            print(res[:-1])
-            res = ""
-
-    if res != "":
-        print(res)
+    if len(res) > 0:
+        print(' '.join(map(str, res)))
