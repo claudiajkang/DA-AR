84f997a1b4bd8f5babcae346ab20e665c15cc3d1
Claudia J.Kang <claudiajkang@gmail.com>
2020-03-30 05:56:06 +0900

    Review BOJ-2696

--- DataStructures/Heap/BOJ-2696.py
+++ DataStructures/Heap/BOJ-2696.py
@@ -1,50 +1,49 @@
 from sys import stdin
-from heapq import heappush, heappop, nlargest
-read = lambda: stdin.readline().rstrip()
+from heapq import heappush, heappop
 
-t = int(read())
+t = int(stdin.readline().rstrip())
 
 for tt in range(t):
-    m = int(read())
+    m = int(stdin.readline().rstrip())
     arr = [0]
 
     for i in range(m // 10 + 1):
-        arr += list(map(int, read().split()))
+        arr += list(map(int, stdin.readline().rstrip().split()))
 
-    l = []
-    r = []
+    minhq = []
+    maxhq = []
     mid = 0
+
     print(len(arr) // 2)
     res = []
 
     for i in range(1, m+1):
         val = arr[i]
+
         if i == 1:
             mid = val
             res.append(mid)
             continue
 
         elif i % 2:
-            if val <= r[0]:
-                heappush(l, (-1)*val)
-                mid = heappop(l) * (-1)
+            if val <= minhq[0]:
+                heappush(maxhq, (-1) * val)
+                mid = (-1) * heappop(maxhq)
 
             else:
-                heappush(r, val)
-                mid = heappop(r)
+                heappush(minhq, val)
+                mid = heappop(minhq)
 
             res.append(mid)
 
         else:
             if val <= mid:
-                heappush(r, mid)
-                mid = val
-                heappush(l, (-1)*mid)
+                heappush(minhq, mid)
+                heappush(maxhq, (-1) * val)
 
             else:
-                heappush(l, (-1)*mid)
-                mid = val
-                heappush(r, mid)
+                heappush(maxhq, (-1) * mid)
+                heappush(minhq, val)
 
         if len(res) == 10:
             print(' '.join(map(str, res)))
