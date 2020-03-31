f3f8c4df9e6a1e66af02277fe7f41c7f4ee0c31f
Claudia J.Kang <claudiajkang@gmail.com>
2020-04-01 08:18:12 +0900

    Review BOJ-1260

--- Algorithms/GraphTheory/BOJ-1260.py
+++ Algorithms/GraphTheory/BOJ-1260.py
@@ -3,42 +3,41 @@ from collections import deque
 read = lambda: stdin.readline().rstrip()
 
 n, m, v = map(int, read().split())
-
 g = [[] for i in range(n+1)]
 
 for i in range(m):
-    A, B = map(int, read().split())
-    g[A].append(B)
-    g[B].append(A)
+    a, b = map(int, read().split())
+    g[a].append(b)
+    g[b].append(a)
+
+dfs = deque()
+q = deque()
+q.append(v)
 
 for i in range(1, n+1):
     g[i] = sorted(g[i])
 
-stack = [v]
-dfs = []
-
-while stack:
-    cur = stack.pop()
-
-    if cur not in dfs:
-        dfs.append(cur)
-
-    for i in reversed(g[cur]):
-        if i not in dfs:
-            stack.append(i)
-
-q = deque()
-q.append(v)
-bfs = []
-
 while q:
-    cur = q.popleft()
-    if cur not in bfs:
-        bfs.append(cur)
+    c = q.pop()
+    if c not in dfs:
+        dfs.append(c)
 
-    for i in g[cur]:
-        if i not in bfs:
+    for i in reversed(g[c]):
+        if i not in dfs:
             q.append(i)
 
 print(' '.join(map(str, dfs)))
+
+bfs = deque()
+q = deque()
+q.append(v)
+
+while q:
+    c = q.popleft()
+    if c not in bfs: bfs.append(c)
+
+    for i in g[c]:
+        if i not in bfs:
+            q.append(i)
+
 print(' '.join(map(str, bfs)))
