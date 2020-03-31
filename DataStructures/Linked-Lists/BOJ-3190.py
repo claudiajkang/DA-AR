b949e76d85813542d01ab548641d50226f5b9b6e
Claudia J.Kang <claudiajkang@gmail.com>
2020-04-01 06:54:59 +0900

    Review BOJ-3190

--- DataStructures/Linked-Lists/BOJ-3190.py
+++ DataStructures/Linked-Lists/BOJ-3190.py
@@ -1,45 +1,40 @@
 from sys import stdin
 from collections import deque
+read = lambda: stdin.readline().rstrip()
 
-n = int(stdin.readline().rstrip())
-k = int(stdin.readline().rstrip())
-board = [[-1] + [0] * n + [-1] for i in range(n+2)]
-board[0] = [-1] * (n+2)
-board[-1] = [-1] * (n+2)
-x, y = 0, 0
+n = int(read())
+k = int(read())
+b = [[-1] + [0] * n + [-1] for i in range(n+2)]
+b[0] = [-1] * (n+2)
+b[-1] = [-1] * (n+2)
 
 for i in range(k):
-    x, y = map(int, stdin.readline().rstrip().split())
-    board[x][y] = 1
+    x, y = map(int, read().split())
+    b[x][y] = 1
 
-l = int(stdin.readline().rstrip())
 time = deque()
 direction = deque()
 time.append(0)
 direction.append(0)
 
-for i in range(l):
-    x, y = stdin.readline().rstrip().split()
-    time.append(int(x))
-
-    if y == 'D':
-        direction.append(1)
-
-    elif y == 'L':
-        direction.append(-1)
-
-x, y = 1, 0
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-d = 0
+l = int(read())
+for i in range(l):
+    t, d = read().split()
+    time.append(int(t))
+    direction.append(1 if d == 'D' else -1)
+
 snake = deque()
+x, y = 1, 0
+d = 0
 
 for t in range(10101):
     x += dx[d]
     y += dy[d]
 
-    if board[x][y] == -1:
+    if b[x][y] == -1:
         print(t)
         exit()
 
@@ -47,15 +42,16 @@ for t in range(10101):
         print(t)
         exit()
 
-    if board[x][y] != 1:
-        if snake:
-            snake.popleft()
+    if b[x][y] != 1:
+        if snake: snake.popleft()
 
-    if board[x][y] == 1:
-        board[x][y] = 0
+    if b[x][y] == 1:
+        b[x][y] = 0
 
     snake.append([x, y])
 
     if t in time:
         time.popleft()
-        d = (d+direction.popleft()) % 4
+        d += direction.popleft()
+        if d == 4: d = 0
+        elif d == -1: d = 3
