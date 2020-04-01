a9690250325738f51e3c3ae9c7c626f341b94bfc
Claudia J.Kang <claudiajkang@gmail.com>
2020-04-01 15:30:00 +0900

    Review BOJ-1743

--- Algorithms/GraphTheory/BOJ-1743.py
+++ Algorithms/GraphTheory/BOJ-1743.py
@@ -1,43 +1,38 @@
 from sys import stdin
-from copy import deepcopy
 from collections import deque
 read = lambda: stdin.readline().rstrip()
 
 n, m, k = map(int, read().split())
-maps = [[-1] + [0] * m + [-1] for i in range(n + 2)]
-maps[0] = [-1] * (m + 2)
-maps[-1] = [-1] * (m + 2)
+g = [[-2] + [0] * m + [-2] for j in range(n+2)]
+g[0] = [-2] * (m + 2)
+g[-1] = [-2] * (m + 2)
 
-visited = deepcopy(maps)
-
-for i in range(k):
-    x, y = map(int, read().split())
-    maps[x][y] = 1
+for kk in range(k):
+    a, b = map(int, read().split())
+    g[a][b] = 1
 
 res = 0
-
-p = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
 for i in range(1, n+1):
     for j in range(1, m+1):
-        if maps[i][j] == 1 and visited[i][j] == 0:
-            dq = deque()
-            dq.append([i, j])
-            visited[i][j] = 1
-            dfs = list()
+        if g[i][j] == 1:
+            q = deque()
+            dfs = deque()
+            q.append([i, j])
+            dfs.append([i, j])
+            g[i][j] = -1
 
-            while dq:
-                ci, cj = dq.popleft()
-
-                dfs.append([ci, cj])
+            while q:
+                ci, cj = q.popleft()
 
                 for ii, jj in p:
-                    ti = ci + ii
-                    tj = cj + jj
+                    ti, tj = ci + ii, cj + jj
 
-                    if maps[ti][tj] == 1 and visited[ti][tj] == 0:
-                        dq.append([ti, tj])
-                        visited[ti][tj] = 1
+                    if g[ti][tj] == 1:
+                        g[ti][tj] = -1
+                        q.append([ti, tj])
+                        dfs.append([ti, tj])
 
             if len(dfs) > res:
                 res = len(dfs)
