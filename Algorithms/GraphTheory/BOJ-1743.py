6de58ab9b2cc1b3324d7248a0c952b9bf1e2fc05
Claudia J.Kang <claudiajkang@gmail.com>
2020-04-01 15:58:28 +0900

    Review BOJ-1743

--- Algorithms/GraphTheory/BOJ-1743.py
+++ Algorithms/GraphTheory/BOJ-1743.py
@@ -2,39 +2,65 @@ from sys import stdin
 from collections import deque
 read = lambda: stdin.readline().rstrip()
 
-n, m, k = map(int, read().split())
-g = [[-2] + [0] * m + [-2] for j in range(n+2)]
-g[0] = [-2] * (m + 2)
-g[-1] = [-2] * (m + 2)
+n = int(read())
+RGB = [['0'] * (n+2) for i in range(n+2)]
+R_B = [['0'] * (n+2) for i in range(n+2)]
 
-for kk in range(k):
-    a, b = map(int, read().split())
-    g[a][b] = 1
+for i in range(1, n+1):
+    r = read()
+    RGB[i] = ['0'] + list(r) + ['0']
+    r = r.replace('G', 'R')
+    R_B[i] = ['0'] + list(r) + ['0']
 
-res = 0
+res = [0, 0]
 p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
 for i in range(1, n+1):
-    for j in range(1, m+1):
-        if g[i][j] == 1:
+    for j in range(1, n+1):
+        if RGB[i][j] != '0':
             q = deque()
             dfs = deque()
+            color = RGB[i][j]
+            RGB[i][j] = '0'
+
             q.append([i, j])
             dfs.append([i, j])
-            g[i][j] = -1
 
             while q:
                 ci, cj = q.popleft()
 
                 for ii, jj in p:
                     ti, tj = ci + ii, cj + jj
-
-                    if g[ti][tj] == 1:
-                        g[ti][tj] = -1
+                    if RGB[ti][tj] == color:
+                        RGB[ti][tj] = '0'
                         q.append([ti, tj])
                         dfs.append([ti, tj])
 
-            if len(dfs) > res:
-                res = len(dfs)
+            if dfs:
+                res[0] += 1
 
-print(res)
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if R_B[i][j] != '0':
+            q = deque()
+            dfs = deque()
+            color = R_B[i][j]
+            R_B[i][j] = '0'
+
+            q.append([i, j])
+            dfs.append([i, j])
+
+            while q:
+                ci, cj = q.popleft()
+
+                for ii, jj in p:
+                    ti, tj = ci + ii, cj + jj
+                    if R_B[ti][tj] == color:
+                        R_B[ti][tj] = '0'
+                        q.append([ti, tj])
+                        dfs.append([ti, tj])
+
+            if dfs:
+                res[1] += 1
+
+print(' '.join(map(str, res)))
