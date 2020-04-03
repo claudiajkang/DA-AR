da89455d302e9aa2678ef0558f971b82e5f44f69
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-03 16:42:48 +0900

    AG-437:[0h 29m]Review BOJ-1655 [hq] 가운데를 말해요
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Heap/BOJ-1655.py
+++ DataStructures/Heap/BOJ-1655.py
@@ -1,37 +1,33 @@
-from sys import stdin
 from heapq import heappush, heappop
+from sys import stdin
+
 read = lambda: int(stdin.readline().rstrip())
 
 n = read()
-arr = [0] * (n+1)
+arr = [0] + [read() for i in range(n)]
 l = []
 r = []
 mid = 0
 res = []
 
 for i in range(1, n+1):
-    arr[i] = read()
-
     if i % 2:
         if not r:
-            mid = arr[i]
-            heappush(r, mid)
+            heappush(r, arr[i])
         elif arr[i] <= r[0]:
             heappush(l, arr[i] * (-1))
-            mid = heappop(l) * (-1)
-            heappush(r, mid)
+            heappush(r, heappop(l) * (-1))
         else:
             heappush(r, arr[i])
-            mid = heappop(r)
-            heappush(r, mid)
-        res.append(mid)
+        mid = r[0]
     else:
         if arr[i] <= mid:
             heappush(l, arr[i] * (-1))
-            res.append(l[0] * (-1))
         else:
             heappush(l, heappop(r) * (-1))
             heappush(r, arr[i])
-            res.append(l[0] * (-1))
+        mid = l[0] * (-1)
+
+    res.append(mid)
 
 print('\n'.join(map(str, res)))
