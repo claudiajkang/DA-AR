60b006bcaf179be3102a0b0d0a8b511de080afc8
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-03 20:20:21 +0900

    AG-54:[0h 30m] BOJ-1068 [Tr] 트리
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-1068.py
+++ DataStructures/Trees/BOJ-1068.py
@@ -1,13 +1,46 @@
 from sys import stdin
+
 read = lambda: stdin.readline().rstrip()
 
+
+def preorder(node):
+    global cnt, tree
+    if node.child == []:
+        cnt += 1
+    for child in node.child:
+        preorder(tree[child])
+
+
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-class Tree:
     def __init__(self):
-        self.root = None
+        self.child = []
 
+    def setChild(self, node):
+        self.child.append(node)
+
+    def removeChild(self, node):
+        self.child.remove(node)
+
+
+n = int(read())
+tree = [Node() for _ in range(n)]
+cnt = 0
+parent = list(map(int, read().split()))
+root = 0
+
+for i in range(n):
+    if parent[i] != -1:
+        tree[parent[i]].setChild(i)
+    else:
+        root = i
+
+if n != 1:
+    i = int(read())
+    if parent[i] == -1:
+        cnt = 0
+
+    else:
+        tree[parent[i]].removeChild(i)
+        preorder(tree[root])
+
+print(cnt)
