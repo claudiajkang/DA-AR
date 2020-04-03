e8d5db718ee061c74442d90ca0da18e56a9db14d
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-03 16:56:39 +0900

    AG-436:[0h 9m]Review BOJ-2696 [hq] 중앙값 구하기 (★)
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Heap/BOJ-2696.py
+++ DataStructures/Heap/BOJ-2696.py
@@ -1,5 +1,6 @@
-from sys import stdin
 from heapq import heappush, heappop
+from sys import stdin
+
 read = lambda: stdin.readline().rstrip()
 
 t = int(read())
@@ -8,37 +9,33 @@ for tt in range(t):
     m = int(read())
     arr = [0]
 
-    for i in range(m//10 + 1):
+    for i in range(m // 10 + 1):
         arr += list(map(int, read().split()))
-
-    print(len(arr)//2)
-    res = []
 
     l = []
     r = []
     mid = 0
+    res = []
+
+    print(len(arr) // 2)
 
     for i in range(1, len(arr)):
         if i % 2:
             if not r:
                 mid = arr[i]
-                heappush(r, mid)
             elif arr[i] <= r[0]:
                 heappush(l, arr[i] * (-1))
                 mid = heappop(l) * (-1)
-                heappush(r, mid)
             else:
                 heappush(r, arr[i])
                 mid = heappop(r)
-                heappush(r, mid)
             res.append(mid)
-
         else:
             if arr[i] <= mid:
+                heappush(r, mid)
                 heappush(l, arr[i] * (-1))
-
             else:
-                heappush(l, heappop(r) * (-1))
+                heappush(l, mid * (-1))
                 heappush(r, arr[i])
 
         if len(res) == 10:
