bd75f561131d63f68df88f01106755d7d3378805
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-06 17:09:36 +0900

    AG-468:[0h 10m] Review BOJ-7562 [Gp] 나이트의 이동
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-7562.py
+++ Algorithms/GraphTheory/BOJ-7562.py
@@ -3,38 +3,42 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-t = int(read())
-res = []
-px = (2, 1, -1, -2, -2, -1, 1, 2)
-py = (1, 2, 2, 1, -1, -2, -2, -1)
+T = int(read())
+p = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
 
+for tt in range(T):
+    l = int(read())
+    g = [[0] * l for i in range(l)]
 
-def bfs():
-    global dx, dy, b, l
+    ci, cj = map(int, read().split())
+    di, dj = map(int, read().split())
+
+    if [ci, cj] == [di, dj]:
+        print(0)
+        continue
 
     q = deque()
-    q.append([cx, cy])
+    q.append([ci, cj])
+    g[ci][cj] = 0
+    end = False
 
     while q:
         ci, cj = q.popleft()
-        if ci == dx and cj == dy:
-            print(b[ci][cj])
-            break
 
-        for idx in range(8):
-            ti = ci + px[idx]
-            tj = cj + py[idx]
+        for ii, jj in p:
+            ti, tj = ci + ii, cj + jj
 
-            if ti < 0 or ti >= l or tj < 0 or tj >= l:
+            if ti == di and tj == dj:
+                print(g[ci][cj] + 1)
+                end = True
+                break
+
+            if (ti < 0 or ti >= l) or (tj < 0 or tj >= l):
                 continue
-            if not b[ti][tj]:
-                b[ti][tj] = b[ci][cj] + 1
+
+            if g[ti][tj] == 0:
+                g[ti][tj] = g[ci][cj] + 1
                 q.append([ti, tj])
 
-
-for tt in range(t):
-    l = int(read())
-    b = [[0] * l for i in range(l)]
-    cx, cy = map(int, read().split())
-    dx, dy = map(int, read().split())
-    bfs()
+        if end:
+            break
