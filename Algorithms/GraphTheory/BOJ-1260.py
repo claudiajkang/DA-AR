3a62cc21fcc6a729392b0ecc4c930b9f80515e47
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-06 16:52:01 +0900

    AG-440:[0h 10m] Review BOJ-1260 [Gp] DFS와 BFS
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-1260.py
+++ Algorithms/GraphTheory/BOJ-1260.py
@@ -1,42 +1,45 @@
-from sys import stdin
 from collections import deque
+from sys import stdin
+
 read = lambda: stdin.readline().rstrip()
 
 n, m, v = map(int, read().split())
-g = [[] for i in range(n+1)]
+g = [[] for i in range(n + 1)]
 
 for i in range(m):
     a, b = map(int, read().split())
     g[a].append(b)
     g[b].append(a)
 
-dfs = deque()
+for i in range(1, n + 1):
+    g[i] = sorted(g[i])
+
+dfs = []
 q = deque()
 q.append(v)
 
-for i in range(1, n+1):
-    g[i] = sorted(g[i])
-
 while q:
-    c = q.pop()
-    if c not in dfs:
-        dfs.append(c)
+    cur = q.pop()
+    if cur not in dfs:
+        dfs.append(cur)
 
-    for i in reversed(g[c]):
+    for i in reversed(g[cur]):
         if i not in dfs:
             q.append(i)
 
+
 print(' '.join(map(str, dfs)))
 
-bfs = deque()
+bfs = []
 q = deque()
 q.append(v)
 
 while q:
-    c = q.popleft()
-    if c not in bfs: bfs.append(c)
+    cur = q.popleft()
+    if cur not in bfs:
+        bfs.append(cur)
 
-    for i in g[c]:
+    for i in g[cur]:
         if i not in bfs:
             q.append(i)
 
