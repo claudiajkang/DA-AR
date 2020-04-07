8cba1e03a36883e800b7f063d7c57f4c83119a44
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-07 09:59:22 +0900

    AG-474:*[0h 16m] Review BOJ-7562 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-7562.py
+++ Algorithms/GraphTheory/BOJ-7562.py
@@ -3,12 +3,11 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-T = int(read())
+tt = int(read())
 p = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
 
-for tt in range(T):
+for t in range(tt):
     l = int(read())
-    g = [[0] * l for i in range(l)]
 
     ci, cj = map(int, read().split())
     di, dj = map(int, read().split())
@@ -17,28 +16,24 @@ for tt in range(T):
         print(0)
         continue
 
+    g = [[0] * l for i in range(l)]
+
     q = deque()
     q.append([ci, cj])
-    g[ci][cj] = 0
-    end = False
 
     while q:
         ci, cj = q.popleft()
 
+        if [ci, cj] == [di, dj]:
+            print(g[ci][cj])
+            break
+
         for ii, jj in p:
             ti, tj = ci + ii, cj + jj
-
-            if ti == di and tj == dj:
-                print(g[ci][cj] + 1)
-                end = True
-                break
 
             if (ti < 0 or ti >= l) or (tj < 0 or tj >= l):
                 continue
 
             if g[ti][tj] == 0:
-                g[ti][tj] = g[ci][cj] + 1
                 q.append([ti, tj])
-
-        if end:
-            break
+                g[ti][tj] = g[ci][cj] + 1
