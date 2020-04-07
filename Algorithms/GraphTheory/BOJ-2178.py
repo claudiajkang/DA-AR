5e85d198438dcf04fd942e3c48d941459a06cc50
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-07 09:50:05 +0900

    AG-473:[0h 4m] Review BOJ-2178 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-2178.py
+++ Algorithms/GraphTheory/BOJ-2178.py
@@ -4,26 +4,22 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n, m = map(int, read().split())
-g = []
+g = [[-1] * (m + 2) for i in range(n + 2)]
 
-for i in range(n):
-    g.append(list(map(int, list(read()))))
+for i in range(1, n + 1):
+    g[i] = [-1] + list(map(int, list(read()))) + [-1]
 
-p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 q = deque()
-q.append([0, 0])
-g[0][0] = 1
+q.append([1, 1])
+p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
 while q:
     ci, cj = q.popleft()
 
     for ii, jj in p:
-        ti, tj = ci + ii, cj + jj
-        if (ti < 0 or ti >= n) or (tj < 0 or tj >= m):
-            continue
-
+        ti, tj = ii + ci, jj + cj
         if g[ti][tj] == 1:
             q.append([ti, tj])
             g[ti][tj] = g[ci][cj] + 1
 
-print(g[n - 1][m - 1])
+print(g[n][m])
