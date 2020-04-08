a52f71e3a784afb7182d8dcb8dd51e1596587c1e
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-09 05:34:16 +0900

    AG-489:*[0h 6m] Review BOJ-4803 [Tr]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-4803.py
+++ DataStructures/Trees/BOJ-4803.py
@@ -3,13 +3,13 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-cs = 1
-res = ""
+results = ""
+case = 1
 while True:
     n, m = map(int, read().split())
 
-    if n == 0 and m == 0:
-        print(res[:-1])
+    if [n, m] == [0, 0]:
+        print(results[:-1])
         break
 
     tree = [[] for i in range(n + 1)]
@@ -21,39 +21,40 @@ while True:
 
     prev = [-1] * (n + 1)
     visited = [False] * (n + 1)
-    results = 0
+
+    count = 0
 
     for i in range(1, n + 1):
         if not visited[i]:
             q = deque()
-            q.append(i)
             visited[i] = True
+            q.append(i)
             flag = True
 
             while q:
                 cur = q.popleft()
-                for nxt in tree[cur]:
-                    if not visited[nxt]:
-                        visited[nxt] = True
-                        prev[nxt] = cur
-                        q.append(nxt)
-                        continue
 
-                    if nxt != prev[cur]:
+                for j in tree[cur]:
+                    if not visited[j]:
+                        visited[j] = True
+                        prev[j] = cur
+                        q.append(j)
+
+                    elif j != prev[cur]:
                         flag = False
+                        break
 
             if flag:
-                results += 1
+                count += 1
 
-    res += f"Case {cs}: "
+    results += f"Case {case}: "
+    if count == 0:
+        results += "No trees.\n"
 
-    if results == 0:
-        res += "No trees.\n"
-
-    elif results == 1:
-        res += "There is one tree.\n"
+    elif count == 1:
+        results += "There is one tree.\n"
 
     else:
-        res += f"A forest of {results} trees.\n"
+        results += f"A forest of {count} trees.\n"
 
-    cs += 1
+    case += 1
