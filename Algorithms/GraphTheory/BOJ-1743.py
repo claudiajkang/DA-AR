573b330ad891e6d14b3130c008816eb4c2cea51b
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-08 14:38:42 +0900

    AG-482:[0h 7m] Review BOJ-1743 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-1743.py
+++ Algorithms/GraphTheory/BOJ-1743.py
@@ -4,32 +4,33 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n, m, k = map(int, read().split())
-g = [[-1] * (m + 2) for i in range(n + 2)]
+
+g = [[-1] + [0] * m + [-1] for i in range(n + 2)]
 
 for i in range(k):
     a, b = map(int, read().split())
     g[a][b] = 1
 
-p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 res = 0
-
+p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 for i in range(1, n + 1):
     for j in range(1, m + 1):
         if g[i][j] == 1:
-            g[i][j] = -1
             q = deque()
             q.append([i, j])
-            dfs = []
+            g[i][j] = -1
+            dfs = [[i, j]]
 
             while q:
                 ci, cj = q.popleft()
-                if [ci, cj] not in dfs:
-                    dfs.append([ci, cj])
 
                 for ii, jj in p:
-                    ti, tj = ci + ii, cj + jj
+                    ti = ci + ii
+                    tj = cj + jj
+
                     if g[ti][tj] == 1:
                         g[ti][tj] = -1
+                        dfs.append([ti, tj])
                         q.append([ti, tj])
 
             if len(dfs) > res:
