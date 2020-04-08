d25cb458b42c118f76f8b354dfab212da605d36e
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-08 14:30:50 +0900

    AG-481:[0h 9m] Review BOJ-7562 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-7562.py
+++ Algorithms/GraphTheory/BOJ-7562.py
@@ -3,33 +3,33 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-tt = int(read())
+t = int(read())
 p = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
-
-for t in range(tt):
+for tt in range(t):
     l = int(read())
-
+    g = [[0] * l for i in range(l)]
     ci, cj = map(int, read().split())
     di, dj = map(int, read().split())
 
-    if [ci, cj] == [di, dj]:
+    if ci == di and cj == dj:
         print(0)
         continue
 
-    g = [[0] * l for i in range(l)]
-
+    g[ci][cj] = 1
     q = deque()
     q.append([ci, cj])
+    end = False
 
     while q:
         ci, cj = q.popleft()
 
-        if [ci, cj] == [di, dj]:
-            print(g[ci][cj])
-            break
-
         for ii, jj in p:
             ti, tj = ci + ii, cj + jj
+
+            if ti == di and tj == dj:
+                print(g[ci][cj])
+                end = True
+                break
 
             if (ti < 0 or ti >= l) or (tj < 0 or tj >= l):
                 continue
@@ -37,3 +37,6 @@ for t in range(tt):
             if g[ti][tj] == 0:
                 q.append([ti, tj])
                 g[ti][tj] = g[ci][cj] + 1
+
+        if end:
+            break
