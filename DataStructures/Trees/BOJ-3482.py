3198cd4d26be2e4513de82a20b9695e086277302
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-10 16:18:37 +0900

    AG-502:*[0h 27m] Review BOJ-3482 [Tr]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-3482.py
+++ DataStructures/Trees/BOJ-3482.py
@@ -4,67 +4,54 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-t = int(read())
 
-result = ""
-p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-
-
-def dfs(i, j, g):
+def dfs(start, g):
     global c, r
 
     q = deque()
-    q.append([i, j])
-    g[i][j] = 0
-    mi, mj = 0, 0
+    q.append(start)
+    g[start[0]][start[1]] = 0
+    midx = [0, 0]
     m = 0
 
     while q:
         ci, cj = q.popleft()
-
-        for ii, jj in p:
+        for ii, jj in [[0, -1], [0, 1], [-1, 0], [1, 0]]:
             ti = ci + ii
             tj = cj + jj
 
-            if (ti < 0 or ti >= r) or (tj < 0 or tj >= c):
-                continue
+            if (0 <= ti < r) and (0 <= tj < c):
+                if g[ti][tj] == '.':
+                    g[ti][tj] = g[ci][cj] + 1
+                    q.append([ti, tj])
+                    if g[ti][tj] > m:
+                        m = g[ti][tj]
+                        midx = [ti, tj]
 
-            if g[ti][tj] == '.':
-                g[ti][tj] = g[ci][cj] + 1
-                q.append([ti, tj])
-                if g[ti][tj] > m:
-                    m = g[ti][tj]
-                    mi, mj = ti, tj
+    return midx, m
 
-    return mi, mj, m
 
+t = int(read())
+result = ""
 
 for tt in range(t):
     c, r = map(int, read().split())
 
-    g = [['#'] * c for i in range(r)]
-    q = deque()
-    res = 0
-    counts = 0
-    initi, initj = 0, 0
+    g = [[] for i in range(r)]
+    init = False
+    start = [-1, -1]
 
     for i in range(r):
-        g[i] = ['#'] + list(read()) + ['#']
+        g[i] = list(read())
 
-        if [initi, initj] == [0, 0] and g[i].count('.') > 0:
-            initi = i
-            initj = g[i].index('.')
+        if not init and g[i].count('.') > 0:
+            start = [i, g[i].index('.')]
+            init = True
 
-        counts += g[i].count('.')
+    g2 = deepcopy(g)
+    fidx, fm = dfs(start, g)
+    sidx, sm = dfs(fidx, g2)
 
-    if counts > 1:
-        g2 = deepcopy(g)
-        fi, fj, f = dfs(initi, initj, g)
-        si, sj, res = dfs(fi, fj, g2)
-
-    else:
-        res = 0
-
-    result += f'Maximum rope length is {res}.\n'
+    result += f"Maximum rope length is {sm}.\n"
 
 print(result[:-1], end="")
