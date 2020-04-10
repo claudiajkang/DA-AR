6b2b5c3700d8d338fa60aa5ef744a83ec0331fb2
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-10 14:56:48 +0900

    AG-497:*[0h 11m] Review BOJ-4803 [Tr]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-4803.py
+++ DataStructures/Trees/BOJ-4803.py
@@ -3,26 +3,25 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-results = ""
 case = 1
+result = ""
 while True:
     n, m = map(int, read().split())
 
-    if [n, m] == [0, 0]:
-        print(results[:-1])
+    if n == 0 and m == 0:
+        print(result[:-1], end="")
         break
 
-    tree = [[] for i in range(n + 1)]
+    g = [[] for i in range(n + 1)]
 
     for i in range(m):
         a, b = map(int, read().split())
-        tree[a].append(b)
-        tree[b].append(a)
+        g[a].append(b)
+        g[b].append(a)
 
-    prev = [-1] * (n + 1)
     visited = [False] * (n + 1)
-
-    count = 0
+    prev = [-1] * (n + 1)
+    tree = 0
 
     for i in range(1, n + 1):
         if not visited[i]:
@@ -34,10 +33,10 @@ while True:
             while q:
                 cur = q.popleft()
 
-                for j in tree[cur]:
+                for j in g[cur]:
                     if not visited[j]:
-                        visited[j] = True
                         prev[j] = cur
+                        visited[j] = True
                         q.append(j)
 
                     elif j != prev[cur]:
@@ -45,16 +44,15 @@ while True:
                         break
 
             if flag:
-                count += 1
+                tree += 1
 
-    results += f"Case {case}: "
-    if count == 0:
-        results += "No trees.\n"
+    if tree == 0:
+        result += f"Case {case}: No trees.\n"
 
-    elif count == 1:
-        results += "There is one tree.\n"
+    elif tree == 1:
+        result += f"Case {case}: There is one tree.\n"
 
     else:
-        results += f"A forest of {count} trees.\n"
+        result += f"Case {case}: A forest of {tree} trees.\n"
 
     case += 1
