8f0634d2885df486bdc1b24ef984dba6f16a6125
Claudia J.Kang <claudiajkang@gmail.com>
2020-04-10 20:42:03 +0900

    AG-61:*[2h 0m] BOJ-5427 [Gp] 불 (★)
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

---
+++ Algorithms/GraphTheory/BOJ-5427.py
@@ -0,0 +1,74 @@
+from sys import stdin
+from collections import deque
+read = lambda: stdin.readline().rstrip()
+
+t = int(read())
+p = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+for tt in range(t):
+    w, h = map(int, read().split())
+
+    visited = [[0] * 1000 for i in range(1000)]
+    maps = [[] for i in range(h)]
+    q = deque()
+    fq = deque()
+
+    for i in range(h):
+        maps[i] = list(read())
+
+        for j in range(w):
+            if maps[i][j] == '@':
+                visited[i][j] = True
+                q.append([i, j])
+
+            elif maps[i][j] == '*':
+                fq.append([i, j])
+
+    success = False
+    result = 0
+
+    while q:
+        result += 1
+
+        qsize = len(q)
+        for i in range(qsize):
+            r, c = q.popleft()
+
+            if maps[r][c] == '*': continue
+
+            for ii, jj in p:
+                nr = r + ii
+                nc = c + jj
+
+                if nr < 0 or nr >= h or nc < 0 or nc >= w:
+                    success = True
+                    break
+
+                if maps[nr][nc] == '.' and not visited[nr][nc]:
+                    visited[nr][nc] = True
+                    q.append([nr, nc])
+
+            if success: break
+
+        if success: break
+
+        qsize = len(fq)
+
+        for i in range(qsize):
+            r, c = fq.popleft()
+
+            for ii, jj in p:
+                nr = r + ii
+                nc = c + jj
+
+                if nr < 0 or nr >= h or nc < 0 or nc >= w:
+                    continue
+
+                if maps[nr][nc] == '.':
+                    maps[nr][nc] = '*'
+                    fq.append([nr, nc])
+
+    if success:
+        print(result)
+    else:
+        print("IMPOSSIBLE")
