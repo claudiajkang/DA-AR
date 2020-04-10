77ea04cdf9c3377887b2a87d0e173a4171c92093
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-10 16:48:19 +0900

    AG-504:[0h 6m] Review BOJ-7576 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-7576.py
+++ Algorithms/GraphTheory/BOJ-7576.py
@@ -3,48 +3,44 @@ from sys import stdin
 
 read = lambda: map(int, stdin.readline().rstrip().split())
 
-m, n = read()
-box = [[-1] * m for i in range(n)]
-
+n, m = read()
+g = [[0] * n for i in range(m)]
 q = deque()
 zero = 0
 minus = 0
 
-for i in range(n):
-    box[i] = list(read())
+for i in range(m):
+    g[i] = list(read())
 
-    if box[i].count(1) > 0:
-        for j in range(m):
-            if box[i][j] == 1:
+    if 1 in g[i]:
+        for j in range(n):
+            if g[i][j] == 1:
                 q.append([i, j])
 
-    zero += box[i].count(0)
-    minus += box[i].count(-1)
+    zero += g[i].count(0)
+    minus += g[i].count(-1)
 
-if len(q) + minus == (m * n):
+if len(q) + minus == (n * m):
     print(0)
-    exit()
 
-p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-res = 0
+else:
+    res = 0
 
-while q:
-    ci, cj = q.popleft()
+    while q:
+        ci, cj = q.popleft()
 
-    for ii, jj in p:
-        ti = ci + ii
-        tj = cj + jj
+        for ii, jj in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+            ti, tj = ii + ci, jj + cj
+            if (0 <= ti < m) and (0 <= tj < n):
+                if g[ti][tj] == 0:
+                    g[ti][tj] = g[ci][cj] + 1
+                    q.append([ti, tj])
+                    if g[ti][tj] > res:
+                        res = g[ti][tj] - 1
 
-        if 0 <= ti < n and 0 <= tj < m:
-            if box[ti][tj] == 0:
-                box[ti][tj] = box[ci][cj] + 1
-                q.append([ti, tj])
-                if box[ti][tj] > res:
-                    res = box[ti][tj] - 1
+    for i in range(m):
+        if 0 in g[i]:
+            res = -1
+            break
 
-for i in range(n):
-    if box[i].count(0) > 0:
-        res = -1
-        break
-
-print(res)
+    print(res, end="")
