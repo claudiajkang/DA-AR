d689106b3cc7a6362dce7469a1b723a3ee21e45e
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-10 15:48:49 +0900

    AG-501:[0h 7m] Review BOJ-1967 [Tr]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-1967.py
+++ DataStructures/Trees/BOJ-1967.py
@@ -1,9 +1,31 @@
 from collections import deque
-from sys import stdin, setrecursionlimit
-
-setrecursionlimit(10 ** 6)
+from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
+
+
+def dfs(num):
+    global g
+
+    visited = [-1] * len(g)
+    visited[num] = 0
+    q = deque()
+    q.append(num)
+    mi, m = 0, 0
+
+    while q:
+        cur = q.popleft()
+
+        for idx, val in g[cur]:
+            if visited[idx] == -1:
+                visited[idx] = visited[cur] + val
+                q.append(idx)
+                if visited[idx] > m:
+                    m = visited[idx]
+                    mi = idx
+
+    return mi, m
+
 
 n = int(read())
 g = [[] for i in range(n + 1)]
@@ -13,32 +35,7 @@ for i in range(n - 1):
     g[a].append([b, c])
     g[b].append([a, c])
 
+fi, f = dfs(1)
+si, s = dfs(fi)
 
-def dfs(nnum):
-    global g
-
-    q = deque()
-    q.append([nnum, 0])
-    d = 0
-    di = 0
-    visited = [False] * (n + 1)
-    visited[nnum] = True
-
-    while q:
-        cur, cd = q.popleft()
-
-        for jj, jd in g[cur]:
-            if not visited[jj]:
-                q.append([jj, cd + jd])
-                visited[jj] = True
-                if (cd + jd) > d:
-                    d = cd + jd
-                    di = jj
-
-    return di, d
-
-
-fidx, fv = dfs(1)
-sidx, sv = dfs(fidx)
-
-print(sv)
+print(s)
