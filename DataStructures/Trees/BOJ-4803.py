506a2d684128932ce130747c917d954946da7a37
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-12 05:40:43 +0900

    AG-525:*[0h 6m] Review BOJ-4803 [Tr]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-4803.py
+++ DataStructures/Trees/BOJ-4803.py
@@ -1,59 +1,57 @@
 from collections import deque
 from sys import stdin
 
-read = lambda: map(int, stdin.readline().rstrip().split())
-
-
-class Node:
-    def __init__(self):
-        self.data = 0
-        self.con = []
-
+read = lambda: stdin.readline().rstrip()
 
 case = 1
 while True:
-    n, m = read()
-    if [n, m] == [0, 0]:
+    n, m = map(int, read().split())
+
+    if n == 0 and m == 0:
         break
 
-    tree = [Node() for i in range(n + 1)]
+    tree = [[] for i in range(n + 1)]
 
     for i in range(m):
-        a, b = read()
-        tree[a].con.append(b)
-        tree[b].con.append(a)
+        a, b = map(int, read().split())
+        tree[a].append(b)
+        tree[b].append(a)
 
-    visited = [False] * (n + 1)
     prev = [-1] * (n + 1)
+    visited = [False] * (n + 1)
     result = 0
 
     for i in range(1, n + 1):
         if not visited[i]:
-            visited[i] = True
             q = deque()
             q.append(i)
+            visited[i] = True
             flag = True
 
             while q:
                 cur = q.popleft()
 
-                for j in tree[cur].con:
+                for j in tree[cur]:
                     if not visited[j]:
+                        prev[j] = cur
                         visited[j] = True
                         q.append(j)
-                        prev[j] = cur
 
-                    elif j != prev[cur]:
+                    elif prev[cur] != j:
                         flag = False
-                        break
+
+                if not flag:
+                    break
 
             if flag:
                 result += 1
 
     if result == 0:
         print(f"Case {case}: No trees.")
+
     elif result == 1:
         print(f"Case {case}: There is one tree.")
+
     else:
         print(f"Case {case}: A forest of {result} trees.")
 
