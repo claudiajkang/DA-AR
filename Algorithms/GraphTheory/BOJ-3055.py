79d9293e19079c76c4dba1a2060beb1ee8fc08a2
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-12 06:03:50 +0900

    AG-527:[0h 6m] Review BOJ-3055 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-3055.py
+++ Algorithms/GraphTheory/BOJ-3055.py
@@ -3,73 +3,76 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-r, c = map(int, read().split())
-m = [[] for i in range(r)]
-visited = [[False] * c for i in range(r)]
-p = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-d = [0, 0]
-w = deque()
-s = deque()
+h, w = map(int, read().split())
+g = [[] for i in range(h)]
 
-for i in range(r):
-    m[i] = list(read())
+dpos = [0, 0]
+spos = deque()
+water = deque()
 
-    for j in range(c):
-        if m[i][j] == 'D':
-            d = [i, j]
+visited = [[False] * w for i in range(h)]
 
-        elif m[i][j] == 'S':
-            s.append([i, j])
+for i in range(h):
+    g[i] = list(read())
+
+    for j in range(w):
+        if g[i][j] == 'D':
+            dpos = [i, j]
+
+        elif g[i][j] == 'S':
+            spos.append([i, j])
             visited[i][j] = True
-            m[i][j] = '.'
 
-        elif m[i][j] == '*':
-            w.append([i, j])
+        elif g[i][j] == '*':
+            water.append([i, j])
 
 suc = False
-res = 0
+time = 0
+p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
-while s:
-    res += 1
+while spos:
+    time += 1
 
-    qsize = len(s)
+    qsize = len(water)
     for i in range(qsize):
-        si, sj = s.popleft()
+        ch, cw = water.popleft()
 
-        if m[si][sj] == '*': continue
+        for hh, ww in p:
+            th = ch + hh
+            tw = cw + ww
 
-        for ii, jj in p:
-            ti, tj = si + ii, sj + jj
-
-            if ti < 0 or ti >= r or tj < 0 or tj >= c:
+            if th < 0 or th >= h or tw < 0 or tw >= w:
                 continue
 
-            if m[ti][tj] == '.' and not visited[ti][tj]:
-                visited[ti][tj] = True
-                m[ti][tj] = '..'
-                s.append([ti, tj])
+            if g[th][tw] == '.':
+                g[th][tw] = '*'
+                water.append([th, tw])
 
-            if [ti, tj] == d:
+    qsize = len(spos)
+
+    for i in range(qsize):
+        ch, cw = spos.popleft()
+
+        for hh, ww in p:
+            th = ch + hh
+            tw = cw + ww
+
+            if [th, tw] == dpos:
                 suc = True
                 break
 
-        if suc:
-            break
-    if suc:
-        break
-
-    qsize = len(w)
-    for i in range(qsize):
-        wi, wj = w.popleft()
-
-        for ii, jj in p:
-            ti, tj = wi + ii, wj + jj
-
-            if ti < 0 or ti >= r or tj < 0 or tj >= c:
+            if th < 0 or th >= h or tw < 0 or tw >= w:
                 continue
 
-            if '.' in m[ti][tj]:
-                m[ti][tj] = '*'
-                w.append([ti, tj])
+            if g[th][tw] == '.' and not visited[th][tw]:
+                spos.append([th, tw])
+                visited[th][tw] = True
 
-print(res if suc else "KAKTUS")
+        if suc: break
+
+    if suc: break
+
+if suc:
+    print(time)
+else:
+    print('KAKTUS')
