30d83e3437d8f62eb255abd2feb7f977284e2157
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-11 13:52:18 +0900

    AG-516:[0h 12m] Review BOJ-3482 [Tr]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-3482.py
+++ DataStructures/Trees/BOJ-3482.py
@@ -4,54 +4,48 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-
-def dfs(start, g):
-    global c, r
+def dfs(pos):
+    global g, c, r
 
     q = deque()
-    q.append(start)
-    g[start[0]][start[1]] = 0
-    midx = [0, 0]
+    q.append(pos)
+    g[pos[0]][pos[1]] = 0
+    mpos = [0, 0]
     m = 0
 
     while q:
         ci, cj = q.popleft()
-        for ii, jj in [[0, -1], [0, 1], [-1, 0], [1, 0]]:
-            ti = ci + ii
-            tj = cj + jj
 
-            if (0 <= ti < r) and (0 <= tj < c):
+        for ii, jj in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+            ti, tj = ii + ci, jj + cj
+
+            if (0 <= ti <= r) and (0 <= tj <= c):
                 if g[ti][tj] == '.':
                     g[ti][tj] = g[ci][cj] + 1
                     q.append([ti, tj])
                     if g[ti][tj] > m:
                         m = g[ti][tj]
-                        midx = [ti, tj]
+                        mpos = [ti, tj]
 
-    return midx, m
+    return mpos, m
 
 
 t = int(read())
-result = ""
 
 for tt in range(t):
     c, r = map(int, read().split())
-
-    g = [[] for i in range(r)]
-    init = False
-    start = [-1, -1]
+    g = [[0] * c for i in range(r)]
+    start = [0, 0]
 
     for i in range(r):
         g[i] = list(read())
 
-        if not init and g[i].count('.') > 0:
+        if start == [0, 0] and '.' in g[i]:
             start = [i, g[i].index('.')]
-            init = True
 
-    g2 = deepcopy(g)
-    fidx, fm = dfs(start, g)
-    sidx, sm = dfs(fidx, g2)
+    g1 = deepcopy(g)
+    fpos, f = dfs(start)
+    g = g1
+    spos, s = dfs(fpos)
 
-    result += f"Maximum rope length is {sm}.\n"
-
-print(result[:-1], end="")
+    print(f"Maximum rope length is {s}.")
