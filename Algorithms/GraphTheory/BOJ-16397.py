a49b5a4d9d84abf64857a7912b5e9d7e102fa92c
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-11 14:01:23 +0900

    AG-517:[0h 7m] Review BOJ-16397 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-16397.py
+++ Algorithms/GraphTheory/BOJ-16397.py
@@ -2,35 +2,40 @@ from collections import deque
 from sys import stdin
 
 n, t, g = map(int, stdin.readline().rstrip().split())
-btn = [-1] * (10 ** 6)
 
-q = deque()
-q.append(n)
-btn[n] = 0
-suc = False
+if n == g:
+    print(0)
 
-while q:
-    cur = q.popleft()
-
-    if btn[cur] > t: break
-
-    if cur == g:
-        suc = True
-        break
-
-    tp = [cur + 1, cur * 2]
-    for v in range(2):
-        i = tp[v]
-        if i > 99999 or i < 0: continue
-
-        if v == 1:
-            i -= 10 ** (len(str(i)) - 1)
-
-        if btn[i] == -1:
-            btn[i] = btn[cur] + 1
-            q.append(i)
-
-if suc:
-    print(btn[g], end="")
 else:
-    print('ANG', end="")
+    b = [-1] * 10 ** 6
+
+    q = deque()
+    q.append(n)
+    b[n] = 0
+    suc = False
+
+    while q:
+        cur = q.popleft()
+
+        if b[cur] > t:
+            break
+
+        if cur == g:
+            suc = True
+            break
+
+        temp = [cur + 1, cur * 2]
+
+        for i in range(2):
+            if temp[i] > 99999:
+                continue
+            if i and temp[i]:
+                temp[i] -= 10 ** (len(str(temp[i])) - 1)
+            if b[temp[i]] == -1:
+                q.append(temp[i])
+                b[temp[i]] = b[cur] + 1
+
+    if suc:
+        print(b[g])
+    else:
+        print("ANG")
