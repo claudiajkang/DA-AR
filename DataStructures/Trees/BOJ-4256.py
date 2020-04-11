d9953f44715b953f37be4dde107ac08cc9e267d6
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-11 13:36:00 +0900

    AG-515:[0h 4m] Review BOJ-4256 [Tr]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-4256.py
+++ DataStructures/Trees/BOJ-4256.py
@@ -1,28 +1,26 @@
-from sys import stdin, setrecursionlimit
-
-setrecursionlimit(10 ** 6)
+from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 
-def poso(po, io):
+def posto(po, io):
     global res
-    if len(po) == 0 or len(io) == 0:
-        return
+
+    if not po or not io: return
 
     idx = io.index(po[0])
-    poso(po[1:idx + 1], io[:idx])
-    poso(po[idx + 1:], io[idx + 1:])
-    res.append(po[0])
+    posto(po[1:idx + 1], io[:idx])
+    posto(po[idx + 1:], io[idx + 1:])
+    res.append(str(po[0]))
 
 
 t = int(read())
-result = ""
+
 for tt in range(t):
     n = int(read())
+
     preo = list(map(int, read().split()))
     ino = list(map(int, read().split()))
     res = []
-    poso(preo, ino)
-    result += ' '.join(map(str, res)) + '\n'
+    posto(preo, ino)
 
-print(result[:-1], end="")
+    print(' '.join(res))
