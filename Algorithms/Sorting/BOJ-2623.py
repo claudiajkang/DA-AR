376d5fd9ad2f2a9fa58630812696322dbbc2462b
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-13 06:21:47 +0900

    AG-542:*[0h 13m] Review BOJ-2623 [Sort]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-2623.py
+++ Algorithms/Sorting/BOJ-2623.py
@@ -1,38 +1,37 @@
 from collections import deque
 from sys import stdin
 
-read = lambda: map(int, stdin.readline().rstrip().split())
+read = lambda: stdin.readline().rstrip()
 
-n, m = read()
-indegree = [0] * (n + 1)
-adj = [[] for i in range(n + 1)]
+n, m = map(int, read().split())
+g = [[] for i in range(n + 1)]
+con = [0] * (n + 1)
 
 for i in range(m):
-    l = list(read())
-
+    l = list(map(int, read().split()))
     for j in range(1, l[0]):
-        adj[l[j]].append(l[j + 1])
-        indegree[l[j + 1]] += 1
-
-result = [0] * (n + 1)
+        g[l[j]].append(l[j + 1])
+        con[l[j + 1]] += 1
 
 q = deque()
 
 for i in range(1, n + 1):
-    if indegree[i] == 0:
+    if con[i] == 0:
         q.append(i)
+
+result = [0] * (n + 1)
 
 for i in range(1, n + 1):
     if not q:
-        print("0")
-        exit()
+        print('0')
+        break
 
     cur = q.popleft()
     result[i] = cur
 
-    for j in adj[cur]:
-        indegree[j] -= 1
-        if indegree[j] == 0:
+    for j in g[cur]:
+        con[j] -= 1
+        if con[j] == 0:
             q.append(j)
 
 for i in range(1, n + 1):
