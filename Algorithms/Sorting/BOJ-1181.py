0faff3f49cdfa8b6300645873e8bf3735e83880a
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-13 05:57:59 +0900

    AG-541:[0h 1m] Review BOJ-1181 [Sort]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-1181.py
+++ Algorithms/Sorting/BOJ-1181.py
@@ -3,14 +3,14 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
-arr = set()
+a = set()
 
 for i in range(n):
-    l = read()
-    arr.add(l)
+    a.add(read())
 
-arr = list(arr)
-arr.sort()
-arr.sort(key=lambda x: len(x))
+a = list(a)
+a.sort()
+a.sort(key=lambda x: len(x))
 
-print("\n".join(arr))
+for i in a:
+    print(i)
