0a6c0ae1f0c3acac3c69975fa07b2296615980de
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-13 05:49:19 +0900

    AG-531:*[0h 6m] Review BOJ-4803 [Tr]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- DataStructures/Trees/BOJ-4803.py
+++ DataStructures/Trees/BOJ-4803.py
@@ -1,24 +1,23 @@
 from collections import deque
 from sys import stdin
 
-read = lambda: stdin.readline().rstrip()
+read = lambda: map(int, stdin.readline().rstrip().split())
 
 case = 1
 while True:
-    n, m = map(int, read().split())
+    n, m = read()
 
-    if n == 0 and m == 0:
-        break
+    if n == 0 and m == 0: break
 
-    tree = [[] for i in range(n + 1)]
+    g = [[] for i in range(n + 1)]
 
     for i in range(m):
-        a, b = map(int, read().split())
-        tree[a].append(b)
-        tree[b].append(a)
+        a, b = read()
+        g[a].append(b)
+        g[b].append(a)
 
-    prev = [-1] * (n + 1)
     visited = [False] * (n + 1)
+    prev = [-1] * (n + 1)
     result = 0
 
     for i in range(1, n + 1):
@@ -31,27 +30,25 @@ while True:
             while q:
                 cur = q.popleft()
 
-                for j in tree[cur]:
+                for j in g[cur]:
                     if not visited[j]:
-                        prev[j] = cur
                         visited[j] = True
+                        prev[j] = cur
                         q.append(j)
 
-                    elif prev[cur] != j:
+                    elif j != prev[cur]:
                         flag = False
+                        break
 
-                if not flag:
-                    break
+                if not flag: break
 
             if flag:
                 result += 1
 
     if result == 0:
         print(f"Case {case}: No trees.")
-
     elif result == 1:
         print(f"Case {case}: There is one tree.")
-
     else:
         print(f"Case {case}: A forest of {result} trees.")
 
