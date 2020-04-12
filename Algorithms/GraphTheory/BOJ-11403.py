1d65fe13d29ee850d8273eb3d444fbcb30465585
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-13 06:48:56 +0900

    AG-532:*[0h 12m] Review BOJ-11403 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-11403.py
+++ Algorithms/GraphTheory/BOJ-11403.py
@@ -13,34 +13,37 @@ for i in range(1, n + 1):
         if l[j] == 1:
             g[i].append(j)
 
-res = [[0] * (n + 1) for i in range(n + 1)]
+result = [[-1] * (n + 1) for i in range(n + 1)]
+visited = [[False] * (n + 1) for i in range(n + 1)]
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        q = deque()
-        q.append(i)
-        dfs = []
-        flag = False
-        if i == j:
-            init = False
-        else:
-            init = True
-        while q:
-            cur = q.popleft()
+        if result[i][j] == -1:
+            q = deque()
+            q.append(i)
+            dfs = []
+            init = False if i == j else True
+            err = True
 
-            if cur == j and init:
-                flag = True
-                break
+            while q:
+                cur = q.popleft()
 
-            if i == j and not init:
-                init = True
+                if init and cur == j:
+                    result[i][j] = 1
+                    err = False
+                    break
 
-            for t in g[cur]:
-                if t not in dfs:
-                    q.append(t)
-                    dfs.append(t)
+                if not init: init = True
 
-        res[i][j] = 1 if flag else 0
+                for k in g[cur]:
+                    if k not in dfs:
+                        result[cur][k] = 1
+                        result[i][k] = 1
+                        q.append(k)
+                        dfs.append(k)
+
+            if err:
+                result[i][j] = 0
 
 for i in range(1, n + 1):
-    print(' '.join(map(str, res[i][1:])))
+    print(' '.join(map(str, result[i][1:])))
