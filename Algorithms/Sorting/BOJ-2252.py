2cb6bdd6330728c3eed0285b49330df17ea86b3f
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-12 14:16:03 +0900

    AG-115:*[0h 39m] BOJ-2252 [Sort] 줄 세우기
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

---
+++ Algorithms/Sorting/BOJ-2252.py
@@ -0,0 +1,36 @@
+from collections import deque
+from sys import stdin
+
+read = lambda: map(int, stdin.readline().rstrip().split())
+
+n, m = read()
+g = [[] for i in range(n + 1)]
+con = [0] * (n + 1)
+
+for i in range(m):
+    a, b = read()
+    g[b].append(a)
+    g[a].append(b)
+    con[b] += 1
+
+q = deque()
+result = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    if con[i] == 0:
+        q.append(i)
+
+for i in range(1, n + 1):
+    if not q:
+        print("0")
+        exit()
+
+    cur = q.popleft()
+    result[i] = cur
+
+    for j in g[cur]:
+        con[j] -= 1
+        if con[j] == 0:
+            q.append(j)
+
+print(' '.join(map(str, result[1:])))
