6707a154803500f191f7a2b1490dedb36adca564
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-13 16:16:51 +0900

    AG-539:[0h 5m] Review BOJ-9019 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-9019.py
+++ Algorithms/GraphTheory/BOJ-9019.py
@@ -7,43 +7,30 @@ t = int(read())
 
 for tt in range(t):
     a, b = map(int, read().split())
+
     visited = [False] * 10000
 
     q = deque()
     q.append([a, ''])
+
+    v = ['D', 'S', 'L', 'R']
     cmd = ''
-    suc = False
 
     while q:
-        qsize = len(q)
+        n, cmd = q.popleft()
 
-        for i in range(qsize):
-            cur, cmd = q.popleft()
+        if n == b:
+            break
 
-            if cur == b:
-                suc = True
-                break
+        d = 2 * n if (2 * n) <= 9999 else (2 * n) % 10000
+        s = n - 1 if n > 0 else 9999
+        l = (n * 10) % 10000 + (n // 1000)
+        r = (n // 10) + (n % 10) * 1000
 
-            d = (cur * 2) if (cur * 2) <= 9999 else (cur * 2) % 10000
-            if not visited[d]:
-                q.append([d, cmd + 'D'])
-                visited[d] = True
-
-            s = (cur - 1) if cur > 0 else 9999
-            if not visited[s]:
-                q.append([s, cmd + 'S'])
-                visited[s] = True
-
-            l = (cur * 10) % 10000 + (cur * 10) // 10000
-            if not visited[l]:
-                q.append([l, cmd + 'L'])
-                visited[l] = True
-
-            r = (cur // 10) + (cur % 10) * 1000
-            if not visited[r]:
-                q.append([r, cmd + 'R'])
-                visited[r] = True
-
-        if suc: break
+        tmp = [d, s, l, r]
+        for i in range(4):
+            if not visited[tmp[i]]:
+                visited[tmp[i]] = True
+                q.append([tmp[i], cmd + v[i]])
 
     print(cmd)
