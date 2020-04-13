8d360a594b499b288a75e4246693d29f59eb1921
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-13 15:19:59 +0900

    AG-536:*[0h 17m] Review BOJ-5427 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-5427.py
+++ Algorithms/GraphTheory/BOJ-5427.py
@@ -8,68 +8,61 @@ t = int(read())
 for tt in range(t):
     w, h = map(int, read().split())
 
-    g = [[] for i in range(h)]
+    b = [[] for i in range(h)]
     visited = [[False] * w for i in range(h)]
-
-    f = deque()
-    s = deque()
+    fire = deque()
+    pos = deque()
 
     for i in range(h):
-        g[i] = list(read())
+        b[i] = list(read())
 
         for j in range(w):
-            if g[i][j] == '@':
-                s.append([i, j])
+            if b[i][j] == '*':
+                fire.append([i, j])
+            elif b[i][j] == '@':
+                pos.append([i, j])
                 visited[i][j] = True
 
-            elif g[i][j] == '*':
-                f.append([i, j])
-
     p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-    suc = False
     time = 0
+    suc = False
 
-    while s:
+    while pos:
         time += 1
-        qsize = len(s)
+
+        qsize = len(pos)
 
         for i in range(qsize):
-            ch, cw = s.popleft()
+            ch, cw = pos.popleft()
 
-            if g[ch][cw] == '*':
-                continue
+            if b[ch][cw] == '*': continue
 
-            for ww, hh in p:
-                tw = ww + cw
-                th = hh + ch
-
-                if tw < 0 or tw >= w or th < 0 or th >= h:
+            for hh, ww in p:
+                th, tw = ch + hh, cw + ww
+                if th < 0 or th >= h or tw < 0 or tw >= w:
                     suc = True
                     break
 
-                if g[th][tw] == '.' and not visited[th][tw]:
-                    s.append([th, tw])
+                if b[th][tw] == '.' and not visited[th][tw]:
                     visited[th][tw] = True
+                    pos.append([th, tw])
 
             if suc: break
 
         if suc: break
 
-        qsize = len(f)
-
+        qsize = len(fire)
         for i in range(qsize):
-            ch, cw = f.popleft()
+            ch, cw = fire.popleft()
 
-            for ww, hh in p:
-                tw = ww + cw
-                th = hh + ch
-
-                if tw < 0 or tw >= w or th < 0 or th >= h:
+            for hh, ww in p:
+                th, tw = ch + hh, cw + ww
+                if th < 0 or th >= h or tw < 0 or tw >= w:
                     continue
 
-                if g[th][tw] == '.':
-                    f.append([th, tw])
-                    g[th][tw] = '*'
+                if b[th][tw] == '.':
+                    fire.append([th, tw])
+                    b[th][tw] = '*'
 
     if suc:
         print(time)
