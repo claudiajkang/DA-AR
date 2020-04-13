04d92f37e79c0bc69ef609728fe338cfab9509e8
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-13 15:54:28 +0900

    AG-538:*[0h 9m] Review BOJ-2206 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-2206.py
+++ Algorithms/GraphTheory/BOJ-2206.py
@@ -1,33 +1,51 @@
+from collections import deque
+from sys import stdin
+
+read = lambda: stdin.readline().rstrip()
+
+h, w = map(int, read().split())
+g = [[] for i in range(h)]
+visited = [[[False] * w for i in range(h)] for j in range(2)]
+
+for i in range(h):
+    g[i] = list(map(int, list(read())))
+
+q = deque()
+q.append([0, 0, 0])
+p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+suc = False
+time = 0
 
 while q:
-    qsize = len(q)
+    qlen = len(q)
     time += 1
 
-    for i in range(qsize):
+    for i in range(qlen):
         crash, ch, cw = q.popleft()
 
         if ch == (h - 1) and cw == (w - 1):
             suc = True
             break
 
-        for hh, ww in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+        for hh, ww in p:
             th = hh + ch
             tw = ww + cw
 
-            if 0 < th or th >= h or 0 < tw or tw >= w: continue
-
-            if visited[crash][th][tw]: continue
-
-            if g[th][tw] and crash:
+            if th < 0 or th >= h or tw < 0 or tw >= w:
                 continue
 
-            elif g[th][tw] and not crash:
-                visited[1][th][tw] = True
-                q.append([1, th, tw])
+            if g[th][tw] == 1:
+                if crash:
+                    continue
+                else:
+                    if not crash and not visited[1][th][tw]:
+                        visited[1][th][tw] = True
+                        q.append([1, th, tw])
 
-            else:
-                visited[crash][th][tw] = True
-                q.append([crash, th, tw])
+            if g[th][tw] == 0:
+                if not visited[crash][th][tw]:
+                    visited[crash][th][tw] = True
+                    q.append([crash, th, tw])
 
     if suc: break
 
