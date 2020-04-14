f8a69eacc73b9f23da4ab840be8985d74156c2cd
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-14 11:06:41 +0900

    AG-552:[0h 3m] Review BOJ-1766 [Sort]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-1766.py
+++ Algorithms/Sorting/BOJ-1766.py
@@ -4,29 +4,27 @@ from sys import stdin
 read = lambda: map(int, stdin.readline().rstrip().split())
 
 n, m = read()
-prob = [[] for i in range(n + 1)]
+g = [[] for i in range(n + 1)]
 con = [0] * (n + 1)
 
 for i in range(m):
     a, b = read()
-    prob[b].append(a)
-    prob[a].append(b)
+    g[a].append(b)
     con[b] += 1
 
 q = []
-result = [0] * (n + 1)
 
 for i in range(1, n + 1):
     if con[i] == 0:
         heappush(q, i)
 
-for i in range(1, n + 1):
-    if not q: break
+result = [0] * (n + 1)
 
+for i in range(1, n + 1):
     cur = heappop(q)
     result[i] = cur
 
-    for j in prob[cur]:
+    for j in g[cur]:
         con[j] -= 1
         if con[j] == 0:
             heappush(q, j)
