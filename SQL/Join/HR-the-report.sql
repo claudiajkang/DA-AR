394d2404c1d19671c790b3faf8de0b709d507ded
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-14 17:30:06 +0900

    AG-563:*[0h 10m] Review HR-The report [Join]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- SQL/Join/HR-the-report.sql
+++ SQL/Join/HR-the-report.sql
@@ -1,4 +1,4 @@
-select IF(g.grade < 8, NULL, s.name), g.grade, s.marks
-from students s
-         inner join grades g on s.marks between g.min_mark and g.max_mark
-order by g.grade DESC, s.name, s.marks;
+SELECT IF(G.Grade < 8, NULL, S.NAME), G.Grade, S.MARKS
+FROM Students S
+         JOIN Grades G ON S.MARKS BETWEEN G.Min_Mark and G.Max_Mark
+ORDER BY G.Grade desc, S.Name, S.marks
