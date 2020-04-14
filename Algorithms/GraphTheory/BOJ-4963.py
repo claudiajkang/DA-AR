3e8839e69920e17901eac8c939030b32481b1e17
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-14 11:19:34 +0900

    AG-553:*[0h 8m] Review BOJ-4963 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-4963.py
+++ Algorithms/GraphTheory/BOJ-4963.py
@@ -1,43 +1,47 @@
 from collections import deque
 from sys import stdin
 
-read = lambda: map(int, stdin.readline().rstrip().split())
+read = lambda: stdin.readline().rstrip()
 
 while True:
-    w, h = read()
+    w, h = map(int, read().split())
 
-    if w == 0 and h == 0: break
+    if [w, h] == [0, 0]: break
 
     g = [[] for i in range(h)]
+
     for i in range(h):
-        g[i] = list(read())
+        g[i] = list(map(int, read().split()))
 
     if w == h and h == 1:
         print(g[0][0])
         continue
 
-    l = 0
+    res = 0
+    visited = [[False] * w for i in range(h)]
 
     for i in range(h):
         for j in range(w):
-            if g[i][j] == 1:
+            if g[i][j] == 1 and not visited[i][j]:
                 q = deque()
                 q.append([i, j])
-                g[i][j] = 0
+                visited[i][j] = True
 
                 while q:
                     ch, cw = q.popleft()
 
-                    for hh, ww in [[-1, -1], [-1, 1], [-1, 0], [1, 0], [0, -1], [0, 1], [1, -1], [1, 1]]:
-                        th, tw = hh + ch, ww + cw
+                    for hh, ww in [[0, -1], [0, 1], [-1, 0], [1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]]:
+                        th = ch + hh
+                        tw = cw + ww
 
-                        if th < 0 or th >= h or tw < 0 or tw >= w:
+                        if th < 0 or tw < 0 or th >= h or tw >= w:
                             continue
 
-                        if g[th][tw] == 1:
+                        if g[th][tw] == 1 and not visited[th][tw]:
                             q.append([th, tw])
+                            visited[th][tw] = True
                             g[th][tw] = 0
 
-                l += 1
+                res += 1
 
-    print(l)
+    print(res)
