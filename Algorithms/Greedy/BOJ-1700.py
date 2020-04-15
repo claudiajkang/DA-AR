cd0878824721ba1a7b06881788efa23ba0088bfe
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-16 06:24:30 +0900

    AG-587:*[0h 13m] Review BOJ-1700 [Gd]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Greedy/BOJ-1700.py
+++ Algorithms/Greedy/BOJ-1700.py
@@ -3,48 +3,37 @@ from sys import stdin
 read = lambda: map(int, stdin.readline().rstrip().split())
 
 n, k = read()
-order = list(read())
-elec = {i: [] for i in set(order)}
+ed = list(read())
+ec = {i: [] for i in set(ed)}
 
 for i in range(k):
-    o = order[i]
-    elec[o].append(i)
+    ec[ed[i]].append(i)
 
-res = 0
 q = []
+res = 0
 
 for i in range(k):
-    o = order[i]
+    o = ed[i]
+
     if o in q:
-        if elec[o]: elec[o].pop(0)
+        if ec[o]: ec[o].pop(0)
         continue
 
-    if len(q) < n:
-        if elec[o]: elec[o].pop(0)
-        q.append(o)
-        continue
+    if len(q) >= n:
+        temp = []
+        for j in q:
+            if len(ec[j]) == 0:
+                temp.append([101, j])
+                break
+            else:
+                temp.append([ec[j][0], j])
 
-    temp = []
-    isZero = False
-    zi = 0
-
-    for t in q:
-        if len(elec[t]) == 0:
-            isZero = True
-            zi = t
-            break
-
-        temp.append([elec[t], t])
-
-    if isZero:
-        q.pop(q.index(zi))
-    else:
         temp.sort(reverse=True, key=lambda x: x[0])
-        t = q.pop(q.index(temp[0][1]))
+        q.pop(q.index(temp[0][1]))
+        res += 1
 
-    if elec[o]:
-        elec[o].pop(0)
     q.append(o)
-    res += 1
+    if ec[o]:
+        ec[o].pop(0)
 
 print(res)
