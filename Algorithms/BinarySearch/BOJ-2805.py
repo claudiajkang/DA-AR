187528217f38e79c884e9aa4d1274f250b643e25
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 21:16:40 +0900

    AG-579:*[0h 14m] Review BOJ-2805 [BS]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-2805.py
+++ Algorithms/BinarySearch/BOJ-2805.py
@@ -1,19 +1,20 @@
 from sys import stdin
 
-n, m = map(int, stdin.readline().rstrip().split())
-height = list(map(int, stdin.readline().rstrip().split()))
+read = lambda: map(int, stdin.readline().rstrip().split())
+
+n, m = read()
+tree = list(read())
+tree.sort(reverse=True)
 
 lo = 0
-hi = max(height)
+hi = max(tree)
 
 while (lo + 1) < hi:
     mid = (lo + hi) // 2
-    s = 0
 
-    for i in range(n):
-        if height[i] <= mid:
-            continue
-        s += height[i] - mid
+    s = 0
+    for h in tree:
+        s += (h - mid) if h > mid else 0
 
     if s >= m:
         lo = mid
