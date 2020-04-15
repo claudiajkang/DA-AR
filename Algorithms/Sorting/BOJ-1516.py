38a5a7dfd2751f1d7fd597b8315d1111b9cfa437
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 16:22:43 +0900

    AG-574:*[0h 10m] Review BOJ-1516 [Sort]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-1516.py
+++ Algorithms/Sorting/BOJ-1516.py
@@ -4,33 +4,36 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
-b = [[] for i in range(n + 1)]
-con = [0] * (n + 1)
+g = [[] for i in range(n + 1)]
 time = [0] * (n + 1)
+con = [0] * (n + 1)
 
 for i in range(1, n + 1):
     l = list(map(int, read().split()))
     time[i] = l[0]
 
     for j in l[1:]:
-        if j != -1:
-            b[j].append(i)
-            con[i] += 1
+        if j == -1: break
+        g[j].append(i)
+        con[i] += 1
 
 q = []
-restime = [0] * (n + 1)
+res = [0] * (n + 1)
+
 for i in range(1, n + 1):
     if con[i] == 0:
-        heappush(q, [con[i], i])
-        restime[i] = time[i]
+        heappush(q, i)
+        res[i] = time[i]
 
 for i in range(1, n + 1):
-    cons, cur = heappop(q)
+    if not q: break
 
-    for j in b[cur]:
+    cur = heappop(q)
+
+    for j in g[cur]:
         con[j] -= 1
-        restime[j] = max(restime[j], restime[cur] + time[j])
+        res[j] = max(res[j], res[cur] + time[j])
         if con[j] == 0:
-            heappush(q, [con[i], j])
+            heappush(q, j)
 
-print('\n'.join(map(str, restime[1:])))
+print('\n'.join(map(str, res[1:])))
