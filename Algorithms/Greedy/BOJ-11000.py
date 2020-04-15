348984132e74c1f2176383f300fcdb9bfa79070a
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 21:50:43 +0900

    AG-578:*[0h 4m] Review BOJ-11000 [Gd]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Greedy/BOJ-11000.py
+++ Algorithms/Greedy/BOJ-11000.py
@@ -3,21 +3,21 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-N = int(read())
-LECTURE = [[] for i in range(N)]
+n = int(read())
+time = [[] for i in range(n)]
 
-for i in range(N):
-    LECTURE[i] = list(map(int, read().split()))
+for i in range(n):
+    time[i] = list(map(int, read().split()))
 
-LECTURE.sort(key=lambda x: x[1])
-LECTURE.sort(key=lambda x: x[0])
+time.sort(key=lambda x: x[1])
+time.sort(key=lambda x: x[0])
 
-CLASSES = []
+q = []
+heappush(q, time[0][1])
 
-for i in range(N):
-    if CLASSES and LECTURE[i][0] >= CLASSES[0]:
-        heappop(CLASSES)
+for i in range(1, n):
+    if q and q[0] <= time[i][0]:
+        heappop(q)
+    heappush(q, time[i][1])
 
-    heappush(CLASSES, LECTURE[i][1])
-
-print(len(CLASSES))
+print(len(q))
