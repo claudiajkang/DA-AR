349ff50c35c1d5c65efa8bbde6a4b3e440fde6b4
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 12:39:01 +0900

    AG-566:[0h 5m] Review BOJ-4963 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-4963.py
+++ Algorithms/GraphTheory/BOJ-4963.py
@@ -3,43 +3,45 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-n = int(read())
-g = [[] for i in range(n)]
-res = [[-1] * n for i in range(n)]
+while True:
+    w, h = map(int, read().split())
 
-for i in range(n):
-    l = list(map(int, read().split()))
+    if [w, h] == [0, 0]: break
 
-    for j in range(n):
-        if l[j] == 1:
-            g[i].append(j)
+    g = [[] for i in range(h)]
 
-for i in range(n):
-    for j in range(n):
-        if res[i][j] == -1:
-            q = deque()
-            q.append(i)
-            dfs = []
-            init = False if i == j else True
+    for i in range(h):
+        g[i] = list(map(int, read().split()))
 
-            while q:
-                cur = q.popleft()
+    if w == h and h == 1:
+        print(g[0][0])
+        continue
 
-                if cur == j and init:
-                    res[i][j] = 1
-                    break
+    visited = [[False] * w for i in range(h)]
 
-                if not init: init = True
+    island = 0
 
-                for k in g[cur]:
-                    if k not in dfs:
-                        q.append(k)
-                        dfs.append(k)
-                        res[cur][k] = 1
-                        res[i][k] = 1
+    p = [[0, -1], [0, 1], [-1, 0], [1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]]
 
-            if res[i][j] == -1:
-                res[i][j] = 0
+    for i in range(h):
+        for j in range(w):
+            if g[i][j] == 1 and not visited[i][j]:
+                island += 1
+                visited[i][j] = True
+                q = deque()
+                q.append([i, j])
 
-for i in range(n):
-    print(' '.join(map(str, res[i])))
+                while q:
+                    ch, cw = q.popleft()
+
+                    for hh, ww in p:
+                        th = ch + hh
+                        tw = cw + ww
+
+                        if th < 0 or tw < 0 or th >= h or tw >= w: continue
+
+                        if g[th][tw] == 1 and not visited[th][tw]:
+                            visited[th][tw] = True
+                            q.append([th, tw])
+
+    print(island)
