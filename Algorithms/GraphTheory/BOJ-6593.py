16b6793c6636ad3ee45a1b172ad20d60ef5e2b0b
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 13:31:57 +0900

    AG-570:*[0h 30m] Review BOJ-6593 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-6593.py
+++ Algorithms/GraphTheory/BOJ-6593.py
@@ -3,55 +3,45 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-result = ""
 while True:
-    l, r, c = map(int, read().split())
+    l, h, w = map(int, read().split())
 
-    if [l, r, c] == [0, 0, 0]:
-        print(result[:-1], end="")
-        break
+    if [l, h, w] == [0, 0, 0]: break
 
-    g = [[['#'] * c for i in range(r)] for k in range(l)]
-    start = [0, 0, 0]
-    end = [0, 0, 0]
+    g = [[[] for i in range(h)] for k in range(l)]
+    q = deque()
 
-    for ll in range(l):
-        for rr in range(r):
-            g[ll][rr] = list(read())
+    for i in range(l):
+        for j in range(h):
+            g[i][j] = list(read())
+            if 'S' in g[i][j]: q.append([i, j, g[i][j].index('S')])
 
-            if 'S' in g[ll][rr]:
-                start = [ll, rr, g[ll][rr].index('S')]
-
-            if 'E' in g[ll][rr]:
-                end = [ll, rr, g[ll][rr].index('E')]
-
-        temp = read()
+        t = read()
 
     p = [[-1, 0, 0], [1, 0, 0], [0, 0, -1], [0, 0, 1], [0, -1, 0], [0, 1, 0]]
-
-    g[start[0]][start[1]][start[2]] = 0
-    q = deque()
-    q.append(start)
-    done = False
-    res = 0
+    suc = False
+    g[q[0][0]][q[0][1]][q[0][2]] = 0
+    time = 0
 
     while q:
-        cur = q.popleft()
+        cl, ch, cw = q.popleft()
 
-        for ll, rr, cc in p:
-            tl, tr, tc = cur[0] + ll, cur[1] + rr, cur[2] + cc
+        for ll, hh, ww in p:
+            tl = ll + cl
+            th = hh + ch
+            tw = ww + cw
 
-            if (0 <= tl < l) and (0 <= tr < r) and (0 <= tc < c):
-                if [tl, tr, tc] == end:
-                    res = g[cur[0]][cur[1]][cur[2]] + 1
-                    done = True
+            if (0 <= tl < l) and (0 <= th < h) and (0 <= tw < w):
+                if g[tl][th][tw] == 'E':
+                    suc = True
+                    time = g[cl][ch][cw] + 1
                     break
 
-                if g[tl][tr][tc] == '.':
-                    g[tl][tr][tc] = g[cur[0]][cur[1]][cur[2]] + 1
-                    q.append([tl, tr, tc])
+                if g[tl][th][tw] == '.':
+                    g[tl][th][tw] = g[cl][ch][cw] + 1
+                    q.append([tl, th, tw])
 
-    if done:
-        result += f"Escaped in {res} minute(s).\n"
+    if suc:
+        print(f'Escaped in {time} minute(s).')
     else:
-        result += f"Trapped!\n"
+        print("Trapped!")
