7b967043219a97752df4d1023c59df7692f1af8d
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-16 06:39:45 +0900

    AG-588:*[0h 11m] Review BOJ-2805 [BS]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-2805.py
+++ Algorithms/BinarySearch/BOJ-2805.py
@@ -1,6 +1,24 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
 
+setrecursionlimit(10 * 6)
 read = lambda: map(int, stdin.readline().rstrip().split())
+
+
+def search(lo, hi):
+    global tree
+
+    mid = (lo + hi) // 2
+
+    s = 0
+    for t in tree:
+        if t <= mid: break
+        s += (t - mid)
+
+    if s < m:
+        return lo, mid - 1
+    else:
+        return mid + 1, hi
+
 
 n, m = read()
 tree = list(read())
@@ -9,16 +27,7 @@ tree.sort(reverse=True)
 lo = 0
 hi = max(tree)
 
-while (lo + 1) < hi:
-    mid = (lo + hi) // 2
+while lo <= hi:
+    lo, hi = search(lo, hi)
 
-    s = 0
-    for h in tree:
-        s += (h - mid) if h > mid else 0
-
-    if s >= m:
-        lo = mid
-    else:
-        hi = mid
-
-print(lo)
+print(lo - 1)
