346597c8d43b677b09e60ab8ec610329227c5f5b
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 20:58:49 +0900

    AG-575:*[0h 9m] Review BOJ-9470 [Sort]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-9470.py
+++ Algorithms/Sorting/BOJ-9470.py
@@ -7,10 +7,9 @@ t = int(read())
 
 for tt in range(t):
     k, m, p = map(int, read().split())
-
     g = [[] for i in range(m + 1)]
     con = [0] * (m + 1)
-    con2 = [0] * (m + 1)
+    visited = [False] * (m + 1)
 
     for i in range(p):
         a, b = map(int, read().split())
@@ -19,32 +18,31 @@ for tt in range(t):
 
     q = []
     res = [0] * (m + 1)
-    visited = [False] * (m + 1)
+    rescon = [0] * (m + 1)
 
     for i in range(1, m + 1):
         if con[i] == 0:
-            heappush(q, [con[i], i])
             res[i] = 1
-            visited[i] = True
+            heappush(q, [res[i], i])
 
-    while q:
-        qlen = len(q)
+    for i in range(1, m + 1):
+        if not q: break
 
-        for i in range(qlen):
-            tcon, cur = heappop(q)
+        order, cur = heappop(q)
 
-            for j in g[cur]:
-                con[j] -= 1
+        for j in g[cur]:
+            con[j] -= 1
+            if order > res[j]:
+                res[j] = order
+                rescon[j] = 1
 
-                if res[cur] > res[j]:
-                    con2[j] = 1
-                    res[j] = res[cur]
-                elif res[cur] == res[j]:
-                    con2[j] += 1
+            elif res[j] == order:
+                rescon[j] += 1
 
-                if con[j] == 0 and not visited[j]:
-                    visited[j] = True
-                    if con2[j] >= 2: res[j] += 1
-                    heappush(q, [con[j], j])
+            if con[j] == 0:
+                if rescon[j] > 1:
+                    res[j] += 1
+
+                heappush(q, [res[j], j])
 
     print(f"{k} {max(res)}")
