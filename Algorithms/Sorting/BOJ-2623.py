f3049b61ee53b954513772ab7bd2ccf93f2ca52c
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 16:04:00 +0900

    AG-573:*[0h 6m] Review BOJ-2623 [Sort]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-2623.py
+++ Algorithms/Sorting/BOJ-2623.py
@@ -1,18 +1,18 @@
 from collections import deque
 from sys import stdin
 
-read = lambda: map(int, stdin.readline().rstrip().split())
+read = lambda: stdin.readline().rstrip()
 
-n, m = read()
-arr = [[] for i in range(n + 1)]
+n, m = map(int, read().split())
+g = [[] for i in range(n + 1)]
 con = [0] * (n + 1)
 
 for i in range(m):
-    l = list(read())
+    l = list(map(int, read().split()))
 
-    for j in range(2, l[0] + 1):
-        arr[l[j - 1]].append(l[j])
-        con[l[j]] += 1
+    for j in range(1, l[0]):
+        g[l[j]].append(l[j + 1])
+        con[l[j + 1]] += 1
 
 q = deque()
 
@@ -20,19 +20,23 @@ for i in range(1, n + 1):
     if con[i] == 0:
         q.append(i)
 
-result = [-1] * (n + 1)
+result = [0] * (n + 1)
+err = False
 
 for i in range(1, n + 1):
     if not q:
-        print(0)
-        exit()
+        err = True
+        break
 
     cur = q.popleft()
     result[i] = cur
 
-    for j in arr[cur]:
+    for j in g[cur]:
         con[j] -= 1
         if con[j] == 0:
             q.append(j)
 
-print('\n'.join(map(str, result[1:])))
+if err:
+    print(0)
+else:
+    print('\n'.join(map(str, result[1:])))
