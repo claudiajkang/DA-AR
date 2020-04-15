2365e205127c173ed332e70eba11242cffd4b9d0
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 14:12:04 +0900

    AG-582:[0h 5m] Review BOJ-2206 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-2206.py
+++ Algorithms/GraphTheory/BOJ-2206.py
@@ -5,21 +5,22 @@ read = lambda: stdin.readline().rstrip()
 
 h, w = map(int, read().split())
 g = [[] for i in range(h)]
-visited = [[[False] * w for i in range(h)] for j in range(2)]
 
 for i in range(h):
     g[i] = list(map(int, list(read())))
 
 q = deque()
 q.append([0, 0, 0])
-p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-suc = False
+visited = [[[False] * w for i in range(h)] for j in range(2)]
+
 time = 0
+suc = False
+p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
 while q:
-    qlen = len(q)
     time += 1
 
+    qlen = len(q)
     for i in range(qlen):
         crash, ch, cw = q.popleft()
 
@@ -31,14 +32,14 @@ while q:
             th = hh + ch
             tw = ww + cw
 
-            if th < 0 or th >= h or tw < 0 or tw >= w:
+            if th < 0 or tw < 0 or th >= h or tw >= w:
                 continue
 
             if g[th][tw] == 1:
                 if crash:
                     continue
                 else:
-                    if not crash and not visited[1][th][tw]:
+                    if not visited[1][th][tw]:
                         visited[1][th][tw] = True
                         q.append([1, th, tw])
 
