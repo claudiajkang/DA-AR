4503acf469cdd43334cb1bd6b01f6878d0db063c
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-15 12:46:32 +0900

    AG-567:[0h 5m] Review BOJ-11403 [Gp]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-11403.py
+++ Algorithms/GraphTheory/BOJ-11403.py
@@ -4,46 +4,43 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
-g = [[] for i in range(n + 1)]
+g = [[] for i in range(n)]
 
-for i in range(1, n + 1):
-    l = [0] + list(map(int, read().split()))
-
-    for j in range(1, n + 1):
+for i in range(n):
+    l = list(map(int, read().split()))
+    for j in range(n):
         if l[j] == 1:
             g[i].append(j)
 
-result = [[-1] * (n + 1) for i in range(n + 1)]
-visited = [[False] * (n + 1) for i in range(n + 1)]
+result = [[-1] * n for i in range(n)]
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
+for i in range(n):
+    for j in range(n):
         if result[i][j] == -1:
             q = deque()
             q.append(i)
-            dfs = []
             init = False if i == j else True
-            err = True
+            dfs = []
 
             while q:
                 cur = q.popleft()
 
                 if init and cur == j:
                     result[i][j] = 1
-                    err = False
                     break
 
-                if not init: init = True
+                if not init:
+                    init = True
 
                 for k in g[cur]:
                     if k not in dfs:
-                        result[cur][k] = 1
                         result[i][k] = 1
+                        result[cur][k] = 1
                         q.append(k)
                         dfs.append(k)
 
-            if err:
+            if result[i][j] == -1:
                 result[i][j] = 0
 
-for i in range(1, n + 1):
-    print(' '.join(map(str, result[i][1:])))
+for i in range(n):
+    print(' '.join(map(str, result[i])))
