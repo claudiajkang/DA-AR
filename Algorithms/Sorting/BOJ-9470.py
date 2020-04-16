41cf543a0ca864807832f8622b595b3228c3420e
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-16 11:10:50 +0900

    AG-593:*[0h 20m] Review BOJ-9470 [Sort]
    Signed-off-by: Claudia J.Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-9470.py
+++ Algorithms/Sorting/BOJ-9470.py
@@ -4,21 +4,19 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 t = int(read())
-
 for tt in range(t):
     k, m, p = map(int, read().split())
     g = [[] for i in range(m + 1)]
     con = [0] * (m + 1)
-    visited = [False] * (m + 1)
 
     for i in range(p):
         a, b = map(int, read().split())
         g[a].append(b)
         con[b] += 1
 
-    q = []
     res = [0] * (m + 1)
-    rescon = [0] * (m + 1)
+    rcon = [0] * (m + 1)
+    q = []
 
     for i in range(1, m + 1):
         if con[i] == 0:
@@ -28,19 +26,20 @@ for tt in range(t):
     for i in range(1, m + 1):
         if not q: break
 
-        order, cur = heappop(q)
+        co, cur = heappop(q)
 
         for j in g[cur]:
             con[j] -= 1
-            if order > res[j]:
-                res[j] = order
-                rescon[j] = 1
 
-            elif res[j] == order:
-                rescon[j] += 1
+            if res[cur] > res[j]:
+                res[j] = res[cur]
+                rcon[j] = 1
+
+            elif res[cur] == res[j]:
+                rcon[j] += 1
 
             if con[j] == 0:
-                if rescon[j] > 1:
+                if rcon[j] > 1:
                     res[j] += 1
 
                 heappush(q, [res[j], j])
