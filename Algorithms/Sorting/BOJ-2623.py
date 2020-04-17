a469ad3697cbce3f1214d9005827ab661416a671
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-17 21:08:16 +0900

    AG-598:[0h 4m] Review BOJ-2623 [Sort]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-2623.py
+++ Algorithms/Sorting/BOJ-2623.py
@@ -4,23 +4,23 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n, m = map(int, read().split())
-g = [[] for i in range(n + 1)]
+s = [[] for i in range(n + 1)]
 con = [0] * (n + 1)
 
 for i in range(m):
     l = list(map(int, read().split()))
 
     for j in range(1, l[0]):
-        g[l[j]].append(l[j + 1])
+        s[l[j]].append(l[j + 1])
         con[l[j + 1]] += 1
 
 q = deque()
-result = [0] * (n + 1)
 
 for i in range(1, n + 1):
     if con[i] == 0:
         q.append(i)
 
+result = [0] * (n + 1)
 err = False
 for i in range(1, n + 1):
     if not q:
@@ -30,7 +30,7 @@ for i in range(1, n + 1):
     cur = q.popleft()
     result[i] = cur
 
-    for j in g[cur]:
+    for j in s[cur]:
         con[j] -= 1
 
         if con[j] == 0:
