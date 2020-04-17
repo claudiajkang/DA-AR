05fbb4cf37c2d70f47abaf054f69a0c7c2eea28c
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-17 14:03:19 +0900

    AG-601:*[0h 5m] Review BOJ-1654 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-1654.py
+++ Algorithms/BinarySearch/BOJ-1654.py
@@ -1,26 +1,25 @@
 from sys import stdin
-
 read = lambda: stdin.readline().rstrip()
 
 k, n = map(int, read().split())
-lan = [0 for i in range(k)]
+lan = [0] * k
 
 for i in range(k):
     lan[i] = int(read())
 
 lo = 1
-hi = max(lan) + 1
+hi = sum(lan) + 1
 
 while (lo + 1) < hi:
     mid = (lo + hi) // 2
 
     c = 0
-    for l in lan:
-        c += (l // mid)
+    for i in lan:
+        c += (i // mid)
 
-    if c < n:
-        hi = mid
-    else:
+    if c >= n:
         lo = mid
+    else:
+        hi = mid
 
 print(lo)
