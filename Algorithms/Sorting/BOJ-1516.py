2795fa6cdcf77bf1612017b69059ea5388c9eb6f
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-17 21:17:20 +0900

    AG-597:*[0h 8m] Review BOJ-1516 [Sort]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-1516.py
+++ Algorithms/Sorting/BOJ-1516.py
@@ -1,12 +1,12 @@
-from collections import deque
+from heapq import heappush, heappop
 from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
 building = [[] for i in range(n + 1)]
-time = [0] * (n + 1)
 con = [0] * (n + 1)
+time = [0] * (n + 1)
 
 for i in range(1, n + 1):
     l = list(map(int, read().split()))
@@ -17,21 +17,24 @@ for i in range(1, n + 1):
         building[j].append(i)
         con[i] += 1
 
-q = deque()
+q = []
 res = [0] * (n + 1)
-for i in range(1, n + 1):
-    if con[i] == 0:
-        res[i] = time[i]
-        q.append(i)
 
 for i in range(1, n + 1):
-    cur = q.popleft()
+    if con[i] == 0:
+        heappush(q, i)
+        res[i] = time[i]
+
+for i in range(1, n + 1):
+    if not q: break
+
+    cur = heappop(q)
 
     for j in building[cur]:
         con[j] -= 1
         res[j] = max(res[j], res[cur] + time[j])
 
         if con[j] == 0:
-            q.append(j)
+            heappush(q, j)
 
 print('\n'.join(map(str, res[1:])))
