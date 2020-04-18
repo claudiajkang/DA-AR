6d6c37c1d38a597f4d51be8e216f4910f812a106
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-18 12:13:45 +0900

    AG-608:*[0h 22m] Review BOJ-2343 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-2343.py
+++ Algorithms/BinarySearch/BOJ-2343.py
@@ -1,18 +1,16 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
 
+setrecursionlimit(10 ** 6)
 read = lambda: stdin.readline().rstrip()
 
-n, m = map(int, read().split())
-lesson = list(map(int, read().split()))
 
-lo = max(lesson)
-hi = sum(lesson)
+def bs(lo, hi):
+    global m, lesson, res
 
-while lo < hi:
     mid = (lo + hi) // 2
 
-    c = 0
     s = 0
+    c = 0
 
     for l in lesson:
         if (s + l) <= mid:
@@ -24,10 +22,22 @@ while lo < hi:
     if s > 0:
         c += 1
 
-    if c >= m:
-        lo = mid + 1
+    if c <= m:
+        res = mid
+        return lo, mid - 1
 
     else:
-        hi = mid
+        return mid + 1, hi
 
-print(lo)
+
+n, m = map(int, read().split())
+lesson = list(map(int, read().split()))
+
+lo = max(lesson)
+hi = sum(lesson)
+res = 0
+
+while lo <= hi:
+    lo, hi = bs(lo, hi)
+
+print(res)
