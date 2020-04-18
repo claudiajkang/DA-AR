bf9f8cd6bd207b5ca17864c7c1c0b074ae9118c1
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-19 07:21:15 +0900

    AG-635:*[0h 24m] Review BOJ-1654 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-1654.py
+++ Algorithms/BinarySearch/BOJ-1654.py
@@ -2,20 +2,21 @@ from sys import stdin, setrecursionlimit
 setrecursionlimit(10 ** 6)
 read = lambda: stdin.readline().rstrip()
 
-
 def bs(lo, hi):
     global lan, n
 
     mid = (lo + hi) // 2
 
     c = 0
+
     for l in lan:
         c += (l // mid)
 
-    if c < n:
-        return lo, mid - 1
+    if c >= n:
+        return mid, hi
+
     else:
-        return mid + 1, hi
+        return lo, mid
 
 
 k, n = map(int, read().split())
@@ -24,7 +25,7 @@ lan = [int(read()) for i in range(k)]
 lo = 1
 hi = max(lan) + 1
 
-while lo <= hi:
+while (lo + 1) < hi:
     lo, hi = bs(lo, hi)
 
-print(hi)
+print(lo)
