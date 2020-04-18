e12f4ad9ea0be909f7d61c8612aad75babbffb12
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-18 13:32:15 +0900

    AG-623:*[0h 6m] Review BOJ-2110 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-2110.py
+++ Algorithms/BinarySearch/BOJ-2110.py
@@ -1,31 +1,37 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10 ** 6)
 read = lambda: stdin.readline().rstrip()
 
-n, c = map(int, read().split())
-p = [0] * n
 
-for i in range(n):
-    p[i] = int(read())
+def bs(lo, hi):
+    global pos, c, n, res
 
-p.sort()
-lo = 1
-hi = max(p) + 1
-
-while (lo + 1) < hi:
     mid = (lo + hi) // 2
 
     count = 1
-    cur = p[0]
+    s = pos[0]
 
     for i in range(1, n):
-        if (cur + mid) <= p[i]:
-            cur = p[i]
+        if (s + mid) <= pos[i]:
             count += 1
+            s = pos[i]
 
     if count >= c:
-        lo = mid
+        res = mid
+        return mid + 1, hi
     else:
-        hi = mid
+        return lo, mid - 1
 
-print(lo)
 
+n, c = map(int, read().split())
+pos = [int(read()) for i in range(n)]
+pos.sort()
+
+lo = 1
+hi = pos[-1] - pos[0]
+res = 0
+
+while lo <= hi:
+    lo, hi = bs(lo, hi)
+
+print(res)
