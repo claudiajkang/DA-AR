001f6934b21ae2eb91f0ab388d6fe1c4b9e0995f
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-18 12:59:23 +0900

    AG-609:*[0h 7m] Review BOJ-6236 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-6236.py
+++ Algorithms/BinarySearch/BOJ-6236.py
@@ -1,40 +1,42 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10 ** 6)
 
 read = lambda: stdin.readline().rstrip()
 
-n, m = map(int, read().split())
-money = [0] * n
 
-for i in range(n):
-    money[i] = int(read())
+def bs(lo, hi):
+    global money, m, res
 
-if m == 1:
-    print(sum(money))
+    mid = (lo + hi) // 2
 
-else:
-    lo = max(money)
-    hi = sum(money)
-
-    while lo < hi:
-        mid = (lo + hi) // 2
-
-        s = 0
-        c = 0
-
-        for mm in money:
-            if (s + mm) <= mid:
-                s += mm
-            else:
-                s = mm
-                c += 1
-
-        if s > 0:
+    s = 0
+    c = 0
+    for mm in money:
+        if (s + mm) <= mid:
+            s += mm
+        else:
+            s = mm
             c += 1
 
-        if c > m:
-            lo = mid + 1
+    if s > 0:
+        c += 1
 
-        else:
-            hi = mid
+    if c <= m:
+        res = mid
+        return lo, mid - 1
 
-    print(lo)
+    else:
+        return mid + 1, hi
+
+
+n, m = map(int, read().split())
+money = [int(read()) for i in range(n)]
+
+lo = max(money)
+hi = sum(money)
+res = 0
+
+while lo <= hi:
+    lo, hi = bs(lo, hi)
+
+print(res)
