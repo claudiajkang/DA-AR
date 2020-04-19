e381f4bd24ec1ec3611e95a4e9d4f21f19757a4b
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-19 19:54:54 +0900

    AG-626:[0h 6m] Review BOJ-1516 [Sort]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-1516.py
+++ Algorithms/Sorting/BOJ-1516.py
@@ -4,17 +4,16 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
-building = [[] for i in range(n + 1)]
-con = [0] * (n + 1)
+buildings = [[] for i in range(n + 1)]
 time = [0] * (n + 1)
+con = [0] * (n + 1)
 
 for i in range(1, n + 1):
     l = list(map(int, read().split()))
     time[i] = l[0]
 
-    for j in l[1:]:
-        if j == -1: break
-        building[j].append(i)
+    for j in l[1:-1]:
+        buildings[j].append(i)
         con[i] += 1
 
 q = []
@@ -22,19 +21,21 @@ res = [0] * (n + 1)
 
 for i in range(1, n + 1):
     if con[i] == 0:
-        heappush(q, i)
         res[i] = time[i]
+        heappush(q, [res[i], i])
 
+count = [0] * (n + 1)
 for i in range(1, n + 1):
-    if not q: break
+    if not q:
+        break
 
-    cur = heappop(q)
+    co, cur = heappop(q)
 
-    for j in building[cur]:
+    for j in buildings[cur]:
         con[j] -= 1
         res[j] = max(res[j], res[cur] + time[j])
 
         if con[j] == 0:
-            heappush(q, j)
+            heappush(q, [res[j], j])
 
 print('\n'.join(map(str, res[1:])))
