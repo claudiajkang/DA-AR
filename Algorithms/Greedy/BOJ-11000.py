19bbc3141beba150ed35a33726f7349ea1aa89df
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-19 15:53:47 +0900

    AG-620:*[0h 3m] Review BOJ-11000 [Gd]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Greedy/BOJ-11000.py
+++ Algorithms/Greedy/BOJ-11000.py
@@ -4,21 +4,17 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
-s = []
-
-for i in range(n):
-    s.append(list(map(int, read().split())))
-
-s.sort(key=lambda x: x[1])
-s.sort(key=lambda x: x[0])
+time = [list(map(int, read().split())) for i in range(n)]
+time.sort(key=lambda x: x[1])
+time.sort(key=lambda x: x[0])
 
 q = []
-heappush(q, s[0][1])
+heappush(q, time[0][1])
 
 for i in range(1, n):
-    if q[0] <= s[i][0]:
+    if q[0] <= time[i][0]:
         r = heappop(q)
 
-    heappush(q, s[i][1])
+    heappush(q, time[i][1])
 
 print(len(q))
