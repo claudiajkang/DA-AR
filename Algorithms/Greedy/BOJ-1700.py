f49548a07f1f1728e434a88e7c96a4244882a0fb
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-19 16:02:48 +0900

    AG-619:*[0h 8m] Review BOJ-1700 [Gd]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Greedy/BOJ-1700.py
+++ Algorithms/Greedy/BOJ-1700.py
@@ -3,40 +3,37 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n, k = map(int, read().split())
-order = list(map(int, read().split()))
-elec = {i: [] for i in set(order)}
+t = list(map(int, read().split()))
+elec = {i: [] for i in set(t)}
 
 for i in range(k):
-    elec[order[i]].append(i)
+    elec[t[i]].append(i)
 
 q = []
-
-res = 0
+count = 0
 
 for i in range(k):
-    o = order[i]
+    cur = t[i]
 
-    if o in q:
-        if elec[o]: t = elec[o].pop(0)
+    if cur in q:
+        elec[cur].pop(0)
         continue
 
     if len(q) == n:
-        t = []
+        tl = []
 
         for i in q:
             if len(elec[i]) == 0:
-                t.append([101, i])
+                tl.append([k + 1, i])
                 break
             else:
-                t.append([elec[i][0], i])
+                tl.append([elec[i][0], i])
 
-        t.sort(reverse=True, key=lambda x: x[0])
-        idx = t[0][1]
+        tl.sort(key=lambda x: x[0], reverse=True)
+        q.pop(q.index(tl[0][1]))
+        count += 1
 
-        q.pop(q.index(idx))
-        res += 1
+    q.append(cur)
+    elec[cur].pop(0)
 
-    q.append(o)
-    if elec[o]: elec[o].pop(0)
-
-print(res)
+print(count)
