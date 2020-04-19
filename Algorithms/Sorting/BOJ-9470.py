e766b033b2e731aa954e3e99d2187779c2406086
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-19 20:04:28 +0900

    AG-625:[0h 4m] Review BOJ-9470 [Sort]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-9470.py
+++ Algorithms/Sorting/BOJ-9470.py
@@ -1,5 +1,6 @@
-from sys import stdin
 from heapq import heappush, heappop
+from sys import stdin
+
 read = lambda: stdin.readline().rstrip()
 
 t = int(read())
@@ -7,42 +8,41 @@ t = int(read())
 for tt in range(t):
     k, m, p = map(int, read().split())
 
-    river = [[] for i in range(m+1)]
-    con = [0] * (m+1)
+    arr = [[] for i in range(m + 1)]
+    con = [0] * (m + 1)
 
     for i in range(p):
         a, b = map(int, read().split())
-        river[a].append(b)
+        arr[a].append(b)
         con[b] += 1
 
     q = []
-    res = [0] * (m+1)
-    rcon = [0] * (m+1)
+    res = [0] * (m + 1)
 
-    for i in range(1, m+1):
+    for i in range(1, m + 1):
         if con[i] == 0:
             res[i] = 1
             heappush(q, [res[i], i])
 
-    for i in range(1, m+1):
-        if not q : break
+    count = [0] * (m + 1)
 
-        cr, cur = heappop(q)
+    for i in range(1, m + 1):
+        if not q: break
 
-        for j in river[cur]:
+        ci, cur = heappop(q)
+
+        for j in arr[cur]:
             con[j] -= 1
 
             if res[cur] > res[j]:
                 res[j] = res[cur]
-                rcon[j] = 1
+                count[j] = 1
 
             elif res[cur] == res[j]:
-                rcon[j] += 1
+                count[j] += 1
 
             if con[j] == 0:
-                if rcon[j] > 1:
-                    res[j] += 1
-
+                res[j] += 1 if count[j] > 1 else 0
                 heappush(q, [res[j], j])
 
     print(f"{k} {max(res)}")
