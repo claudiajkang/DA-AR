340e56aec11c0d07be9dbbbd033972b387082998
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-19 14:29:59 +0900

    AG-622:*[0h 8m] Review BOJ-1920 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-1920.py
+++ Algorithms/BinarySearch/BOJ-1920.py
@@ -1,27 +1,35 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
 
+setrecursionlimit(10 ** 6)
 read = lambda: stdin.readline().rstrip()
 
+
+def bs(lo, hi, i):
+    global nn, mm
+    mid = (lo + hi) // 2
+
+    if nn[mid] > mm[i]:
+        return lo, mid
+
+    elif nn[mid] == mm[i]:
+        return mid, mid
+
+    else:
+        return mid, hi
+
+
 n = int(read())
-nint = list(map(int, read().split()))
-nint.sort()
+nn = list(map(int, read().split()))
+nn.sort()
+
 m = int(read())
-mint = list(map(int, read().split()))
+mm = list(map(int, read().split()))
 
 for i in range(m):
     lo = 0
     hi = n
 
     while (lo + 1) < hi:
-        mid = (lo + hi) // 2
+        lo, hi = bs(lo, hi, i)
 
-        if nint[mid] > mint[i]:
-            lo = mid
-
-        else:
-            hi = mid
-
-    if mint[lo] == mint[i]:
-        print(1)
-    else:
-        print(0)
+    print(1 if nn[lo] == mm[i] else 0)
