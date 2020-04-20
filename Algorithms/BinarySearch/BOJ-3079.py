59d2bd28ca6fb1a402ec272c1071db2a7ec1c295
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-20 20:44:09 +0900

    AG-656:[0h 2m] Review BOJ-3079 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-3079.py
+++ Algorithms/BinarySearch/BOJ-3079.py
@@ -1,28 +1,33 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
 
+setrecursionlimit(10 ** 6)
 read = lambda: stdin.readline().rstrip()
+
+
+def bs(lo, hi):
+    global time, m
+
+    mid = (lo + hi) // 2
+    s = 0
+
+    for t in time:
+        s += (mid // t)
+
+    if s >= m:
+        return lo, mid
+
+    else:
+        return mid, hi
+
 
 n, m = map(int, read().split())
 time = [int(read()) for i in range(n)]
+time.sort()
 
-if m == 1:
-    print(min(time))
-    exit()
-
-lo = 1
-hi = min(time) * m + 1
+lo = 0
+hi = time[-1] * m
 
 while (lo + 1) < hi:
-    mid = (lo + hi) // 2
-
-    count = 0
-    for t in time:
-        count += (mid // t)
-
-    if count < m:
-        lo = mid
-
-    else:
-        hi = mid
+    lo, hi = bs(lo, hi)
 
 print(hi)
