01f42510e39d13cabc8f0f6382c09ddb893eda3b
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-20 21:23:27 +0900

    AG-659:*[0h 5m] Review BOJ-10819 [BF]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BruteForce/BOJ-10819.py
+++ Algorithms/BruteForce/BOJ-10819.py
@@ -1,17 +1,19 @@
-from sys import stdin
 from itertools import permutations
+from sys import stdin
+
 read = lambda: stdin.readline().rstrip()
 
 n = int(read())
 a = list(map(int, read().split()))
-alist = list(map(list, permutations(sorted(a))))
+a.sort()
+l = list(permutations(a))
 
 res = 0
 
-for i in alist:
+for i in l:
     ts = 0
-    for j in range(n-1):
-        ts += abs(i[j] - i[j+1])
+    for j in range(n - 1):
+        ts += abs(i[j] - i[j + 1])
     res = max(res, ts)
 
 print(res)
