a6352c53e96b37136088e6654e9b8220db72b801
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 07:03:15 +0900

    AG-680:*[0h 10m] Review BOJ-1504 [Gp]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-1504.py
+++ Algorithms/GraphTheory/BOJ-1504.py
@@ -5,25 +5,27 @@ setrecursionlimit(10 ** 6)
 read = lambda: stdin.readline().rstrip()
 
 
-def dijkstra(s, e):
+def search(start, end):
     global g, n, INF
 
-    q = []
     dist = [INF] * (n + 1)
-    dist[s] = 0
-    heappush(q, [0, s])
+    dist[start] = 0
+
+    q = []
+    heappush(q, [0, start])
 
     while q:
         cost, cur = heappop(q)
 
-        if dist[cur] < cost: continue
+        if dist[cur] < cost:
+            continue
 
         for nxt, d in g[cur]:
-            if dist[nxt] > dist[cur] + d:
+            if dist[nxt] > (dist[cur] + d):
                 dist[nxt] = dist[cur] + d
                 heappush(q, [dist[nxt], nxt])
 
-    return dist[e]
+    return dist[end]
 
 
 INF = 1e9
@@ -31,14 +33,14 @@ n, e = map(int, read().split())
 g = [[] for i in range(n + 1)]
 
 for i in range(e):
-    tu, tv, tw = map(int, read().split())
-    g[tu].append([tv, tw])
-    g[tv].append([tu, tw])
+    a, b, c = map(int, read().split())
+    g[a].append([b, c])
+    g[b].append([a, c])
 
-a, b = map(int, read().split())
+v1, v2 = map(int, read().split())
 
-d1 = dijkstra(1, a) + dijkstra(a, b) + dijkstra(b, n)
-d2 = dijkstra(1, b) + dijkstra(b, a) + dijkstra(a, n)
+d1 = search(1, v1) + search(v1, v2) + search(v2, n)
+d2 = search(1, v2) + search(v2, v1) + search(v1, n)
 
 res = min(d1, d2)
-print(res if res < INF else -1)
+print(res if res < 1e9 else -1)
