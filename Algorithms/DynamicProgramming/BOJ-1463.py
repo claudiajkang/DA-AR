db192b25a0a57b539d884c76c1f1fbb90384662c
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 08:03:28 +0900

    AG-687:[0h 3m] Review BOJ-1463 [DP]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/DynamicProgramming/BOJ-1463.py
+++ Algorithms/DynamicProgramming/BOJ-1463.py
@@ -1,25 +1,22 @@
 from sys import stdin
 
 x = int(stdin.readline().rstrip())
-dp = [0, 0, 1, 1]
-if x <= 3:
-    print(dp[x])
-    exit()
 
-dp = dp + [0] * x
-mn = 10 ** 6 + 1
+dp = [0, 0, 1, 1] + [0] * x
+INF = 10 ** 6
 
-for i in range(4, x + 1):
-    t1 = dp[i - 1] + 1
+if x > 3:
+    for i in range(4, x + 1):
+        t1 = dp[i - 1] + 1
+        t2 = INF
+        t3 = INF
 
-    t2 = mn
-    if i % 2 == 0:
-        t2 = dp[i // 2] + 1
+        if i % 2 == 0:
+            t2 = dp[i // 2] + 1
 
-    t3 = mn
-    if i % 3 == 0:
-        t3 = dp[i // 3] + 1
+        if i % 3 == 0:
+            t3 = dp[i // 3] + 1
 
-    dp[i] = min(t1, t2, t3)
+        dp[i] = min(t1, t2, t3)
 
 print(dp[x])
