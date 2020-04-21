dd3e9da0869d3ff275741170ca50026bdff0d4bb
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 08:10:22 +0900

    AG-684:*[0h 5m] Review BOJ-1904 [DP]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/DynamicProgramming/BOJ-1904.py
+++ Algorithms/DynamicProgramming/BOJ-1904.py
@@ -1,12 +1,11 @@
 from sys import stdin
 
 n = int(stdin.readline().rstrip())
-dp = [0] * (n + 1)
 
-for i in range(1, n + 1):
-    if i in [1, 2]:
-        dp[i] = i
-        continue
-    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+dp = [0, 1, 2] + [0] * n
+
+if n > 2:
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
 
 print(dp[n])
