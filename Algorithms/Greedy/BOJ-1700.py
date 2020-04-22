7effc7e6b4d48ac0f1f0c3e1d0259e2e5b63bac9
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 12:40:06 +0900

    AG-671:*[0h 7m] Review BOJ-1700 [Gd]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Greedy/BOJ-1700.py
+++ Algorithms/Greedy/BOJ-1700.py
@@ -3,35 +3,36 @@ from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
 n, k = map(int, read().split())
-e = list(map(int, read().split()))
-elec = {i: [] for i in set(e)}
+device = list(map(int, read().split()))
+elec = {i: [] for i in set(device)}
 
 for i in range(k):
-    elec[e[i]].append(i)
+    elec[device[i]].append(i)
 
 q = []
 res = 0
-for i in range(k):
-    o = e[i]
 
-    if o in q:
-        elec[o].pop(0)
+for i in range(k):
+    e = device[i]
+
+    if e in q:
+        elec[e].pop(0)
         continue
 
     if len(q) == n:
         tq = []
 
-        for i in q:
-            if len(elec[i]) == 0:
-                tq.append([k + 1, i])
+        for tt in q:
+            if len(elec[tt]) == 0:
+                tq.append([k + 1, tt])
                 break
-            tq.append([elec[i][0], i])
+            tq.append([elec[tt][0], tt])
 
-        tq.sort(key=lambda x: x[0], reverse=True)
+        tq.sort(reverse=True, key=lambda x: x[0])
         q.remove(tq[0][1])
         res += 1
 
-    q.append(o)
-    elec[o].pop(0)
+    q.append(e)
+    elec[e].pop(0)
 
 print(res)
