7473132207db7725b266704ec83ff1921913feed
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 12:15:05 +0900

    AG-673:[0h 3m] Review BOJ-10815 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-10815.py
+++ Algorithms/BinarySearch/BOJ-10815.py
@@ -1,17 +1,5 @@
-from sys import stdin, setrecursionlimit
-setrecursionlimit(10**6)
+from sys import stdin
 read = lambda: stdin.readline().rstrip()
-
-def bs(lo, hi, v):
-    global a
-
-    mid = (lo + hi) // 2
-
-    if a[mid] > v:
-        return lo, mid
-    else:
-        return mid, hi
-
 
 n = int(read())
 a = list(map(int, read().split()))
@@ -20,19 +8,22 @@ a.sort()
 m = int(read())
 b = list(map(int, read().split()))
 
-res = [0] * m
-
+res = []
 for i in range(m):
+    if b[i] == a[n - 1]:
+        res.append(1)
+        continue
     lo = 0
     hi = n - 1
 
-    if a[n-1] == b[i]:
-        res[i] = 1
-        continue
-
     while (lo + 1) < hi:
-        lo, hi = bs(lo, hi, b[i])
+        mid = (lo + hi) // 2
 
-    res[i] = 1 if a[lo] == b[i] else 0
+        if a[mid] > b[i]:
+            hi = mid
+        else:
+            lo = mid
+
+    res.append(1 if a[lo] == b[i] else 0)
 
 print(' '.join(map(str, res)))
