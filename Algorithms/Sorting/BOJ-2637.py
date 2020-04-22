7f40859a37573a1105fac2e422fb9ff873b83a2c
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 12:26:00 +0900

    AG-662:*[0h 6m] Review BOJ-2637 [Sort]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Sorting/BOJ-2637.py
+++ Algorithms/Sorting/BOJ-2637.py
@@ -5,36 +5,36 @@ read = lambda: stdin.readline().rstrip()
 
 n = int(read())
 m = int(read())
-base = [[] for i in range(n + 1)]
+
+g = [[] for i in range(n + 1)]
 con = [0] * (n + 1)
 
 for i in range(m):
     a, b, c = map(int, read().split())
-    base[b].append([a, c])
+    g[b].append([a, c])
     con[a] += 1
 
-res = [[0] * n for i in range(n + 1)]
-
-must = []
+res = [[0] * (n + 1) for i in range(n + 1)]
 q = deque()
+must = deque()
+
 for i in range(1, n + 1):
     if con[i] == 0:
         res[i][i] = 1
-        must.append(i)
         q.append(i)
+        must.append(i)
 
 while q:
     cur = q.popleft()
 
-    for mid, count in base[cur]:
-        con[mid] -= 1
+    for nxt, d in g[cur]:
+        con[nxt] -= 1
 
-        for i in must:
-            res[mid][i] += res[cur][i] * count
+        for j in must:
+            res[nxt][j] += res[cur][j] * d
 
-        if con[mid] == 0:
-            q.append(mid)
+        if con[nxt] == 0:
+            q.append(nxt)
 
-must.sort()
 for i in must:
     print(f"{i} {res[n][i]}")
