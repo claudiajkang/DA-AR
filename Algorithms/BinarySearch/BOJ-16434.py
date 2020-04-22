509201d520c461d3ca535336c5d5d7cc0dbb80d1
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 12:10:01 +0900

    AG-660:[0h 6m] Review BOJ-16434 [BS]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/BinarySearch/BOJ-16434.py
+++ Algorithms/BinarySearch/BOJ-16434.py
@@ -1,46 +1,47 @@
-from sys import stdin, setrecursionlimit
-setrecursionlimit(10 ** 6)
+from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
-class Node:
+
+class r:
     def __init__(self, l):
         self.type = l[0]
         self.atk = l[1]
         self.hp = l[2]
 
 
-def bs(lo, hi):
-    global room, Hatk
-
-    maxhp = (lo + hi) // 2
-    curhp = maxhp
-    curatk = Hatk
-
-    for r in room:
-        if r.type == 1:
-            if r.hp % curatk:
-                curhp -= (r.hp // curatk) * r.atk
-
-            else:
-                curhp -= ((r.hp // curatk) - 1) * r.atk
-
-            if curhp <= 0:
-                return maxhp, hi
-
-        else:
-            curatk += r.atk
-            curhp = min(curhp + r.hp, maxhp)
-
-    return lo, maxhp
-
-
 n, Hatk = map(int, read().split())
-room = [Node(list(map(int, read().split()))) for i in range(n)]
+room = [r(list(map(int, read().split()))) for i in range(n)]
 
 lo = 0
 hi = 10 ** 18
 
 while (lo + 1) < hi:
-    lo, hi = bs(lo, hi)
+    Hmax = (lo + hi) // 2
+
+    curHp = Hmax
+    curAtk = Hatk
+    flag = False
+
+    for t in room:
+        if t.type == 1:
+            if t.hp % curAtk:
+                curHp -= ((t.hp // curAtk) * t.atk)
+
+            else:
+                curHp -= (((t.hp // curAtk) - 1) * t.atk)
+
+            if curHp <= 0:
+                flag = True
+                break
+
+        else:
+            curAtk += t.atk
+            curHp = min(curHp + t.hp, Hmax)
+
+    if flag:
+        lo = Hmax
+
+    else:
+        hi = Hmax
 
 print(hi)
