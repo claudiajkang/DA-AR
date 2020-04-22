0a3662aa9eff6eb8e61053b6556d535b4c518fd7
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 09:53:00 +0900

    AG-670:*[0h 3m] Review BOJ-1629 [D/C]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/DivideAndConquer/BOJ-1629.py
+++ Algorithms/DivideAndConquer/BOJ-1629.py
@@ -1,22 +1,19 @@
 from sys import stdin, setrecursionlimit
-
 setrecursionlimit(10 ** 6)
-
 
 def multiple(a, b):
     global c
 
-    if 0 <= b <= 1:
+    if b == 1:
         return a % c
 
-    elif b % 2 == 0:
-        r = multiple(a, b // 2)
-        return (r * r) % c
     else:
-        r = multiple(a, b // 2)
-        return (((r * r) % c) * a) % c
+        t = multiple(a, b // 2)
+        if b % 2:
+            return (((t * t) % c) * a) % c
+        else:
+            return (t * t) % c
 
 
 a, b, c = map(int, stdin.readline().rstrip().split())
-
-print(multiple(a, b))
+print(multiple(a, b) % c)
