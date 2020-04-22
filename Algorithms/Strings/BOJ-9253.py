65c627e1a19e33561c3a65df4c11d0c30bb32adb
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-22 19:15:31 +0900

    AG-668:*[0h 7m] Review BOJ-9253 [str]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Strings/BOJ-9253.py
+++ Algorithms/Strings/BOJ-9253.py
@@ -1,51 +1,46 @@
 from sys import stdin
 read = lambda: stdin.readline().rstrip()
 
+def get_fail(pattern):
+    fail = [0] * len(pattern)
+
+    j = 0
+    for i in range(1, len(pattern)):
+        if j > 0 and pattern[i] != pattern[j]:
+            j = fail[j - 1]
+
+        if pattern[i] == pattern[j]:
+            j += 1
+            fail[i] = j
+
+    return fail
+
+
+def kmp(strings, pattern, fail):
+    j = 0
+    for i in range(len(strings)):
+        if j > 0 and strings[i] != pattern[j]:
+            j = fail[j - 1]
+
+        if strings[i] == pattern[j]:
+            if j == len(pattern) - 1:
+                return True
+
+            else:
+                j += 1
+
+    return False
+
+
 a = read()
 b = read()
 p = read()
 
-alen = len(a)
-blen = len(b)
-plen = len(p)
+fail = get_fail(p)
+ra = kmp(a, p, fail)
+rb = kmp(b, p, fail)
 
-fail = [0] * plen
-
-j = 0
-for i in range(1, plen):
-    while j > 0 and p[i] != p[j]:
-        j = fail[j-1]
-
-    if p[i] == p[j]:
-        j += 1
-        fail[i] = j
-
-j = 0
-suc = [False, False]
-
-for i in range(alen):
-    while j > 0 and a[i] != p[j]:
-        j = fail[j-1]
-
-    if a[i] == p[j]:
-        if j == (plen - 1):
-            suc[0] = True
-            j = fail[j]
-
-        else:
-            j += 1
-
-for i in range(blen):
-    while j > 0 and b[i] != p[j]:
-        j = fail[j-1]
-
-    if b[i] == p[j]:
-        if j == (plen - 1):
-            suc[1] = True
-            j = fail[j]
-
-        else:
-            j += 1
-
-if suc[0] and suc[1] : print("YES")
-else: print("NO")
+if ra and rb:
+    print('YES')
+else:
+    print('NO')
