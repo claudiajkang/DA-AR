6fa0ad050bfe40998b18242f30908d7347625c3f
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 21:04:36 +0900

    AG-712:*[0h 10m] Review BOJ-1786 [Str]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Strings/BOJ-1786.py
+++ Algorithms/Strings/BOJ-1786.py
@@ -1,38 +1,34 @@
-from collections import deque
 from sys import stdin
 
-read = lambda: stdin.readline().rstrip()
+t = stdin.readline().rstrip()
+p = stdin.readline().rstrip()
 
-
-s = read()
-p = read()
-slen = len(s)
+tlen = len(t)
 plen = len(p)
 
-fail = [0] * len(p)
+fail = [0] * plen
 
 j = 0
 for i in range(1, plen):
-    while (j > 0) and p[i] != p[j]:
+    while j > 0 and p[j] != p[i]:
         j = fail[j - 1]
 
-    if p[i] == p[j]:
+    if p[j] == p[i]:
         j += 1
         fail[i] = j
 
 j = 0
-res = deque()
-for i in range(slen):
-    while j > 0 and s[i] != p[j]:
+res = []
+for i in range(tlen):
+    while j > 0 and p[j] != t[i]:
         j = fail[j - 1]
 
-    if s[i] == p[j]:
-        if j == plen - 1:
+    if p[j] == t[i]:
+        if j == (plen - 1):
             res.append(i - plen + 2)
             j = fail[j]
         else:
             j += 1
 
 print(len(res))
-for i in res:
-    print(i)
+print('\n'.join(map(str, res)))
