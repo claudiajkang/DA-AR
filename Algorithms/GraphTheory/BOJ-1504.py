53e5aefea570bb45d00aea4b8968c185b0cb71c2
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 22:10:00 +0900

    AG-701:[0h 4m] Review BOJ-1504 [Gp]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-1504.py
+++ Algorithms/GraphTheory/BOJ-1504.py
@@ -1,46 +1,47 @@
 from heapq import heappush, heappop
-from sys import stdin, setrecursionlimit
+from sys import stdin
 
-setrecursionlimit(10 ** 6)
 read = lambda: stdin.readline().rstrip()
 
 
-def search(start, end):
-    global g, n, INF
+def distance(start, end):
+    global g, INF, n
 
     dist = [INF] * (n + 1)
-    dist[start] = 0
+    visited = [False] * (n + 1)
 
+    dist[start] = 0
     q = []
-    heappush(q, [0, start])
+    heappush(q, [dist[start], start])
 
     while q:
         cost, cur = heappop(q)
 
-        if dist[cur] < cost:
-            continue
+        if visited[cur]: continue
+        visited[cur] = True
 
-        for nxt, d in g[cur]:
-            if dist[nxt] > (dist[cur] + d):
-                dist[nxt] = dist[cur] + d
-                heappush(q, [dist[nxt], nxt])
+        for j, d in g[cur]:
+            if dist[j] > dist[cur] + d:
+                dist[j] = dist[cur] + d
+                heappush(q, [dist[j], j])
 
     return dist[end]
 
 
-INF = 1e9
 n, e = map(int, read().split())
 g = [[] for i in range(n + 1)]
+INF = 1e9
 
 for i in range(e):
     a, b, c = map(int, read().split())
     g[a].append([b, c])
     g[b].append([a, c])
 
-v1, v2 = map(int, read().split())
+a, b = map(int, read().split())
 
-d1 = search(1, v1) + search(v1, v2) + search(v2, n)
-d2 = search(1, v2) + search(v2, v1) + search(v1, n)
+d1 = distance(1, a) + distance(a, b) + distance(b, n)
+d2 = distance(1, b) + distance(b, a) + distance(a, n)
 
 res = min(d1, d2)
-print(res if res < 1e9 else -1)
+
+print(res if res < INF else -1)
