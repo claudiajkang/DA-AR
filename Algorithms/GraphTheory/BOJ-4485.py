ccc973b3d1d15b6796c3d648871fcc105bf6dde2
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 22:15:48 +0900

    AG-699:[0h 4m] Review BOJ-4485 [Gp]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-4485.py
+++ Algorithms/GraphTheory/BOJ-4485.py
@@ -3,12 +3,9 @@ from sys import stdin
 
 read = lambda: stdin.readline().rstrip()
 
-INF = 1e9
-p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 case = 1
 while True:
     n = int(read())
-
     if n == 0: break
 
     g = [[] for i in range(n)]
@@ -16,30 +13,35 @@ while True:
     for i in range(n):
         g[i] = list(map(int, read().split()))
 
-    visited = [[False] * n for i in range(n)]
+    INF = 1e9
+
     dist = [[INF] * n for i in range(n)]
+    visited = [[False] * n for i in range(n)]
 
     dist[0][0] = g[0][0]
     q = []
     heappush(q, [dist[0][0], 0, 0])
 
+    p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+
     while q:
-        cost, ci, cj = heappop(q)
+        cost, ch, cw = heappop(q)
 
-        if visited[ci][cj]: continue
+        if visited[ch][cw]:
+            continue
 
-        visited[ci][cj] = True
+        visited[ch][cw] = True
 
-        for ii, jj in p:
-            ti = ii + ci
-            tj = jj + cj
+        for hh, ww in p:
+            th = ch + hh
+            tw = cw + ww
 
-            if ti < 0 or ti >= n or tj < 0 or tj >= n:
+            if th < 0 or tw < 0 or th >= n or tw >= n:
                 continue
 
-            if dist[ti][tj] > dist[ci][cj] + g[ti][tj]:
-                dist[ti][tj] = dist[ci][cj] + g[ti][tj]
-                heappush(q, [dist[ti][tj], ti, tj])
+            if dist[th][tw] > dist[ch][cw] + g[th][tw]:
+                dist[th][tw] = dist[ch][cw] + g[th][tw]
+                heappush(q, [dist[th][tw], th, tw])
 
     print(f"Problem {case}: {dist[n - 1][n - 1]}")
     case += 1
