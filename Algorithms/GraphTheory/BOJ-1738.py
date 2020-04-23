bccb48f0e1474d10fa5db7ef8350e8b1da9fdbf5
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 22:24:14 +0900

    AG-707:*[0h 7m] Review BOJ-1738 [Gp]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-1738.py
+++ Algorithms/GraphTheory/BOJ-1738.py
@@ -5,7 +5,7 @@ read = lambda: stdin.readline().rstrip()
 
 n, m = map(int, read().split())
 g = [[] for i in range(n + 1)]
-r = [[] for j in range(n + 1)]
+r = [[] for i in range(n + 1)]
 
 for i in range(m):
     a, b, c = map(int, read().split())
@@ -13,36 +13,37 @@ for i in range(m):
     r[b].append(a)
 
 visited = [False] * (n + 1)
+INF = 1e9
+dist = [INF] * (n + 1)
+
 visited[n] = True
 q = deque()
 q.append(n)
 
 while q:
     cur = q.popleft()
+
     for j in r[cur]:
         if not visited[j]:
             visited[j] = True
             q.append(j)
 
-INF = 1e9
-dist = [INF] * (n + 1)
-prev = [0] * (n + 1)
-
 dist[1] = 0
+prev = [0] * (n + 1)
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         for nxt, d in g[j]:
-            if dist[j] != INF and dist[nxt] > dist[j] + d:
+            if dist[nxt] > dist[j] + d:
                 if i == n and visited[nxt]:
-                    print("-1")
+                    print(-1)
                     exit()
 
-                dist[nxt] = dist[j] + d
                 prev[nxt] = j
+                dist[nxt] = dist[j] + d
 
 if dist[n] == INF:
-    print("-1")
+    print(-1)
 else:
     res = [n]
 
