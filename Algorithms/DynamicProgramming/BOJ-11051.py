a3f3560e02edbf551d9a4778a7a6ee7c43c15cb3
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 23:22:31 +0900

    AG-711:*[0h 2m] Review BOJ-11051 [DP]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/DynamicProgramming/BOJ-11051.py
+++ Algorithms/DynamicProgramming/BOJ-11051.py
@@ -1,5 +1,6 @@
 from math import factorial
+from sys import stdin
 
-n, k = map(int, input().split())
+n, k = map(int, stdin.readline().rstrip().split())
 result = factorial(n) // (factorial(k) * factorial(n - k))
 print(result % 10007)
