f417924fc30c9c1c7b316c8ca11a43b938053f7a
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 22:34:05 +0900

    AG-707:*[0h 7m] Review BOJ-11657 [Gp]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-11657.py
+++ Algorithms/GraphTheory/BOJ-11657.py
@@ -1,33 +1,27 @@
 from sys import stdin
-
 read = lambda: stdin.readline().rstrip()
 
 n, m = map(int, read().split())
-adj = [[] for i in range(n + 1)]
+g = [[] for i in range(n + 1)]
 
 for i in range(m):
     a, b, c = map(int, read().split())
-    adj[a].append([b, c])
+    g[a].append([b, c])
 
-minusCycle = False
 INF = 1e9
-
 dist = [INF] * (n + 1)
+
 dist[1] = 0
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        for nxt, d in adj[j]:
+        for nxt, d in g[j]:
             if (dist[j] != INF) and (dist[nxt] > dist[j] + d):
-                dist[nxt] = dist[j] + d
                 if i == n:
-                    minusCycle = True
+                    print(-1)
+                    exit()
 
-if minusCycle:
-    print(-1)
-else:
-    for i in range(2, n + 1):
-        if dist[i] == INF:
-            print(-1)
-        else:
-            print(dist[i])
+                dist[nxt] = dist[j] + d
+
+for i in range(2, n + 1):
+    print(dist[i] if dist[i] < INF else -1)
