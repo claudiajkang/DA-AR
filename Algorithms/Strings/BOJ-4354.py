671a7bfb70f15aa33073cf013ab09f630d3273ef
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 21:20:45 +0900

    AG-718:*[0h 4m] Review BOJ-4354 [Str]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Strings/BOJ-4354.py
+++ Algorithms/Strings/BOJ-4354.py
@@ -1,32 +1,27 @@
 from sys import stdin
-
 read = lambda: stdin.readline().rstrip()
 
 while True:
     p = read()
-
-    if p == '.':
-        break
+    if p == '.': break
 
     l = len(p)
-
     fail = [0] * l
 
     j = 0
 
     for i in range(1, l):
-        if (j > 0) and p[i] != p[j]:
+        if j > 0 and p[i] != p[j]:
             j = fail[j - 1]
 
         if p[i] == p[j]:
             j += 1
             fail[i] = j
 
-    q = fail[l - 1]
-    r = l - q
+    p = fail[-1]
+    q = l - p
 
-    if q == 0 or (q % r != 0):
+    if p == 0 or (p % q != 0):
         print(1)
-
     else:
-        print(q // r + 1)
+        print(p // q + 1)
