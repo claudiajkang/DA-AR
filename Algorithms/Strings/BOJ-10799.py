c8d408d63979269606d90ed3b7abf743b02ca535
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 20:03:38 +0900

    AG-702:[0h 3m] Review BOJ-10799 [Str]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Strings/BOJ-10799.py
+++ Algorithms/Strings/BOJ-10799.py
@@ -1,19 +1,19 @@
 from sys import stdin
 
-s = stdin.readline().rstrip()
+strings = stdin.readline().rstrip()
 stack = []
 res = 0
 
-for i in range(len(s)):
-    if stack and s[i-1] == '(' and s[i] == ')':
-        v = stack.pop()
+for i in range(len(strings)):
+    if stack and strings[i - 1] == '(' and strings[i] == ')':
+        stack.pop()
         res += len(stack)
 
-    elif stack and stack[-1] == '(' and s[i] == ')':
-        v = stack.pop()
+    elif stack and stack[-1] == '(' and strings[i] == ')':
+        stack.pop()
         res += 1
 
     else:
-        stack.append(s[i])
+        stack.append(strings[i])
 
 print(res)
