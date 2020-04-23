da82fc3e07ab07b86b9aa4ff8f15154834b8f69c
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-23 22:04:39 +0900

    AG-717:*[0h 12m] Review BOJ-1261 [Gp]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/GraphTheory/BOJ-1261.py
+++ Algorithms/GraphTheory/BOJ-1261.py
@@ -1,5 +1,6 @@
+from heapq import heappop, heappush
 from sys import stdin
-from heapq import heappush, heappop
+
 read = lambda: stdin.readline().rstrip()
 
 w, h = map(int, read().split())
@@ -8,16 +9,15 @@ g = [[] for i in range(h)]
 for i in range(h):
     g[i] = list(map(int, list(read())))
 
+p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+
 INF = 1e9
-visited = [[False] * w for i in range(h)]
 dist = [[INF] * w for i in range(h)]
+visited = [[False] * w for i in range(h)]
 
 dist[0][0] = 0
 q = []
 heappush(q, [dist[0][0], 0, 0])
-
-res = 0
-p = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
 while q:
     cost, ch, cw = heappop(q)
@@ -26,10 +26,10 @@ while q:
     visited[ch][cw] = True
 
     for hh, ww in p:
-        th = hh + ch
-        tw = ww + cw
+        th = ch + hh
+        tw = cw + ww
 
-        if th < 0 or th >= h or tw < 0 or tw >= w:
+        if th < 0 or tw < 0 or th >= h or tw >= w:
             continue
 
         if dist[th][tw] > dist[ch][cw] + g[th][tw]:
