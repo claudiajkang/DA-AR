f6ef0cc33bef3710a4757399dd1a1b7f401132db
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-24 12:17:58 +0900

    AG-721:*[0h 5m] Review BOJ-2294 [DP]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/DynamicProgramming/BOJ-2294.py
+++ Algorithms/DynamicProgramming/BOJ-2294.py
@@ -4,6 +4,7 @@ read = lambda: stdin.readline().rstrip()
 
 n, k = map(int, read().split())
 coin = [int(read()) for i in range(n)]
+
 dp = [0] * (k + 1)
 
 for i in range(1, k + 1):
@@ -13,9 +14,6 @@ for i in range(1, k + 1):
         if j <= i and dp[i - j] != -1:
             a.append(dp[i - j])
 
-    if not a:
-        dp[i] = -1
-    else:
-        dp[i] = min(a) + 1
+    dp[i] = min(a) + 1 if a else -1
 
 print(dp[k])
