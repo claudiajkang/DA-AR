96387fdd3c9df63dbe3f0cd8403e1db4a7a2fb23
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-24 15:07:09 +0900

    AG-216:*[0h 7m] BOJ-11054 [DP] 가장 긴 바이토닉 부분 수열
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

---
+++ Algorithms/DynamicProgramming/BOJ-11054.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+read = lambda: stdin.readline().rstrip()
+
+n = int(read())
+a = list(map(int, read().split()))
+
+dp1 = [0] * n
+dp2 = [0] * n
+
+for i in range(n):
+    for j in range(i - 1, -1, -1):
+        if a[j] < a[i]:
+            dp1[i] = max(dp1[i], dp1[j])
+
+    dp1[i] += 1
+
+for i in range(n - 1, -1, -1):
+    for j in range(n - 1, i, -1):
+        if a[j] < a[i]:
+            dp2[i] = max(dp2[i], dp2[j])
+
+    dp2[i] += 1
+
+for i in range(n):
+    dp1[i] += dp2[i]
+
+print(max(dp1) - 1)
