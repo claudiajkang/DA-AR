d4c38896ce3797378155b3d4398b6dc2f57604a9
Claudia J. Kang <claudiajkang@gmail.com>
2020-04-24 16:05:49 +0900

    AG-735:[0h 1m] Review BOJ-1305 [str]
    Signed-off-by: Claudia J. Kang <claudiajkang@gmail.com>

--- Algorithms/Strings/BOJ-1305.py
+++ Algorithms/Strings/BOJ-1305.py
@@ -1,19 +1,17 @@
 from sys import stdin
-
 read = lambda: stdin.readline().rstrip()
 
 l = int(read())
-p = read()
+s = read()
 
 fail = [0] * l
 
 j = 0
-
 for i in range(1, l):
-    if j > 0 and p[i] != p[j]:
+    if j > 0 and s[i] != s[j]:
         j = fail[j - 1]
 
-    if p[i] == p[j]:
+    if s[i] == s[j]:
         j += 1
         fail[i] = j
 
